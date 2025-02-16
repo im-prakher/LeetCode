92f1732dad2228864d035c47e5b7ddebf4e829df
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-17 03:10:11 +0530

    LC-course-schedule-ii [Runtime: N/A; Memory: N/A]

--- topological-sort/course-schedule-ii.java
+++ topological-sort/course-schedule-ii.java
@@ -1,26 +1,36 @@
 class Solution {
     int idx;
-    public void dfsTopo(int[] ans, int node, List<Integer> adj[], boolean vis[]) {
+    public boolean dfsTopo(int[] ans, int node, List<Integer> adj[], boolean vis[], boolean pathVis[]) {
         vis[node] = true;
+        pathVis[node] = true;
+        boolean res = false;
         for(int vtx : adj[node]) {
             if(!vis[vtx])
-                dfsTopo(ans, vtx, adj, vis);
+                res = dfsTopo(ans, vtx, adj, vis, pathVis);
+            else if(pathVis[vtx])
+                return true;
         }
+        pathVis[node] = false;
         ans[idx--] = node;
+        return res;
     }
 
     public int[] findOrder(int n, int[][] preq) {
         idx = n-1;
         int[] ans = new int[n];
         List<Integer> adj[] = new List[n];
-        Arrays.fill(adj, new ArrayList<>());
+        // Arrays.fill(adj, new ArrayList<>());--> will fill each adj[i] with same list reference so don't use as insertion at one index will insert at all indexes
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
         for(int[] edge : preq)  {
             adj[edge[1]].add(edge[0]);
         }
         boolean[] vis = new boolean[n];
+        boolean[] pathVis = new boolean[n];
         for(int i = 0; i < n; i++) {
-            if(!vis[i])
-                dfsTopo(ans, i, adj, vis);
+            if(!vis[i] && dfsTopo(ans, i, adj, vis, pathVis))
+                return new int[0];
         }
         return ans;  
     }
