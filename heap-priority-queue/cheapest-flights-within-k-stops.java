2c8b1d719088576a09f3ed86860f787969d3b24c
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-17 04:28:06 +0530

    LC-cheapest-flights-within-k-stops [Runtime: 6 ms; Memory: 44.7 MB]

--- heap-priority-queue/cheapest-flights-within-k-stops.java
+++ heap-priority-queue/cheapest-flights-within-k-stops.java
@@ -1,21 +1,26 @@
 class Solution {
     final int max = 10000_00000;
+    int dp[][];
     public int dfs(int node, int dst, List<int[]> adj[], boolean[] vis, int k) {
-        int res = max;
+        int res = max;  
         if(k < 0)
             return res;
         if(node == dst)
             return 0;
-        vis[node] = true;
+        if(dp[node][k]!=-1)
+            return dp[node][k];      
         for(int[] vtx : adj[node]) {
-            // if(!vis[vtx[0]])
-                res = Math.min(res, vtx[1] + dfs(vtx[0], dst, adj, vis, k-1));
+            res = Math.min(res, vtx[1] + dfs(vtx[0], dst, adj, vis, k-1));
         }
-        return res;
+        return dp[node][k] = res;
     }
 
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<int[]> adj[] = new List[n];
+        dp = new int[n][k+2];
+        for(int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
         for(int i = 0; i < n; i++) 
             adj[i] = new ArrayList<>();         
         for(int[] flight : flights) {
