446c1d25d247139ee78956b3f089ab4ca95c2f86
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-17 17:39:37 +0530

    LC-meeting-rooms-ii [Runtime: 8 ms; Memory: 46.5 MB]

--- sorting/meeting-rooms-ii.java
+++ sorting/meeting-rooms-ii.java
@@ -1,16 +1,17 @@
 class Solution {
     public int minMeetingRooms(int[][] intv) {
-        Arrays.sort(intv, (a, b) -> (a[1] == b[1]) ? a[0]-b[0] : a[1]-b[1]);
-        int cnt = 1, finish = intv[0][1];
+        Arrays.sort(intv, (a, b) -> (a[0] == b[0]) ? a[1]-b[1] : a[0]-b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int cnt = 1, finish; 
+        pq.offer(intv[0][1]);
         for(int i = 1; i < intv.length; i++) {
+            finish = pq.peek();
             if(finish > intv[i][0]) {
-                finish = Math.min(finish, intv[i][1]);
                 cnt++;
-            } else if(cnt > 1){
-                finish = Math.min(finish, intv[i][1]);
             } else {
-                finish = intv[i][1];
+                pq.poll();
             }
+            pq.offer(intv[i][1]);
         }
         return cnt;
     }
