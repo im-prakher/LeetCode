e2335607c4ea871de84b1b74cb98cf8774f64dfb
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-17 13:45:42 +0530

    LC-path-with-maximum-probability [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/path-with-maximum-probability.java
+++ heap-priority-queue/path-with-maximum-probability.java
@@ -1,11 +1,17 @@
 class Solution {
-    final double max = 1000_00.0;
+    final double min = 0.0;
     public record Node(int val, double wt){}
 
     public double maxPath(int node, int end, List<Node> adj[], boolean[] vis) {
         if(node == end)
-            return 0.0;
+            return 1.0;
         vis[node] = true;
+        double res = min;
+        for(var vtx : adj[node]) {
+            if(!vis[vtx.val])
+                res = Math.max(res, vtx.wt * maxPath(vtx.val, end, adj, vis));
+        }
+        return res;
     }
 
     public double maxProbability(int n, int[][] edges, double[] prob, int start, int end) {
@@ -13,10 +19,10 @@ class Solution {
         boolean[] vis = new boolean[n];
         for(int i= 0; i < n; i++)
             adj[i] = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            adj[edge[i][0]].add(new Node(edge[i][1], prob[i]));
-            adj[edge[i][1]].add(new Node(edge[i][0], prob[i]));
+        for(int i = 0; i < edges.length; i++) {
+            adj[edges[i][0]].add(new Node(edges[i][1], prob[i]));
+            adj[edges[i][1]].add(new Node(edges[i][0], prob[i]));
         }
-        return maxPath(start, end, edges, adj, vis);
+        return maxPath(start, end, adj, vis);
     }
 }
