9d4ec1aa57487d4b5dcea6facf4934f9838a3bb3
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-18 21:09:24 +0530

    LC-network-delay-time [Runtime: 11 ms; Memory: 48.6 MB]

--- depth-first-search/network-delay-time.java
+++ depth-first-search/network-delay-time.java
@@ -1,21 +1,34 @@
 class Solution {
-    //Bellman-Ford Algorithm
+    public record Node(int val, int wt) {}
+    //Dijkstra's Algorithm
     public int networkDelayTime(int[][] times, int n, int k) {
         int dist[] = new int[n+1];
+        List<Node> adj[] = new List[n+1];
+        for(int i = 0; i < n+1; i++)
+            adj[i] = new ArrayList<>();
+        for(int time[] : times) {
+            adj[time[0]].add(new Node(time[1], time[2]));
+        }
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = dist[0] = 0;
-        for(int i = 1; i < n; i++) {
-            for(int[] time : times) {
-                if(dist[time[0]] != Integer.MAX_VALUE 
-                    && dist[time[0]] + time[2] < dist[time[1]]) {
-                        dist[time[1]] = dist[time[0]] + time[2];
+        // boolean vis[] = new boolean[n+1];
+        PriorityQueue<Node> pq = new PriorityQueue<>(
+            (a, b) -> a.val - b.val
+        );
+        pq.offer(new Node(k, 0));
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            for(Node vtx : adj[node.val]) {
+                if(dist[node.val]!= Integer.MAX_VALUE 
+                        && dist[node.val] + vtx.wt < dist[vtx.val]) {
+                    dist[vtx.val] = dist[node.val] + vtx.wt;
+                    pq.offer(vtx);
                 }
             }
         }
-
         int ans = 0;
         for(int dis : dist) {
-            ans = Math.max(dis, ans);
+            ans = Math.max(ans, dis);
         }
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
