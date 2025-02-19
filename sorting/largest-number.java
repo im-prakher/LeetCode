e3fa2ed625e424f9c073324514a927e04f160559
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-19 06:50:31 +0530

    LC-largest-number [Runtime: 5 ms; Memory: 43.5 MB]

--- sorting/largest-number.java
+++ sorting/largest-number.java
@@ -1,54 +1,18 @@
 class Solution {
-    public long combine(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        for(char ch : a.toCharArray()) {
-            sb.append(ch);
-        }
-        for(char ch : b.toCharArray()) {
-            sb.append(ch);
-        }
-        String str = sb.toString();
-        long num = 0;
-        for(char ch : str.toCharArray()) {
-            num = num * 10 + (int)(ch-'0');
-        }
-        return num;
-    }
     public String largestNumber(int[] nums) {
         StringBuilder s = new StringBuilder();
         int k = 0,n = nums.length; 
         String[] str = new String[n];
         for(int i : nums) {
-            s = new StringBuilder();
-            if(i!=0) {
-                while(i > 0) {
-                    s.insert(0, (char)('0' + (i % 10)));
-                    i = i / 10;
-                }
-            } else
-                s.append("0");
-            str[k++] = s.toString();
+            str[k++] = String.valueOf(i);
         }
-        Arrays.sort(str, (a, b)-> {
-            long p = combine(a, b);
-            long q = combine(b, a);
-            if(p > q)
-                return -1;
-            return 1;
-        });
-
+        Arrays.sort(str, (a, b)-> (b+a).compareTo(a+b));
         StringBuilder ans = new StringBuilder();
-        boolean isZero = true;
+        if(Integer.parseInt(str[0]) == 0)
+            return "0";
         for(int i = 0; i < n; i++) {
             ans.append(str[i]);
-            if(Integer.parseInt(str[i]) != 0)
-                isZero = false;
         }
-        return isZero ? "0" : ans.toString();
-    }
-    public boolean equalsZero(String a, String b) {
-        int p = Integer.parseInt(a);
-        int q = Integer.parseInt(b);
-        return p==0 && p==q;
+        return ans.toString();
     }
 }
