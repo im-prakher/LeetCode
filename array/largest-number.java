d43066b9fb3d7316ac563097321d58586acdc550
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-19 05:56:05 +0530

    LC-largest-number [Runtime: N/A; Memory: N/A]

---
+++ array/largest-number.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        StringBuilder s = new StringBuilder();
+        int k = 0; 
+        String[] str = new String[nums.length];
+        for(int i : nums) {
+            s = new StringBuilder();
+            while(i > 0) {
+                s.insert(0, (char)('0' + (i % 10)));
+                i = i / 10;
+            }
+            str[k++] = s.toString();
+        }
+        Arrays.sort(str, (a, b)-> {
+            int i = 0, j = 0;
+            while(i < a.length() && j < b.length()) {
+                if(a.charAt(i) > b.charAt(j))
+                    return -1;
+                else if(a.charAt(i) < b.charAt(j))
+                    return 1;
+                else {
+                    i++; j++;
+                }
+            }
+            if(i < a.length() && a.charAt(i)!='0')                
+                return -1;
+            return 1;
+        });
+
+        StringBuilder ans = new StringBuilder();
+        for(String n : str) {
+            ans.append(n);
+        }
+        return ans.toString();
+    }
+}
