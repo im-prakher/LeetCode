1ef298bdb548a5511abcbb5ebf274b750d9b5c84
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-19 11:03:46 +0530

    LC-maximum-swap [Runtime: 0 ms; Memory: 40.4 MB]

--- greedy/maximum-swap.java
+++ greedy/maximum-swap.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maximumSwap(int num) {
         char str[] = String.valueOf(num).toCharArray();
-        int n = str.length, mxIdx = n-1, sp = n-1, sq = n-1;
+        int n = str.length, mxIdx = -1, sp = n-1, sq = n-1;
         for(int i = n-1; i >= 0; i--) {
-            if(str[mxIdx] <= str[i])
+            if(mxIdx == -1 || str[mxIdx] < str[i])
                 mxIdx = i;
-            else {
+            else if(str[i] != str[mxIdx]){
                 sp = mxIdx;
                 sq = i;
             }
