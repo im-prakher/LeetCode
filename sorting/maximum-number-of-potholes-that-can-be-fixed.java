df8c9c7ab8da7fe3ce2444ac74b25dfe64c445c3
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-19 07:38:10 +0530

    LC-maximum-number-of-potholes-that-can-be-fixed [Runtime: 79 ms; Memory: 45.7 MB]

--- sorting/maximum-number-of-potholes-that-can-be-fixed.java
+++ sorting/maximum-number-of-potholes-that-can-be-fixed.java
@@ -1,29 +1,23 @@
 class Solution {
     public int maxPotholes(String road, int budget) {
-        List<Integer> list = new ArrayList<>();
+        Queue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
         int j = 0, holes = 0;
         for(char pot : road.toCharArray()) {
             if(pot == 'x')
                holes++;
             else {
                 if(holes!=0)
-                    list.add(holes);
+                    pq.offer(holes);
                 holes = 0;
             }
         }
         if(holes!=0)
-            list.add(holes);
-        list.sort((a, b) -> Integer.compare(b,a));
-        int cost = 0, cnt = 0;
-        for(int i : list) {
-            if(i==0)
-                break;
-            if(i >= budget) {
-                cnt += budget!= 0 ? budget-1 : 0;
-                break;
-            }            
-             cnt += i;
-             budget -= i+1;           
+            pq.offer(holes);
+        int cnt = 0;
+        while(!pq.isEmpty() && budget > 0) {
+            int n = pq.poll();
+            cnt += Math.min(n, budget-1);
+            budget -= Math.min(n+1, budget);
         }
         return cnt;
     }
