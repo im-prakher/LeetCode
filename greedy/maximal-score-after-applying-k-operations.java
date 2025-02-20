5d9475f4ebd1c726426933b575d1547b607b7bf4
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-20 13:36:52 +0530

    LC-maximal-score-after-applying-k-operations [Runtime: 124 ms; Memory: 63.3 MB]

--- greedy/maximal-score-after-applying-k-operations.java
+++ greedy/maximal-score-after-applying-k-operations.java
@@ -1,13 +1,13 @@
 class Solution {
     public long maxKelements(int[] nums, int k) {
-        PriorityQueue<Double> pq = new PriorityQueue<>((a, b) -> Double.compare(b,a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)-> b-a);
         for(int i : nums) 
-            pq.offer((double)i);
+            pq.offer(i);
         long sum = 0;
         while(k-- > 0) {
-            double val = pq.poll();
+            int val = pq.poll();
             sum += val;
-            pq.offer(Math.ceil(val/3));
+            pq.offer((int)Math.ceil(val/3.0));
         }
         return sum;
     }
