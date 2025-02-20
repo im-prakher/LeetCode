b1a6ec382be35ed267bab460cfeba4002302738a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-20 12:18:11 +0530

    LC-maximum-matrix-sum [Runtime: 6 ms; Memory: 55.2 MB]

--- greedy/maximum-matrix-sum.java
+++ greedy/maximum-matrix-sum.java
@@ -1,9 +1,8 @@
 class Solution {
     public long maxMatrixSum(int[][] mat) {
-        int  minus = 0, min = Integer.MAX_VALUE;
+        int neg = 0, min = Integer.MAX_VALUE;
         long sum = 0;
         for(int i = 0; i < mat.length; i++) {
-            int neg = 0;
             for(int j = 0; j < mat[0].length; j++) {
                 if(mat[i][j] < 0) {
                     neg++;
@@ -12,10 +11,8 @@ class Solution {
                 min = Math.min(min, mat[i][j]);
                 sum += mat[i][j];
             }
-            neg = neg % 2;
-            minus += neg;
         }
-        sum -= minus % 2 == 1 ? 2 * min : 0;
+        sum -= neg % 2 == 1 ? 2 * min : 0;
         return sum;
     }
 }
