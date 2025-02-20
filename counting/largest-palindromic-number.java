731fa79cb97bc19e252aa07feb5e84747a708879
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-20 17:44:35 +0530

    LC-largest-palindromic-number [Runtime: N/A; Memory: N/A]

--- counting/largest-palindromic-number.java
+++ counting/largest-palindromic-number.java
@@ -1,6 +1,23 @@
 class Solution {
     public String largestPalindromic(String num) {
         int n = num.length();
-        int freq[] = new int[];
+        int freq[] = new int[10];
+        for(char ch : num.toCharArray()) {
+            freq[ch-'0']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        int idx = 0, j = -1;
+        for(int i =9 ; i >= 0; i--) {
+            int cnt = freq[i] - (freq[i] % 2), c = cnt;
+            freq[i] = freq[i] % 2;              
+            boolean isZero = i!=0 || sb.length() > 0;
+            while(cnt-- > 0 && isZero) {
+                sb.insert(idx, (char)('0' + i));
+            }
+            idx += isZero ? (c / 2) : idx;
+            j = (j==-1 && freq[i] == 1) ? i : j;
+        }
+        sb.insert(idx, (char)('0'+j));
+        return sb.toString();
     }
 }
