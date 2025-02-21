2398d5208586f8e2959e8a80917fdbe348c8f97f
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-21 11:10:40 +0530

    LC-maximum-total-importance-of-roads [Runtime: 7 ms; Memory: 62.3 MB]

--- greedy/maximum-total-importance-of-roads.java
+++ greedy/maximum-total-importance-of-roads.java
@@ -1,24 +1,16 @@
 class Solution {
-    public class Node {
-        int v; long e;
-        Node(int v, long e) {
-            this.v = v;
-            this.e = e;
-        }
-    }
     public long maximumImportance(int n, int[][] roads) {
-        Node[] adj = new Node[n];
-        for(int i =0; i < n; i++)
-            adj[i] = new Node(i,0);
+        int[] freq = new int[n];
+        Arrays.fill(freq, 0);
         for(int road[] : roads) {
-            adj[road[0]].e++;
-            adj[road[1]].e++;
+            freq[road[0]]++;
+            freq[road[1]]++;
         }
-        Arrays.sort(adj, (a,b)-> Long.compare(b.e, a.e));
-        long sum = 0;
-        for(Node node : adj) {
-            sum = sum + node.e * n;
-            n--;
+        Arrays.sort(freq);
+        long sum = 0; int j = 1;
+        for(int i : freq) {
+            sum += (long)i * j;
+            j++;
         }
         return sum;
     }
