7b33ea2dea3f2f66632026075c3accca6a860a89
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-21 11:02:22 +0530

    LC-maximum-total-importance-of-roads [Runtime: N/A; Memory: N/A]

---
+++ heap-priority-queue/maximum-total-importance-of-roads.java
@@ -0,0 +1,25 @@
+class Solution {
+    public class Node {
+        int v, e;
+        Node(int v, int e) {
+            this.v = v;
+            this.e = e;
+        }
+    }
+    public long maximumImportance(int n, int[][] roads) {
+        Node[] adj = new Node[n];
+        for(int i =0; i < n; i++)
+            adj[i] = new Node(i,0);
+        for(int road[] : roads) {
+            adj[road[0]].e++;
+            adj[road[1]].e++;
+        }
+        Arrays.sort(adj, (a,b)-> b.e - a.e);
+        int sum = 0;
+        for(Node node : adj) {
+            sum += node.e * n;
+            n--;
+        }
+        return sum;
+    }
+}
