b8956d1fa6f21fcb1c1fb6d825309d0692c3f5f4
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-21 12:23:48 +0530

    LC-validate-binary-tree-nodes [Runtime: N/A; Memory: N/A]

--- union-find/validate-binary-tree-nodes.java
+++ union-find/validate-binary-tree-nodes.java
@@ -12,13 +12,28 @@ class Solution {
     public boolean validateBinaryTreeNodes(int n, int[] left, int[] right) {
         List<Integer> adj[] = new List[n];
         boolean[] vis = new boolean[n];
+        int[] par = new int[n];
+        Arrays.fill(par, -1);
         for(int i = 0; i < n; i++)
             adj[i] = new ArrayList<>();
         for(int i = 0; i < n ; i++) {
-            if(left[i]!=-1) 
+            if(left[i]!=-1) {
                 adj[i].add(left[i]);
-            if(right[i]!=-1) 
+                par[left[i]] = i;
+            }
+            if(right[i]!=-1) {
                 adj[i].add(right[i]);
+                par[right[i]] = i;
+            }
+        }
+        int start = -1, cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(par[i] == -1) {
+                cnt++;
+                start = i;
+            }
+            if(cnt > 1)
+                return false;
         }
         for(int i = 0; i < n; i++) {
             if(!vis[i] && isCycle(i, vis, adj)) 
