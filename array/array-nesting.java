59aa08e6228d605faea45dd13aa31b187bc1a000
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-02-24 12:20:25 +0530

    LC-array-nesting [Runtime: 4 ms; Memory: 55.2 MB]

--- array/array-nesting.java
+++ array/array-nesting.java
@@ -1,18 +1,21 @@
 class Solution {
-    public int depth(int idx, int[] nums) {
+    public int depth(int idx, int[] nums, boolean[] vis) {
         int depth = 1, strt = idx;
         while(nums[idx] != strt) {
             depth++;
             idx = nums[idx];
+            vis[idx] = true;
         }
         return depth;
     }
     public int arrayNesting(int[] nums) {
         int ans = 0;
+        boolean[] vis = new boolean[nums.length];
         for(int i = 0; i < nums.length; i++) {
             if(ans == nums.length)
                 break;
-            ans = Math.max(ans, depth(i, nums));
+            if(!vis[i])
+                ans = Math.max(ans, depth(i, nums, vis));
         }
         return ans;
     }
