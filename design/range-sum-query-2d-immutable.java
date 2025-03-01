20532d1ddee96cf72b47be1fa95ed460674ef4f8
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-01 12:49:46 +0530

    LC-range-sum-query-2d-immutable [Runtime: N/A; Memory: N/A]

---
+++ design/range-sum-query-2d-immutable.java
@@ -0,0 +1,28 @@
+class NumMatrix {
+    int prefix[][];
+    public NumMatrix(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        prefix = new int[n][m];
+        for(int i = 1; i < n; i++)
+            prefix[i][0] = prefix[i-1][0] + mat[i][0];
+        for(int j = 1; j < m; j++)
+            prefix[0][j] = prefix[0][j-1] + mat[0][j];
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                prefix[i][j] = mat[i][j] + prefix[i][j-1] 
+                                + prefix[i-1][j]- prefix[i-1][j-1];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix[row2][col2] + prefix[row1-1][col1-1]
+                        - prefix[row2][col1-1] - prefix[row1-1][col2];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
