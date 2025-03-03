9739224372171626953774bdabbf99bdf8f9d54b
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-03 22:08:50 +0530

    LC-course-schedule-iv [Runtime: 10 ms; Memory: 49.6 MB]

--- topological-sort/course-schedule-iv.java
+++ topological-sort/course-schedule-iv.java
@@ -1,36 +1,34 @@
 class Solution {
-    public void dfsTopo(int node, boolean vis[], List<Integer> adj[], BitSet prqDp[]) {
-        vis[node] = true;
-        for(int vtx : adj[node]) {
-            prqDp[vtx].set(node);
-            prqDp[vtx].or(prqDp[node]);
-            // if(!vis[vtx])
-                dfsTopo(vtx, vis, adj, prqDp);
-        }
-    }
-
-    public List<Boolean> checkIfPrerequisite(int n, int[][] preq, int[][] queries) {
-        boolean vis[] = new boolean[n];
+    public List<Boolean> checkIfPrerequisite(int n, int[][] preq, int[][] qrys) {
         List<Integer> adj[] = new List[n];
-        BitSet prqDp[] = new BitSet[n];
+        int inDeg[] = new int[n];
+        BitSet preqDb[] = new BitSet[n];
         for(int i = 0; i < n; i++) {
             adj[i] = new ArrayList<>();
-            prqDp[i] = new BitSet(n);
+            preqDb[i] = new BitSet(n);
         }
         for(int[] pre : preq) {
             adj[pre[0]].add(pre[1]);
+            inDeg[pre[1]]++;
         }
-        
-        if(preq.length != 0) {
-            for(int i = 0; i < n; i++) {
-                if(!vis[i])
-                    dfsTopo(i, vis, adj, prqDp);
-            }
+        Queue<Integer> que = new LinkedList<>();
+        for(int i = 0; i < n; i++) {
+            if(inDeg[i] == 0)
+                que.offer(i);
         }
 
+        while(!que.isEmpty()) {
+            int node = que.poll();
+            for(int vtx : adj[node]) {
+                preqDb[vtx].set(node);
+                preqDb[vtx].or(preqDb[node]);
+                if(--inDeg[vtx] == 0)
+                    que.offer(vtx);
+            }
+        }
         List<Boolean> res = new ArrayList<>();
-        for(int[] qry : queries) {
-            res.add(prqDp[qry[1]].get(qry[0]));
+        for(int[] qry : qrys) {
+            res.add(preqDb[qry[1]].get(qry[0]));
         }
         return res;
     }
