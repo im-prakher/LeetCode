e42b9e6b868c49a2233dd50bf6af8f47ba33f251
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-03 20:32:26 +0530

    LC-course-schedule-iv [Runtime: 53 ms; Memory: 48.1 MB]

--- topological-sort/course-schedule-iv.java
+++ topological-sort/course-schedule-iv.java
@@ -1,43 +1,34 @@
 class Solution {
-    public void dfsTopo(int node, boolean vis[], List<Integer> adj[], List<Integer> topo, Set<Integer> set, Set<Integer> qryDb[]) {
-        vis[node] = true;
-        set.add(node);
-        for(int vtx : adj[node]) {
-            qryDb[vtx].addAll(set);
-            // if(!vis[vtx])
-                dfsTopo(vtx, vis, adj, topo, set, qryDb);
-        }
-        topo.add(node);
-    }
-
-    public List<Boolean> checkIfPrerequisite(int n, int[][] preq, int[][] queries) {
-        boolean vis[] = new boolean[n];
+    public List<Boolean> checkIfPrerequisite(int n, int[][] preq, int[][] qrys) {
         List<Integer> adj[] = new List[n];
-        List<Integer> topo = new ArrayList<>();
-        Set<Integer> qryDb[] = new HashSet[n];
+        Map<Integer, Set<Integer>> mp = new HashMap<>();
+        int inDeg[] = new int[n];
         for(int i = 0; i < n; i++) {
             adj[i] = new ArrayList<>();
-            qryDb[i] = new HashSet<>();
+            mp.put(i, new HashSet<>());
         }
         for(int[] pre : preq) {
             adj[pre[0]].add(pre[1]);
+            inDeg[pre[1]]++;
         }
-        Set<Integer> set = new HashSet<>();
-        if(preq.length != 0) {
-            for(int i = 0; i < n; i++) {
-                if(!vis[i])
-                    dfsTopo(i, vis, adj, topo, set, qryDb);
-                set.clear();
-            }
+        Queue<Integer> que = new LinkedList<>();
+        for(int i = 0; i < n; i++) {
+            if(inDeg[i] == 0)
+                que.offer(i);
         }
 
+        while(!que.isEmpty()) {
+            int node = que.poll();
+            for(int vtx : adj[node]) {
+                mp.get(vtx).add(node);
+                mp.get(vtx).addAll(mp.get(node));
+                if(--inDeg[vtx] == 0)
+                    que.offer(vtx);
+            }
+        }
         List<Boolean> res = new ArrayList<>();
-        int k = 0;
-        for(int[] qry : queries) {
-            if(qryDb[qry[1]].contains(qry[0]))
-                res.add(true);
-            else
-                res.add(false);
+        for(int[] qry : qrys) {
+            res.add(mp.get(qry[1]).contains(qry[0]));
         }
         return res;
     }
