9e635870189798e31fa2a6b42789ade97288d45b
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-03 13:13:32 +0530

    LC-find-the-celebrity [Runtime: 17 ms; Memory: 45.8 MB]

--- graph/find-the-celebrity.java
+++ graph/find-the-celebrity.java
@@ -4,18 +4,10 @@
 public class Solution extends Relation {
     public int findCelebrity(int n) {
         int k = 0, celeb = 0;
-        while(k++ < n) { 
-            int j;           
-            for(j = 0; j < n; j++) {
-                if(celeb == j)
-                    continue;
-                if(knows(celeb, j)) {
-                    celeb = j;
-                    break;
-                }
+        for(int j = 0; j < n; j++) {
+            if(knows(celeb, j)) {
+                celeb = j;
             }
-            if(j == n)
-                break;
         }
         int cnt = 0;
         for(int i = 0; i < n; i++) {
