c78a8d59ee33b37af971bd53c4d23659aaf80f0f
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-04 14:05:43 +0530

    LC-design-hit-counter [Runtime: N/A; Memory: N/A]

---
+++ data-stream/design-hit-counter.java
@@ -0,0 +1,32 @@
+class HitCounter {
+    List<Integer> hits;
+    public HitCounter() {
+        hits = new ArrayList<>();
+    }
+    
+    public void hit(int timestamp) {
+        hits.add(timestamp);
+    }
+    
+    public int getHits(int timestamp) {
+        int i = 0, j = hits.size();
+        int time = timestamp - 300;
+        // if(time < hits.get(0))
+        //     return hits.size();
+        while(i < j) {
+            int mid = (i + j) >> 1;
+            if(hits.get(mid) <= time) {
+                i = mid + 1;
+            } else 
+                j = mid - 1;
+        }
+        return hits.size() - i;
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
