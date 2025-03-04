5d680bc116b4cd193b77ffbd4d436608ec89cc65
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-04 06:32:58 +0530

    LC-koko-eating-bananas [Runtime: N/A; Memory: N/A]

---
+++ binary-search/koko-eating-bananas.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canEat(int[] piles, int k, int h) {
+        int time = 0;
+        for(int p : piles) {
+            time += p/k + (p % k == 0 ? 0 : 1);
+        }
+        return time <= h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int lo = piles[0], hi = piles[piles.length-1], k = piles[0];
+        while(lo <= hi) {
+            int mid = (lo + hi) >> 1;
+            if(canEat(piles, mid, h)) { 
+                k = mid;    
+                hi = mid - 1;
+            }
+            else 
+                lo = mid + 1;
+        }
+        return k;
+    }
+}
