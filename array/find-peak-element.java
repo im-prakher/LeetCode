976ff72ba5bc5fb169d2567dd8051ec72879d4a5
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-04 06:01:56 +0530

    LC-find-peak-element [Runtime: 0 ms; Memory: 42.4 MB]

--- array/find-peak-element.java
+++ array/find-peak-element.java
@@ -3,9 +3,10 @@ class Solution {
         int lo = 0, hi = nums.length-1, peak = 0;
         while(lo <= hi) {
             int mid = (lo + hi) >> 1;
-            if(mid!=nums.length-1 && nums[mid] > nums[mid+1]) {
-                peak = mid;
-                hi = mid-1;
+            if(lo == hi)
+                return lo;
+            if(nums[mid] > nums[mid+1]) {
+                hi = mid;
             }
             else
                 lo = mid + 1;
