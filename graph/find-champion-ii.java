26567a30ea4ad560038c6f3dec17779b69c09003
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-05 07:58:31 +0530

    LC-find-champion-ii [Runtime: 1 ms; Memory: 45.7 MB]

--- graph/find-champion-ii.java
+++ graph/find-champion-ii.java
@@ -1,11 +1,7 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
         int[] inDeg = new int[n];
-        List<Integer> adj[] = new List[n];
-        for(int i = 0; i < n; i++) 
-            adj[i] = new ArrayList<>();
         for(int[] edge : edges) {
-            adj[edge[0]].add(edge[1]);
             inDeg[edge[1]]++;
         }
         int start = -1, cnt = 0;
