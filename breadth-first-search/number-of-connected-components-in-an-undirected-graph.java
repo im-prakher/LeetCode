ade9e0ac65dce4ee76492f68375c29f090e7f5c7
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-05 08:15:33 +0530

    LC-number-of-connected-components-in-an-undirected-graph [Runtime: 6 ms; Memory: 46.6 MB]

--- breadth-first-search/number-of-connected-components-in-an-undirected-graph.java
+++ breadth-first-search/number-of-connected-components-in-an-undirected-graph.java
@@ -10,9 +10,9 @@ class Solution {
     public int countComponents(int n, int[][] edges) {
         Map<Integer, List<Integer>> adj = new HashMap<>();
         boolean vis[] = new boolean[n];
+        for(int i =0; i < n; i++)
+            adj.put(i, new ArrayList<>());
         for(int[] edge : edges) {
-            adj.putIfAbsent(edge[0], new ArrayList<>());
-            adj.putIfAbsent(edge[1], new ArrayList<>());
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
