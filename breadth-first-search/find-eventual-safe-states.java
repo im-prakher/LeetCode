1714a81099a395a141624be4f9c1a8f189b117a8
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-05 12:09:41 +0530

    LC-find-eventual-safe-states [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/find-eventual-safe-states.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean cycle(int node, int[] cyc, boolean[] pathVis, int[][] graph) {
+        cyc[node] = 1;
+        pathVis[node] = true;
+        boolean isCycle = false;
+        for(int vtx : graph[node]) {
+            if(cyc[vtx]==0) 
+                isCycle |= cycle(vtx, cyc, pathVis, graph);
+            else if(pathVis[vtx]) {
+                cyc[node] = 2;
+                return true;
+            }
+        }
+        cyc[node] = isCycle ? 2 : 1;
+        pathVis[node] = false;
+        return isCycle;
+    }
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        int cyc[] = new int[n];
+        boolean[] pathVis;
+        for(int i = 0; i < n; i++) {
+            pathVis = new boolean[n];
+            if(cyc[i]==0)
+                cycle(i, cyc, pathVis, graph);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            if(cyc[i] == 1)
+                res.add(i);
+        }
+        return res;
+    }
+}
