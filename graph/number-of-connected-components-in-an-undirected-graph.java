074965d5233dce58ec6624b269a59130d0553c57
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-05 08:14:16 +0530

    LC-number-of-connected-components-in-an-undirected-graph [Runtime: N/A; Memory: N/A]

---
+++ graph/number-of-connected-components-in-an-undirected-graph.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void dfsVis(int node, Map<Integer, List<Integer>> adj, boolean[] vis) {
+        vis[node] = true;
+        for(int vtx : adj.get(node)) {
+            if(!vis[vtx])
+                dfsVis(vtx, adj, vis);
+        }
+    }
+
+    public int countComponents(int n, int[][] edges) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        boolean vis[] = new boolean[n];
+        for(int[] edge : edges) {
+            adj.putIfAbsent(edge[0], new ArrayList<>());
+            adj.putIfAbsent(edge[1], new ArrayList<>());
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        int components = 0;
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                components++;
+                dfsVis(i, adj, vis);
+            }
+        }
+        return components;
+    }
+}
