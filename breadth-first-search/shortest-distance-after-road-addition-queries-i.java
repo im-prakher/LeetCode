dece576f92c73d4ebfbe758129cc2745e24f12bc
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-05 07:01:26 +0530

    LC-shortest-distance-after-road-addition-queries-i [Runtime: 7 ms; Memory: 45.8 MB]

--- breadth-first-search/shortest-distance-after-road-addition-queries-i.java
+++ breadth-first-search/shortest-distance-after-road-addition-queries-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public void dfsReduce(int node, List<Integer> adj[], int[] dist) {
-        for(int vtx : adj[node]) {
-            if(dist[vtx] > dist[node] + 1) {
+        for (int vtx : adj[node]) {
+            if (dist[vtx] > dist[node] + 1) {
                 dist[vtx] = dist[node] + 1;
                 dfsReduce(vtx, adj, dist);
             }
@@ -9,27 +9,24 @@ class Solution {
     }
 
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-     //use topo sort: 0->1->..->n-1
-     //maintain distance array
-     //reduce dist for next nodes after one query in topo sort 
-    List<Integer> adj[] = new List[n];
-    int dist[] = new int[n];
-    for(int i = 0; i < n; i++) {
-        dist[i] = n-i-1;
-        adj[i] = new ArrayList();
-        if(i!=0)
-            adj[i].add(i-1);
-    }
-    int[] ans = new int[queries.length];
-    int k = 0;
-    for(int[] qry : queries) {
-        if(dist[qry[0]] > dist[qry[1]] + 1) {
-            dist[qry[0]] = dist[qry[1]] + 1;
-            dfsReduce(qry[0], adj, dist);
-        }        
-        adj[qry[1]].add(qry[0]);
-        ans[k++] = dist[0];
-    }
-    return ans;
+        List<Integer> adj[] = new List[n];
+        int dist[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            dist[i] = n - i - 1;
+            adj[i] = new ArrayList();
+            if (i != 0)
+                adj[i].add(i - 1);
+        }
+        int[] ans = new int[queries.length];
+        int k = 0;
+        for (int[] qry : queries) {
+            if (dist[qry[0]] > dist[qry[1]] + 1) {
+                dist[qry[0]] = dist[qry[1]] + 1;
+                dfsReduce(qry[0], adj, dist);
+            }
+            adj[qry[1]].add(qry[0]);
+            ans[k++] = dist[0];
+        }
+        return ans;
     }
 }
