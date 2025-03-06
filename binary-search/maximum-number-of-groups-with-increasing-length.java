f11b374a8441819e2d6e8de8113f6212467cbd54
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-06 17:36:35 +0530

    LC-maximum-number-of-groups-with-increasing-length [Runtime: N/A; Memory: N/A]

--- binary-search/maximum-number-of-groups-with-increasing-length.java
+++ binary-search/maximum-number-of-groups-with-increasing-length.java
@@ -1,25 +1,12 @@
 class Solution {
     public int maxIncreasingGroups(List<Integer> limits) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b)-> a[0]!=b[0] ? b[0]-a[0] : b[1]-a[1]);
-        for(int i = 0; i < limits.size(); i++ ) {
-            int[] pair = {limits.get(i), i};
-            pq.offer(pair);
+        limits.sort((a, b)-> b-a);
+        int nx = limits.get(0)-1, i = 0;
+        for(i = 1 ; i < limits.size(); i++) {
+            if(nx == 0)
+                break;
+            nx = Math.min(nx-1, limits.get(i)-1);
         }
-        int n = 1;
-        List<int[]> tmp = new ArrayList<>();
-        while(pq.size() >= n) {
-            int dgts = n;
-            while(dgts-- > 0) {
-                int[] pair = pq.poll();
-                if(--pair[0]!=0)
-                    tmp.add(pair);
-            }
-            for(int[] tmpPairs : tmp) 
-                pq.offer(tmpPairs);
-            tmp = new ArrayList<>();
-            n++;
-        }
-        return n-1;
+        return i;
     }
 }
