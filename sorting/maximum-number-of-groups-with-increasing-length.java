58683f238e5f6fedcbd786c31fb4469f12cb551d
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-06 18:15:18 +0530

    LC-maximum-number-of-groups-with-increasing-length [Runtime: 115 ms; Memory: 56.5 MB]

--- sorting/maximum-number-of-groups-with-increasing-length.java
+++ sorting/maximum-number-of-groups-with-increasing-length.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxIncreasingGroups(List<Integer> limits) {
         limits.sort((a, b)-> a-b);
-        int sum = 0, cnt = 0, n = 1, exp = 1;
+        long sum = 0, n = 0, exp = 1;
         for(int i = 0 ; i < limits.size(); i++) {
             sum += limits.get(i);
-            exp = n * (n + 1) / 2; // sum of (i+1)th natural no.s
+            exp = (n + 1) * (n + 2) / 2; // sum of (i+1)th natural no.s
             if(sum >= exp)
                 n++;
         }
-        return n-1;
+        return (int)n;
     }
 }
