2bf1396295a60a985ea7c2fae9a2472a601aaf0f
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-31 21:06:07 +0530

    LC-separate-black-and-white-balls [Runtime: N/A; Memory: N/A]

--- two-pointers/separate-black-and-white-balls.java
+++ two-pointers/separate-black-and-white-balls.java
@@ -1,6 +1,17 @@
 class Solution {
     public long minimumSteps(String s) {
-        int pre[] = new int[];
-        for(int i = n-1; )
+        int n = s.length(), zero = n-1;
+        int pre[] = new int[n];
+        char str[] = s.toCharArray();
+        for(int i = n-1; i >= 0; i--) {
+            if(str[i] == '1') {
+                pre[i] = zero - i;
+                zero--;
+            }
+        }
+        int ans = 0;
+        for(int i : pre) 
+            ans += i;
+        return ans;
     }
 }
