9d1c4806e0aadce2855fbedc74c6e22ea69dd875
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-31 12:01:16 +0530

    LC-put-marbles-in-bags [Runtime: 34 ms; Memory: 63.2 MB]

--- greedy/put-marbles-in-bags.java
+++ greedy/put-marbles-in-bags.java
@@ -1,8 +1,6 @@
 class Solution {
     public long putMarbles(int[] weights, int k) {
         int n = weights.length;
-        if(n == k || k == 1)
-            return 0;
         long pre[] = new long[n-1];
         for(int i = 0; i < n-1; i++) {
             pre[i] = (long)(weights[i] + weights[i+1]);
