8a091dfd246114bd6dc18661a9c24e76636b1208
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-03-31 21:08:21 +0530

    LC-separate-black-and-white-balls [Runtime: 7 ms; Memory: 45.3 MB]

--- greedy/separate-black-and-white-balls.java
+++ greedy/separate-black-and-white-balls.java
@@ -1,17 +1,14 @@
 class Solution {
     public long minimumSteps(String s) {
         int n = s.length(), zero = n-1;
-        int pre[] = new int[n];
+        long ans = 0;
         for(int i = n-1; i >= 0; i--) {
             char ch = s.charAt(i);
             if(ch == '1') {
-                pre[i] = zero - i;
+                ans += zero - i;
                 zero--;
             }
         }
-        long ans = 0;
-        for(int i : pre) 
-            ans += i;
         return ans;
     }
 }
