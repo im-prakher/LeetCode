06623e03a521fcca3c416ffbac8f346f548e0713
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-01 20:45:16 +0530

    LC-solving-questions-with-brainpower [Runtime: 6 ms; Memory: 107.8 MB]

--- array/solving-questions-with-brainpower.java
+++ array/solving-questions-with-brainpower.java
@@ -1,18 +1,7 @@
 class Solution {
-    long dp[];
-    public long solve(int[][] ques, int idx) {
-        if(idx >= ques.length)
-            return 0;
-        if(dp[idx]!=-1)
-            return dp[idx];
-        long pick = ques[idx][0] + solve(ques, idx + ques[idx][1] + 1);
-        long not_pick = solve(ques, idx + 1);
-        return dp[idx] = Math.max(pick, not_pick);
-    }
-
     public long mostPoints(int[][] questions) {
-        dp = new long[questions.length];
         int n = questions.length;
+        long dp[] = new long[n];
         long not_pick = 0, pick = 0;
         for(int i= n-1; i >= 0; i--) {
             pick = questions[i][0];
