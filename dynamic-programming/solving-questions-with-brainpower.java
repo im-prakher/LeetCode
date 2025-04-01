07d02ccb0c332bb77b9d1136ed9c9c9d57d942e6
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-01 20:48:19 +0530

    LC-solving-questions-with-brainpower [Runtime: 5 ms; Memory: 108.2 MB]

--- dynamic-programming/solving-questions-with-brainpower.java
+++ dynamic-programming/solving-questions-with-brainpower.java
@@ -1,13 +1,14 @@
 class Solution {
     public long mostPoints(int[][] questions) {
         int n = questions.length;
-        long dp[] = new long[n];
+        long dp[] = new long[n+1];
         long not_pick = 0, pick = 0;
-        for(int i= n-1; i >= 0; i--) {
+        dp[n-1] = questions[n-1][0];
+        for(int i= n-2; i >= 0; i--) {
             pick = questions[i][0];
-            if(i + questions[i][1] + 1 < n)
-                pick += dp[i + questions[i][1] + 1];
-            not_pick = i < n-1 ? dp[i + 1] : 0;
+            int idx = i + questions[i][1] + 1;
+            pick += idx < n ? dp[idx] : 0;
+            not_pick = dp[i + 1];
             dp[i] = Math.max(pick, not_pick);
         }
         return dp[0];
