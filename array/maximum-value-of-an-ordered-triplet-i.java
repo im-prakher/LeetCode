fb2a8e9ba9829e98e96b6ef2cb90323a7bd30c48
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-02 23:02:32 +0530

    LC-maximum-value-of-an-ordered-triplet-i [Runtime: N/A; Memory: N/A]

--- array/maximum-value-of-an-ordered-triplet-i.java
+++ array/maximum-value-of-an-ordered-triplet-i.java
@@ -1,15 +1,13 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
+        long min = nums[1], max = Math.max(nums[0], nums[1]), diff = Math.max(0, max - min), ans= 0;
         int n = nums.length;
-        long pre_max[] = new long[n];
-        long suf_max[] = new long[n];
-        long ans = 0;
-        for(int i = 1; i < n-1; i++) {
-            pre_max[i] = Math.max(nums[i-1], pre_max[i-1]);
-            suf_max[n-i-1] = Math.max(nums[n-i], suf_max[n-i]);
+        for(int i = 2; i < n; i++) {
+            ans = Math.max(ans, diff * nums[i]);
+            diff = Math.max(diff, max - min);
+            min = Math.min(nums[i], min);
+            max = Math.max(nums[i-1], max);
         }
-        for(int i= 1; i < n-1; i++) 
-            ans = Math.max(ans, (pre_max[i] - nums[i]) * suf_max[i]);
         return ans < 0 ? 0 : ans;
     }
 }
