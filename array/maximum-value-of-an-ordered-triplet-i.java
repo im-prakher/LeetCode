39024c567ea8b6b9f8ac952938d2af6ad3d8e132
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-02 22:51:24 +0530

    LC-maximum-value-of-an-ordered-triplet-i [Runtime: N/A; Memory: N/A]

--- array/maximum-value-of-an-ordered-triplet-i.java
+++ array/maximum-value-of-an-ordered-triplet-i.java
@@ -2,16 +2,14 @@ class Solution {
     public long maximumTripletValue(int[] nums) {
         int n = nums.length;
         int pre_max[] = new int[n];
-        int pre_min[] = new int[n];
-        Arrays.fill(pre_min, Integer.MAX_VALUE);
+        int suf_max[] = new int[n];
         long ans = 0;
         for(int i = 1; i < n; i++) {
             pre_max[i] = Math.max(nums[i-1], pre_max[i-1]);
-            if(i < n-1)
-                pre_min[i+1] = Math.min(nums[i], pre_min[i]);
-            if(i >= 2)
-                ans = (pre_max[i] - pre_min[i]) * nums[i];
+            suf_max[n-i-1] = Math.max(nums[n-i], suf_max[n-i]);
         }
+        for(int i= 1; i < n; i++) 
+            ans = Math.max(ans, (pre_max[i] - nums[i]) * suf_max[i]);
         return ans < 0 ? 0 : ans;
     }
     // Do i need to create new operation for this?
