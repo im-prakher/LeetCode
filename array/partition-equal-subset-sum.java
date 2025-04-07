257200c6391ebfccaa20750551d172cabf704317
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-07 10:01:09 +0530

    LC-partition-equal-subset-sum [Runtime: 33 ms; Memory: 41.9 MB]

--- array/partition-equal-subset-sum.java
+++ array/partition-equal-subset-sum.java
@@ -1,11 +1,4 @@
 class Solution {
-    // solve() {
-    //     if(tg == 0)
-    //         true;
-    //     if(nums[i] <= tg)
-    //         solve(i-1, tg-nums[i]);
-    //     solve(i-1, tg);
-    // }
     public boolean canPartition(int[] nums) {
         int sum = 0;
         for(int i : nums)
@@ -16,9 +9,8 @@ class Solution {
         boolean dp[] = new boolean[target+1];
         dp[0] = true;
         for(int i = 0; i < n; i++) {
-            for(int j = nums[i]; j <= target; j++) {
-                // if(nums[i] <= j)
-                    dp[j] = dp[j] || dp[j-nums[i]];
+            for(int j = target; j >= nums[i]; j--) {
+                dp[j] = dp[j] || dp[j-nums[i]];
             }
         }
         return dp[target];
