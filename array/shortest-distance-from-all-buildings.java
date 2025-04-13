76d3192da040c4c82a8902c73fc7599bb56605f8
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-13 18:28:04 +0530

    LC-shortest-distance-from-all-buildings [Runtime: N/A; Memory: N/A]

--- array/shortest-distance-from-all-buildings.java
+++ array/shortest-distance-from-all-buildings.java
@@ -6,11 +6,12 @@ class Solution {
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
         boolean[][] vis = new boolean[n][m];
+        vis[i][j] = true;
         while(!que.isEmpty()) {
             int[] node =  que.poll();
             for(int k = 0; k < 4; k++) {
                 int p = node[0] + dx[k], q = node[1] + dy[k];
-                if(p >= 0 && p < n && q >= 0 && q < m && grid[p][q] == 0 && !vis[i][j] && dis[p * m + q] > node[2]+1) {
+                if(p >= 0 && p < n && q >= 0 && q < m && grid[p][q] == 0 && !vis[p][q] && dis[p * m + q] > node[2]+1) {
                     dis[p * m + q] = node[2] + 1;
             vis[p][q] = true;
                     que.offer(new int[]{p, q, node[2]+1});
