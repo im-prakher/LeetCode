6c68f5dec266e7afd33c58ea95d04fa479a5e44a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-15 23:58:54 +0530

    LC-continuous-subarrays [Runtime: 28 ms; Memory: 57.7 MB]

--- heap-priority-queue/continuous-subarrays.java
+++ heap-priority-queue/continuous-subarrays.java
@@ -1,16 +1,15 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
         long cnt = 0;
-        int mx = 0, mn = 0;
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
         for(int i = 0, j = 0; i < nums.length; i++) {
-            if(Math.abs(nums[mx]-nums[i]) > 2 
-                    || Math.abs(nums[mn]-nums[i]) > 2) {
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+            while(mp.lastKey() - mp.firstKey() > 2) {
+                mp.put(nums[j], mp.get(nums[j]) - 1);
+                if(mp.get(nums[j]) == 0)
+                    mp.remove(nums[j]);
                j++;   
-               mx = mx < j ? j : mx;
-               mn = mn < j ? j : mn;
             }
-            mx = nums[mx] < nums[i] ? i : mx;
-            mn = nums[mn] > nums[i] ? i : mn;
             cnt += i - j + 1;
         }
         return cnt;
