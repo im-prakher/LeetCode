6643154125789b26a97cb9010ce172b0676b4d48
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-16 21:23:46 +0530

    LC-maximum-sum-of-3-non-overlapping-subarrays [Runtime: N/A; Memory: N/A]

--- dynamic-programming/maximum-sum-of-3-non-overlapping-subarrays.java
+++ dynamic-programming/maximum-sum-of-3-non-overlapping-subarrays.java
@@ -1,7 +1,7 @@
 class Solution {
-    Map<Integer, int[]> mp;
+    
     int max;
-    void insert(int sum, int[] ans) {
+    void insert(int sum, int[] ans, Map<Integer, int[]> mp) {
         boolean flag = true;
         if(mp.containsKey(sum)) {
             for(int i = 0; i < 3; i++) 
@@ -12,27 +12,27 @@ class Solution {
             mp.put(sum, nums);
         }
     }
-    public int triplets(List<Integer> list, int[] nums, int c, int k, int[] ans, int i, int sum) {
+    public int triplets(List<Integer> list, int[] nums, int c, int k, int[] ans, int i, int sum, Map<Integer, int[]> mp) {
         if(c == 0) {
             if(sum >= max) {
-                insert(sum, ans);
+                insert(sum, ans, mp);
                 max = sum;
             }
             return max;
         }
         if(i < 0)
             return Integer.MIN_VALUE;
-        ans[c-1] = i-1;
-        int pick = triplets(list, nums, c-1, k, ans, i-k, sum + nums[i]);
+        ans[c-1] = i;
+        int pick = triplets(list, nums, c-1, k, ans, i-k, sum + list.get(i), mp);
         ans[c-1] = nums.length;
-        int not_pick = triplets(list, nums, c, k, ans, i-1,  sum);
+        int not_pick = triplets(list, nums, c, k, ans, i-1,  sum, mp);
         return Math.max(pick, not_pick);
     }
 
     public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
         List<Integer> list = new ArrayList<>();
         max = Integer.MIN_VALUE;
-        mp = new HashMap<>();
+        Map<Integer, int[]> mp = new HashMap<>();
         int sum =0 ;
         for(int i = 0; i < nums.length; i++) {
             sum += nums[i];
@@ -42,7 +42,7 @@ class Solution {
         }
         int[] ans = new int[3];
         Arrays.fill(ans, list.size());
-        int val = triplets(list, nums, 3, k, ans, list.size() - 1, 0);
+        int val = triplets(list, nums, 3, k, ans, list.size() - 1, 0, mp);
         return mp.get(val);
     }
 }
