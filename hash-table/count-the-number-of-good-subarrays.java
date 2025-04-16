e306df8490ed5f1184cd13278a9c9ab19e5f0e3a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-16 09:32:51 +0530

    LC-count-the-number-of-good-subarrays [Runtime: 57 ms; Memory: 58.1 MB]

--- hash-table/count-the-number-of-good-subarrays.java
+++ hash-table/count-the-number-of-good-subarrays.java
@@ -6,19 +6,15 @@ class Solution {
         for(int i = 0, j = 0; i < nums.length; i++) {
             pairs += map.getOrDefault(nums[i], 0);
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if(pairs >= k)
-                cnt += join++;
             while(pairs >= k) {
-                cnt++;
                 map.put(nums[j], map.getOrDefault(nums[j], 1) - 1);
                 pairs -= map.get(nums[j]);
                 if(map.get(nums[j]) == 0)
                     map.remove(nums[j]);
                 j++;
             }
+            cnt += j;
         }
-        int j = 0;
-        
         return cnt;
     }
 }
