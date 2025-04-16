5780c9a4ddbed6bd639ba47781b72e4711513134
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-16 09:05:20 +0530

    LC-count-the-number-of-good-subarrays [Runtime: N/A; Memory: N/A]

--- sliding-window/count-the-number-of-good-subarrays.java
+++ sliding-window/count-the-number-of-good-subarrays.java
@@ -2,15 +2,15 @@ class Solution {
     public long countGood(int[] nums, int k) {
         long cnt = 0, pairs = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        int join = -1;
+        int join = 0;
         for(int i = 0, j = 0; i < nums.length; i++) {
             pairs += map.getOrDefault(nums[i], 0);
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if(pairs == k)
-                cnt += ++join;
+            if(pairs >= k)
+                cnt += join++;
             while(pairs >= k) {
                 cnt++;
-                map.put(nums[j], map.getOrDefault(nums[j], 0) - 1);
+                map.put(nums[j], map.getOrDefault(nums[j], 1) - 1);
                 pairs -= map.get(nums[j]);
                 if(map.get(nums[j]) == 0)
                     map.remove(nums[j]);
