8819e6aff3d17fe035ad41eed2266827216b976e
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-19 14:28:31 +0530

    LC-count-the-number-of-fair-pairs [Runtime: 24 ms; Memory: 57.1 MB]

--- binary-search/count-the-number-of-fair-pairs.java
+++ binary-search/count-the-number-of-fair-pairs.java
@@ -1,21 +1,21 @@
 class Solution {
-    //get cnt of elements less than val
-    public long lower_bound(int[] nums, int val) {
+    public long lower_bound_pairs(int[] nums, int val) {
         long cnt = 0;
-        int i = 0, j = nums.length-1;
+        int i = 0, j = nums.length - 1;
         while(i < j) {
             int sum = nums[i] + nums[j];
-            if(sum < val) {
-                cnt += j-i;
+            if(sum >= val) {
+                cnt += j - i;
+                j--;
+            } else {
                 i++;
-            } else
-                j--;            
+            }
         }
         return cnt;
     }
 
     public long countFairPairs(int[] nums, int lower, int upper) {
         Arrays.sort(nums);
-        return lower_bound(nums, upper+1) - lower_bound(nums, lower);
+        return lower_bound_pairs(nums, lower) - lower_bound_pairs(nums, upper + 1);
     }
 }
