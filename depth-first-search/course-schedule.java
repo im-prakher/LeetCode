c992e7de387dfacdc6240ab93f217f23c611ec0a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-19 16:21:59 +0530

    LC-course-schedule [Runtime: 5 ms; Memory: 45.3 MB]

---
+++ depth-first-search/course-schedule.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canFinish(int num, int[][] prereq) {
+        List<Integer> adj[] = new List[num];
+        for(int i = 0; i < num; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        int[] inDeg = new int[num];
+        for(int[] pre : prereq) {
+            adj[pre[0]].add(pre[1]);
+            inDeg[pre[1]]++;
+        }
+
+        Queue<Integer> que = new LinkedList<>();
+        for(int i = 0; i < num; i++) {
+            if(inDeg[i] == 0)
+                que.offer(i);
+        }
+
+        while(!que.isEmpty()) {
+            int node = que.poll();
+            for(int vtx : adj[node]) {
+                if(--inDeg[vtx] == 0)
+                    que.offer(vtx);
+            }
+        }
+        for(int i = 0; i < num; i++) {
+            if(inDeg[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
