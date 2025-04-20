4e84a33b386de49c6961937e14618dc4c728c5b7
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-21 05:19:43 +0530

    LC-satisfiability-of-equality-equations [Runtime: N/A; Memory: N/A]

--- union-find/satisfiability-of-equality-equations.java
+++ union-find/satisfiability-of-equality-equations.java
@@ -1,8 +1,29 @@
 class Solution {
-    public boolean equationsPossible(String[] equations) {
-        
-        for(String eqn[] : equations) {
+    char node[];
+    void union(char a, char b) {
+        node[a-'a'] = node[b-'a'];
+    }
 
+    char find(char a) {
+        if(a == node[a-'a'])
+            return a;
+        return node[a-'a'] = find(node[a-'a']);
+    }
+    public boolean equationsPossible(String[] equations) {
+        node = new char[26];
+        for(int i = 0; i < 26; i++) {
+            node[i] = (char)('a' + i);
         }
+        for(String eqtn : equations) {
+            char[] eqn = eqtn.toCharArray();
+            char u = find(eqn[0]), v = find(eqn[3]);
+            if(eqn[1] == eqn[2]){
+                if(u != v) 
+                    union(u, v);
+            } else if(u == v){
+                return false;
+            }            
+        }
+        return true;
     }
 }
