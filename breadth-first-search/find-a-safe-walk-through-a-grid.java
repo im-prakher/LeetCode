946e06e3c1c88cd85b3f24d43556baa4bd9f9a46
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-21 05:58:09 +0530

    LC-find-a-safe-walk-through-a-grid [Runtime: 95 ms; Memory: 52.7 MB]

--- breadth-first-search/find-a-safe-walk-through-a-grid.java
+++ breadth-first-search/find-a-safe-walk-through-a-grid.java
@@ -1,19 +1,21 @@
 class Solution {
     Map<String, Boolean> memo;
-    boolean[][] vis;
+    int[][] vis;
     public boolean traverse(List<List<Integer>> grid, int health, int i, int j) {
-        if(i== 0 && j == 0) 
-            return health > 0;
+        
         if(i < 0 || i >= grid.size() ||
-                 j < 0 || j >= grid.get(0).size() || health <= 0)
+                 j < 0 || j >= grid.get(0).size())
             return false;
-        if(vis[i][j])
+        health -= grid.get(i).get(j);
+        if (health <= 0) return false;
+        if(i== 0 && j == 0) 
+            return true;
+        if(vis[i][j] >= health)
             return false;
-        vis[i][j] = true;
+        vis[i][j] = health;
         String key = i + "," + j + "," + health;
         if(memo.containsKey(key))
             return memo.get(key);
-        health -= grid.get(i).get(j);
         boolean res =  traverse(grid, health , i-1, j)
                 || traverse(grid, health, i, j-1) 
                 || traverse(grid, health, i+1, j) 
@@ -24,7 +26,7 @@ class Solution {
     }
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
         memo = new HashMap<>();
-        vis = new boolean[grid.size()][grid.get(0).size()];
+        vis = new int[grid.size()][grid.get(0).size()];
         return traverse(grid, health, grid.size() -1 , grid.get(0).size() - 1);
     }
 }
