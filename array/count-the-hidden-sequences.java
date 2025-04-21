1967f0a174f6b6404537b88cd6e78571ae94a873
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-21 12:23:07 +0530

    LC-count-the-hidden-sequences [Runtime: 4 ms; Memory: 62 MB]

--- array/count-the-hidden-sequences.java
+++ array/count-the-hidden-sequences.java
@@ -1,11 +1,13 @@
 class Solution {
     public int numberOfArrays(int[] diff, int lower, int upper) {
-        int min = diff[0], max = diff[0], sum = 0;
+        int min = 0, max = 0, sum = 0;
         for(int i = 0; i < diff.length; i++) {
             sum += diff[i];
             min = Math.min(min, sum);
             max = Math.max(max, sum);
+            if(max - min > upper-lower)
+                return 0;
         }
-        return Math.max(0, (upper - max) - (lower - min) + 1);
+        return (upper - max) - (lower - min) + 1;
     }
 }
