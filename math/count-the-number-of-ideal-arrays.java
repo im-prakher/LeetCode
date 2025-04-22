8b69cff1c6a75c21fa614931ccf839fd8744a5c4
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-22 17:31:04 +0530

    LC-count-the-number-of-ideal-arrays [Runtime: N/A; Memory: N/A]

--- math/count-the-number-of-ideal-arrays.java
+++ math/count-the-number-of-ideal-arrays.java
@@ -1,27 +1,22 @@
 class Solution {
-    int dp[][];
-    int func(int n, int maxValue, int prev) {
-        if(dp[n][prev] != -1)
-            return dp[n][prev];
+    long dp[][];
+    int MOD = 1000_000_007;
+    long func(int n, int maxValue, int prev) {
         if(n ==0)
             return 1;
-        int ans = 0;
+        if(dp[n][prev] != -1)
+            return dp[n][prev];
+        long ans = 0;
         for(int i = prev; i <= maxValue; i++) {
             if(i % prev == 0)
                 ans += func(n-1, maxValue, i);
         }
-        return dp[n][prev] = ans;
+        return dp[n][prev] = ans % MOD;
     }
     public int idealArrays(int n, int maxValue) {
-        int ans = 0;
-        dp = new int[n+1][maxValue+1];
-        for(int[] row : dp)
+        dp = new long[n+1][maxValue+1];
+        for(long[] row : dp)
             Arrays.fill(row, -1);
-        // for(int num = 1; num <= maxValue; num++) {
-        //     int mul = maxValue / num ;
-        //     ans += (mul- 1) * (n-1) + 1 ;
-        // }
-        // return dp[n][maxValue];
-        return func(n, maxValue, 1);
+        return (int)func(n, maxValue, 1);
     }
 }
