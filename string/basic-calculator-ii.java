4a138e9000d89340fd82948290b1280cbc3631ae
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-22 12:02:24 +0530

    LC-basic-calculator-ii [Runtime: 10 ms; Memory: 46.7 MB]

--- string/basic-calculator-ii.java
+++ string/basic-calculator-ii.java
@@ -7,13 +7,28 @@ class Solution {
         return op == '-' ? -1 : 1;
     }
 
+    public boolean isDig(char ch) {
+        return ch >= '0' && ch <='9';
+    }
+
+    public int[] getDigit(char[] str, int idx) {
+        int dig = 0;
+        while(idx < str.length && isDig(str[idx])) {
+            dig = dig * 10 + (str[idx] - '0');
+            idx++;
+        }
+        return new int[] {dig, idx};
+    }
+
     public int calculate(String s) {
         s = s.replace(" ", "");
         char[] str = s.toCharArray();
-        int sum = str[0] - '0', sign = 1, eval = 0;
-        for(int i = 1; i < str.length-1; i+=2) {
+        int[] digit = getDigit(str, 0);
+        int sum = digit[0], sign = 1, eval = 0;
+        for(int i = digit[1]; i < str.length; i = digit[1]) {
             char op = str[i];
-            int dig = str[i+1]-'0';
+            digit = getDigit(str, i+1);
+            int dig = digit[0];
             if(isAddSub(op)) {
                 eval += sign * sum;
                 sign = getSign(op);
