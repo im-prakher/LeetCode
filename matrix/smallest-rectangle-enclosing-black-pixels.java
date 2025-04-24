d765ff5924dcc9895bf3538c3e9da0c7da8b936a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-24 07:26:17 +0530

    LC-smallest-rectangle-enclosing-black-pixels [Runtime: 0 ms; Memory: 45 MB]

--- matrix/smallest-rectangle-enclosing-black-pixels.java
+++ matrix/smallest-rectangle-enclosing-black-pixels.java
@@ -1,27 +1,32 @@
-class Solution {
-    public int minArea(char[][] image, int a, int b) {
-        if(image.length == 0 || image[0].length == 0) 
-            return 0;
-        int minh = a, maxh = a, miny = b, maxy = b;
-        Queue<int[]> que = new LinkedList<>();
-        que.offer(new int[] {a, b});
-        int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
-        image[a][b] = '0';
-        while(!que.isEmpty()) {
-            int[] loc = que.poll();
-            maxh = Math.max(maxh, loc[0]);
-            minh = Math.min(minh, loc[0]);
-            maxy = Math.max(maxy, loc[1]);
-            miny = Math.min(miny, loc[1]);
-            for(int i = 0; i < 4; i++) {
-                int x = loc[0] + dx[i], y = loc[1] + dy[i];
-                if(x >= 0 && x < image.length && y >=0 && y < image[0].length 
-                    && image[x][y] == '1') {
-                        image[x][y] = '0';
-                        que.offer(new int[] {x, y});
-                    }
-            }
+public class Solution {
+    public int minArea(char[][] image, int x, int y) {
+        int m = image.length, n = image[0].length;
+        int left = searchColumns(image, 0, y, 0, m, true);
+        int right = searchColumns(image, y + 1, n, 0, m, false);
+        int top = searchRows(image, 0, x, left, right, true);
+        int bottom = searchRows(image, x + 1, m, left, right, false);
+        return (right - left) * (bottom - top);
+    }
+    private int searchColumns(char[][] image, int i, int j, int top, int bottom, boolean whiteToBlack) {
+        while (i != j) {
+            int k = top, mid = (i + j) / 2;
+            while (k < bottom && image[k][mid] == '0') ++k;
+            if (k < bottom == whiteToBlack) // k < bottom means the column mid has black pixel
+                j = mid; //search the boundary in the smaller half
+            else
+                i = mid + 1; //search the boundary in the greater half
         }
-        return (maxh - minh + 1) * (maxy - miny + 1);
+        return i;
+    }
+    private int searchRows(char[][] image, int i, int j, int left, int right, boolean whiteToBlack) {
+        while (i != j) {
+            int k = left, mid = (i + j) / 2;
+            while (k < right && image[mid][k] == '0') ++k;
+            if (k < right == whiteToBlack) // k < right means the row mid has black pixel
+                j = mid;
+            else
+                i = mid + 1;
+        }
+        return i;
     }
 }
