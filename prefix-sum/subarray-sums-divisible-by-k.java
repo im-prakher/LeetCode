629558049ed84f2d26ec3aacec5d2fffdafc1587
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-25 18:01:08 +0530

    LC-subarray-sums-divisible-by-k [Runtime: 28 ms; Memory: 47.3 MB]

--- prefix-sum/subarray-sums-divisible-by-k.java
+++ prefix-sum/subarray-sums-divisible-by-k.java
@@ -4,12 +4,11 @@ class Solution {
         int sum = 0, cnt = 0;
         for(int i : nums) {
             sum += i;
-            if(mp.containsKey(sum % k))
-                cnt += mp.get(sum % k);
+            if(mp.containsKey((sum % k + k) % k))
+                cnt += mp.get((sum % k + k) % k);
             if(sum % k == 0)
                 cnt++;
             mp.put((sum % k + k) % k, mp.getOrDefault((sum % k + k) % k, 0) + 1);
-            // mp.put(sum, mp.getOrDefault(sum, 0) + 1);
         }
         return cnt;
     }
