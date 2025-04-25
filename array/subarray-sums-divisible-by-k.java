94f1c73af9787a44db9d45d1079cba87c64ca020
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-25 17:47:19 +0530

    LC-subarray-sums-divisible-by-k [Runtime: N/A; Memory: N/A]

---
+++ array/subarray-sums-divisible-by-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        int sum = 0, cnt = 0;
+        for(int i : nums) {
+            sum += i;
+            if(mp.containsKey(sum % k))
+                cnt += mp.get(sum % k);
+            if(sum % k == 0)
+                cnt++;
+            mp.put(sum, mp.getOrDefault(sum % k, 0) + 1);
+            mp.put(sum, mp.getOrDefault(sum, 0) + 1);
+        }
+        return cnt;
+    }
+}
