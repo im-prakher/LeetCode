77ff323335629832fbbc057901d2d353cf7f69f0
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-27 11:50:09 +0530

    LC-count-subarrays-of-length-three-with-a-condition [Runtime: 2 ms; Memory: 44.9 MB]

--- array/count-subarrays-of-length-three-with-a-condition.java
+++ array/count-subarrays-of-length-three-with-a-condition.java
@@ -2,7 +2,7 @@ class Solution {
     public int countSubarrays(int[] nums) {
         int cnt = 0;
         for(int i = 0; i < nums.length-2; i++) {
-            if(nums[i] + nums[i+2] == nums[i+1] / 2)
+            if(nums[i+1] % 2 == 0 && nums[i] + nums[i+2] == nums[i+1] / 2)
                 cnt++;
         }
         return cnt;
