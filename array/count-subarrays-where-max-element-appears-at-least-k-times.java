b4e584e9361f64db5ad2ebb699c2ce3dab3255da
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-04-29 10:09:59 +0530

    LC-count-subarrays-where-max-element-appears-at-least-k-times [Runtime: 10 ms; Memory: 66.1 MB]

--- array/count-subarrays-where-max-element-appears-at-least-k-times.java
+++ array/count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -2,9 +2,9 @@ class Solution {
     public long countSubarrays(int[] nums, int k) {
         long ans = 0; int n = nums.length;
         int max = Arrays.stream(nums).max().orElse(0);
-        long cnt = Arrays.stream(nums).filter(i->i==max).count();
-        if(cnt < k)
-            return 0;
+        // long cnt = Arrays.stream(nums).filter(i->i==max).count();
+        // if(cnt < k)
+        //     return 0;
         long maxFreq= 0;
         for(int i = 0, j = 0; i < n; i++) {
             maxFreq += nums[i] == max ? 1 : 0;
