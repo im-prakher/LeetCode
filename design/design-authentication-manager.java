d721d7599f4a6857ff73f3fee9f7b5e75f7d53f7
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-01 14:30:32 +0530

    LC-design-authentication-manager [Runtime: 55 ms; Memory: 46.6 MB]

---
+++ design/design-authentication-manager.java
@@ -0,0 +1,45 @@
+class AuthenticationManager {
+    TreeSet<Integer> set;
+    HashMap<String, Integer> tokenMap;
+    int alive;
+    public AuthenticationManager(int timeToLive) {
+        set = new TreeSet<>();
+        tokenMap = new HashMap<>();
+        alive = timeToLive;
+    }
+    
+    public void generate(String tokenId, int currentTime) {
+        int expiry = currentTime + alive;
+        tokenMap.put(tokenId, expiry);
+        set.add(expiry);
+    }
+    
+    public void renew(String tokenId, int currentTime) {
+        if(tokenMap.containsKey(tokenId)) {
+            if(tokenMap.get(tokenId) > currentTime) {
+                int expiry = currentTime + alive;
+                set.remove(tokenMap.get(tokenId));
+                tokenMap.put(tokenId, expiry);
+                set.add(expiry);
+            }
+        }
+    }
+    
+    public int countUnexpiredTokens(int currentTime) {
+        int cnt = 0;
+        for(int num : set) {
+            cnt++;
+            if(num > currentTime)
+                break;
+        }
+        return set.tailSet(currentTime, false).size();
+    }
+}
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager obj = new AuthenticationManager(timeToLive);
+ * obj.generate(tokenId,currentTime);
+ * obj.renew(tokenId,currentTime);
+ * int param_3 = obj.countUnexpiredTokens(currentTime);
+ */
