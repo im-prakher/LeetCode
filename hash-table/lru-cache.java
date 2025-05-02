59a3f8190c020e30cfe466664c27a89819578b3e
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-02 22:25:08 +0530

    LC-lru-cache [Runtime: 52 ms; Memory: 116.2 MB]

--- hash-table/lru-cache.java
+++ hash-table/lru-cache.java
@@ -1,9 +1,11 @@
 class LRUCache {
     class Node {
         int val;
+        int key;
         Node prev;
         Node next;
-        Node(int _val) {
+        Node(int _key, int _val) {
+            key = _key;
             val = _val;
         }
     }
@@ -15,47 +17,47 @@ class LRUCache {
     public LRUCache(int capacity) {
         size = capacity;
         map = new HashMap<>();
-        head = new Node(-1);
-        tail = new Node(-99);
+        head = new Node(-1,-1);
+        tail = new Node(-99,-99);
         head.next = tail;
         tail.prev = head;
     }
     
-    public void addLRU(int key, int val) {
-        Node node = new Node(val);
+    public void addList(Node node) {
         node.prev = head;
         node.next = head.next;
-        head.next.prev = node;
+        node.next.prev = node;
         head.next = node;
-        map.put(key, node);
-        size--;
     }
 
-    public int delLRU(int key) {
-        Node node = map.get(key);
+    public void delList(Node node) {
         node.next.prev = node.prev;
         node.prev.next = node.next;
-        map.remove(key);
-        size++;
-        return node.val;
     }
     
     public int get(int key) {
         if(map.containsKey(key)) {
-            int val = delLRU(key);
-            addLRU(key, val);
-            return val;
+            Node node = map.get(key);
+            delList(node);
+            addList(node);
+            return node.val;
         }
         return -1;
     }
     
     public void put(int key, int value) {
-        if(size == 0) {
-            delLRU(tail.prev.val);
-        } else if(map.containsKey(key)) {
-            delLRU(key);
-        }
-        addLRU(key, value);
+        if(map.containsKey(key)) {
+            delList(map.get(key));
+            size++;
+        } else if(size == 0) {
+            map.remove(tail.prev.key);
+            delList(tail.prev);
+            size++;
+        } 
+        Node node = new Node(key, value);
+        addList(node);
+        map.put(key, node);
+        size--;
     }
 }
 
