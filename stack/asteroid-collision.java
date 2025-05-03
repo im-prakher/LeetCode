d29c577727dd082d86c80fc851d653549f578289
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-03 12:41:37 +0530

    LC-asteroid-collision [Runtime: N/A; Memory: N/A]

---
+++ stack/asteroid-collision.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isCollision(int a, int b) {
+        return a * b < 0;
+    }
+    public int abs(int n) {
+        return Math.abs(n);
+    }
+    public int[] asteroidCollision(int[] asteroids) {
+        int top = -1, n = asteroids.length; boolean isAlive;
+        int stack[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            while(top!= -1 && isCollision(asteroids[stack[top]], asteroids[i]) 
+                    && abs(asteroids[i]) > abs(asteroids[stack[top]])) {
+                top--;
+            }
+            if(top != -1) {
+                if(!isCollision(asteroids[stack[top]], asteroids[i]))
+                    stack[++top] = i;
+                else if(abs(asteroids[i]) == abs(asteroids[stack[top]]))
+                    top--;
+            } else {
+                stack[++top] = i;;
+            }
+
+        }
+        int[] res = new int[top+1];
+        while(top >= 0) 
+            res[top] = asteroids[stack[top--]];
+        return res;
+    }
+}
