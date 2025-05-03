d64e08b18e5c992bc02234121451b675ea00411c
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-03 21:23:38 +0530

    LC-boundary-of-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/boundary-of-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> travel;
+    void addLeftBoundary(TreeNode root) {
+        if(root == null || (root.left == null && root.right == null))
+            return;
+        travel.add(root.val);
+        if(root.left != null)
+            addLeftBoundary(root.left);
+        else
+            addLeftBoundary(root.right);
+    }
+
+    void addLeafNodes(TreeNode root) {
+         if(root == null)
+            return;
+         if(root.left == null && root.right == null) {
+            travel.add(root.val);
+            return;
+         }
+         addLeafNodes(root.left);
+         addLeafNodes(root.right);
+    }
+
+    void addRightBoundary(TreeNode root) {
+        if(root == null || (root.left == null && root.right == null))
+            return;
+        if(root.left != null)
+            addRightBoundary(root.left);
+        else
+            addRightBoundary(root.right);
+        travel.add(root.val);
+    }
+
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        // return new ArrayList<>();
+        travel = new ArrayList<>();
+        travel.add(root.val);
+        addLeftBoundary(root.left);
+        addLeafNodes(root);
+        addRightBoundary(root.right);
+        return travel;
+    }
+}
