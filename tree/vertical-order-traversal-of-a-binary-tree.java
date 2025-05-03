398ac384a6e941122cf38d53b4f776fea116dadd
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-03 23:18:23 +0530

    LC-vertical-order-traversal-of-a-binary-tree [Runtime: 4 ms; Memory: 42.8 MB]

--- tree/vertical-order-traversal-of-a-binary-tree.java
+++ tree/vertical-order-traversal-of-a-binary-tree.java
@@ -15,34 +15,33 @@
  */
 class Solution {
     static class Pair implements Comparable<Pair>{
-    int lvl;
-    int val;
-    Pair(){
-        lvl = val =0;
-    }
-    Pair(int lvl, int val){
-        this.lvl = lvl;
-        this.val =val;
-    }
-    
-    @Override
-    public int compareTo(Pair p) {
-        return this.val - p.val;
-    }
-    
+        int lvl;
+        int val;
+        Pair(){
+            lvl = val =0;
+        }
+        Pair(int lvl, int val){
+            this.lvl = lvl;
+            this.val =val;
+        }
+        
+        @Override
+        public int compareTo(Pair p) {
+            return this.lvl == p.lvl ? this.val - p.val : this.lvl - p.lvl;
+        } 
     }
     //hlv-> columns left to right
     //lvl-> up to down
-    static void helper(TreeNode root, Map<Integer, TreeSet<Integer>> mp, int hlv, int lvl){
+    static void helper(TreeNode root, Map<Integer, ArrayList<Pair>> mp, int hlv, int lvl){
         if(root == null)
             return;
         if(mp.containsKey(hlv)){
-            TreeSet<Integer> list = mp.get(hlv);
-            list.add(root.val);
+            ArrayList<Pair> list = mp.get(hlv);
+            list.add(new Pair(lvl, root.val));
             mp.put(hlv, list);
         } else {
-            TreeSet<Integer> set = new TreeSet<>();
-            set.add(root.val);
+            ArrayList<Pair> set = new ArrayList<>();
+            set.add(new Pair(lvl, root.val));
             mp.put(hlv, set);
         }
         helper(root.left, mp, hlv-1, lvl+1);
@@ -50,11 +49,15 @@ class Solution {
 
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        Map<Integer, TreeSet<Integer>> mp = new TreeMap<>();
+        Map<Integer, ArrayList<Pair>> mp = new TreeMap<>();
         List<List<Integer>> ans = new ArrayList<>();
         helper(root, mp, 0, 0);
         for(Integer key : mp.keySet()){
-            ans.add(new ArrayList<>(mp.get(key)));
+            List<Pair> list = mp.get(key);
+            List<Integer> trv = new ArrayList<>();
+            list.sort((a, b)-> a.lvl == b.lvl ? a.val - b.val : a.lvl - b.lvl);
+            list.stream().forEach(pair-> trv.add(pair.val));
+            ans.add(trv);
         }
         return ans;
     }
