deb8502b8e760e8972a799f4c71b2329efc9c91f
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-03 14:35:50 +0530

    LC-largest-rectangle-in-histogram [Runtime: 6 ms; Memory: 60.5 MB]

--- monotonic-stack/largest-rectangle-in-histogram.java
+++ monotonic-stack/largest-rectangle-in-histogram.java
@@ -1,36 +1,16 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int lft[] = new int[n];
-        int rht[] = new int[n];
-        for(int i= 0; i < n; i++) {
-            lft[i] = rht[i] = i;
-        }
+    public int largestRectangleArea(int[] nums) {
+        int n = nums.length;
         int stack[] = new int[n+1];
-        int top = -1;
-        for(int i = 0; i < n; i++) {
-            while(top!=-1){
-                if(heights[stack[top]] > heights[i]) {
-                    lft[i] = lft[stack[top]];
-                    rht[stack[top]] = i-1;
-                    top--;
-                } else if(heights[stack[top]] == heights[i]) {
-                    lft[i] = lft[stack[top]];
-                    break;
-                }
-                else
-                    break;         
+        int top = -1, ans = 0;
+        for(int i = 0; i <= n; i++) {
+            int ele = i == n ? -1 : nums[i];
+            while(top!=-1 && nums[stack[top]] >= ele) {
+                int val = nums[stack[top--]], j = top == -1 ? -1 : stack[top];
+                ans = Math.max(ans, (i-j-1) * val);
             }
             stack[++top] = i;
-        }
-        int b = stack[top];
-        while(top >= 0) {
-            rht[stack[top--]] = b;
-        }
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            ans = Math.max(ans, (rht[i] - lft[i] + 1) * heights[i]);
-        }
+        }        
         return ans;
     }
 }
