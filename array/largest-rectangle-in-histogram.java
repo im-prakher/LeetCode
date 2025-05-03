500023b191ee358bbc1d3cd0b25fca6e73df9d76
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-03 13:18:29 +0530

    LC-largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

--- array/largest-rectangle-in-histogram.java
+++ array/largest-rectangle-in-histogram.java
@@ -4,17 +4,16 @@ class Solution {
         int lft[] = new int[n];
         int rht[] = new int[n];
         for(int i= 0; i < n; i++) {
-            lft[i] = 0;
-            rht[i] = n-1;
+            lft[i] = rht[i] = i;
         }
         int stack[] = new int[n+1];
         int top = -1;
         for(int i = 0; i < n; i++) {
             while(top!=-1 && heights[stack[top]] > heights[i]) {
+                lft[i] = stack[top];
                 rht[stack[top]] = i-1;
                 top--;
             }
-            lft[i] = top!=-1 ? stack[top]+1 : lft[i];
             stack[++top] = i;
         }
         int ans = 0;
