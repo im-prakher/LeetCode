687a4d8841109264fb3fe611df9e97c3c04d286d
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-04 15:31:47 +0530

    LC-number-of-provinces [Runtime: 0 ms; Memory: 48.4 MB]

--- graph/number-of-provinces.java
+++ graph/number-of-provinces.java
@@ -1,29 +1,21 @@
 class Solution {
-    int nodes[];
-    int findRoot(int root) {
-        if(nodes[root] == root)
-            return root;
-        return nodes[root] = findRoot(nodes[root]);
-    }
-    void union(int u, int v){
-        nodes[nodes[v]] = nodes[nodes[u]];
-    }
-
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length, m = isConnected[0].length;
-        int ans = n;        
-        nodes = new int[n];
-        for(int i = 0; i < n; i++) 
-            nodes[i] = i;
-        for(int i =0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                int u = findRoot(i), v = findRoot(j);
-                if(isConnected[i][j] == 1 && u != v) {
-                    union(u, v);
-                    ans--;
-                }
+    public int findCircleNum(int[][] arr) {
+        int [] vis=new int [arr.length+1];
+        int cnt=0;
+        for(int i=1;i<arr.length+1;i++){
+            if(vis[i]==0){
+                cnt++;
+                dfs(i,arr,vis);
             }
         }
-        return ans;
+        return cnt;
+    }
+    void dfs(int node,int [][] arr,int [] vis){
+        vis[node]=1;
+        for(int i=1;i<arr.length+1;i++){
+            if(vis[i]==0 && arr[node-1][i-1]==1){
+                dfs(i,arr,vis);
+            }
+        }
     }
 }
