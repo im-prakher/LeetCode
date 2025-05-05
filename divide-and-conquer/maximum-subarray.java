27c551fb985d7e06578c8dde5778c0a72624a84d
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-05 07:27:30 +0530

    LC-maximum-subarray [Runtime: N/A; Memory: N/A]

---
+++ divide-and-conquer/maximum-subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, maxSum = 0;
+        for(int i : nums) {
+            sum += i;
+            sum = Math.max(sum, i);
+            maxSum = Math.max(maxSum , sum);
+        }
+        return maxSum;
+    }
+}
