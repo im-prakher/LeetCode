dd436169297d82513572e59fc0f578419a1bc10a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-06 23:43:01 +0530

    LC-two-sum-iii-data-structure-design [Runtime: N/A; Memory: N/A]

---
+++ hash-table/two-sum-iii-data-structure-design.java
@@ -0,0 +1,50 @@
+class TwoSum {
+    HashMap<Integer, Boolean> mp;
+    List<Integer> list;
+    public int upperBound(List<Integer> list, int x) {
+        int idx = Collections.binarySearch(list, x);
+        if (idx < 0) {
+            return idx;
+        }
+        // Move right to find the first greater element
+        while (idx < list.size() && list.get(idx).equals(x)) {
+            idx++;
+        }
+        return idx;
+    }
+    public TwoSum() {
+        mp = new HashMap<>();
+        list = new ArrayList<>();
+    }
+    
+    public void add(int number) {
+        int idx = Collections.binarySearch(list, number);
+        int ub = idx < 0 ? -idx - 1 : idx - 1;
+        list.add(ub, number);
+    }
+    
+    public boolean twoSum(int value) {
+        int i = 0, j = list.size()-1;
+        while(i < j) {
+            int sum = list.get(i) + list.get(j);
+            if(sum == value) {
+                return true;
+            } else if(sum < value) 
+                i++;
+            else 
+                j--;
+        }
+        return false;
+    }
+
+    public boolean find(int value) {
+        return twoSum(value);
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
