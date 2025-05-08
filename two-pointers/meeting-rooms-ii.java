74a086ff0391435df2fb7172104c65a178eeaadc
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-08 06:45:46 +0530

    LC-meeting-rooms-ii [Runtime: 3 ms; Memory: 46.3 MB]

--- two-pointers/meeting-rooms-ii.java
+++ two-pointers/meeting-rooms-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        int n = intervals.length, i = 0;
-        int[] start = new int[n];
-        int[] end = new int[n];
-        for(int[] intv : intervals) {
-            start[i] = intv[0];
-            end[i++] = intv[1];
+        int n = intervals.length;
+        int start[] = new int[n];
+        int end[] = new int[n];
+        int rooms = 1;
+        for(int i = 0; i < n; i++) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
         }
         Arrays.sort(start);
         Arrays.sort(end);
-        int rooms = 0, j = 0;
-        for(i = 0; i < n; i++) {
-            if(start[i] < end[j]) 
+        for(int i = 1, j = 0; i < intervals.length; i++) {
+            if(start[i] < end[j])
                 rooms++;
             else
                 j++;
