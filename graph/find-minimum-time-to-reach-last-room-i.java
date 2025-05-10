ab5c554a152007e6721468cf8b52fc86b7622756
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-10 07:33:36 +0530

    LC-find-minimum-time-to-reach-last-room-i [Runtime: N/A; Memory: N/A]

---
+++ graph/find-minimum-time-to-reach-last-room-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int dfsTime(int[][] moveTime, int i, int j) {
+        if(i == 0 && j == 0)
+            return moveTime[i][j];
+        if(i < 0 || j < 0)
+            return Integer.MAX_VALUE/10;
+        int minTime = Math.min(dfsTime(moveTime, i-1, j),
+                    dfsTime(moveTime, i, j-1));
+        return 1 + Math.max(minTime, moveTime[i][j]);
+    }
+
+    public int minTimeToReach(int[][] moveTime) {
+        int time = 0;
+        return dfsTime(moveTime, moveTime.length - 1, moveTime[0].length - 1);
+    }
+}
