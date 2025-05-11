2364fbbd342030c6e5943d61cb4024eedd98aa25
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-11 23:56:35 +0530

    LC-shortest-word-distance [Runtime: N/A; Memory: N/A]

---
+++ string/shortest-word-distance.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int i = -1, j = -1, k = 0;
+        for(String word : wordsDict) {
+            if(word1.equals(word))
+                i = k;
+            else if(word2.equals(word))
+                j = k;
+            k++;
+        }
+        return Math.abs(i-j);
+    }
+}
