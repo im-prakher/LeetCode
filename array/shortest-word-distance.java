c030c3f587cfc24fc62967f10dce6278e603ca18
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-11 23:57:20 +0530

    LC-shortest-word-distance [Runtime: N/A; Memory: N/A]

--- array/shortest-word-distance.java
+++ array/shortest-word-distance.java
@@ -2,6 +2,8 @@ class Solution {
     public int shortestDistance(String[] wordsDict, String word1, String word2) {
         int i = -1, j = -1, k = 0;
         for(String word : wordsDict) {
+            if(i!=-1 && j!=-1)
+                break;
             if(word1.equals(word))
                 i = k;
             else if(word2.equals(word))
