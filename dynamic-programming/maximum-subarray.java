af8b77f846204561abc0844516ac93ee36a9ca45
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-12 09:36:58 +0530

    LC-maximum-subarray [Runtime: 1 ms; Memory: 57 MB]

--- dynamic-programming/maximum-subarray.java
+++ dynamic-programming/maximum-subarray.java
@@ -2,9 +2,8 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int sum = 0, maxSum = nums[0];
         for(int i : nums) {
-            sum += i;
-            sum = Math.max(sum, i);
-            maxSum = Math.max(maxSum , sum);
+            sum = i + Math.max(0, sum);
+            maxSum = Math.max(sum, maxSum);
         }
         return maxSum;
     }
