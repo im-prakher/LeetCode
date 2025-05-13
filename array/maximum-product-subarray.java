498e4c2058c35140d7bfb8993314d6f6e0a18ac1
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-13 21:03:43 +0530

    LC-maximum-product-subarray [Runtime: N/A; Memory: N/A]

--- array/maximum-product-subarray.java
+++ array/maximum-product-subarray.java
@@ -2,10 +2,9 @@ class Solution {
     public int maxProduct(int[] arr) {
         int n = arr.length, max1 = arr[0], max2 = arr[n-1], left = max1, right = max2;
         for(int i = 1; i < arr.length; i++) {
-            left = arr[i] * Math.max(1, left);
-            right = arr[n-i-1] * Math.max(1, right);
-            max1 = Math.max(max1, left);
-            max2 = Math.max(max2, right);
+            left *= arr[i]; right *= arr[n-i-1];
+            max1 = Math.max(max1, Math.max(left, arr[i]));
+            max2 = Math.max(max2, Math.max(right, arr[n-i-1]));
         }
         return Math.max(max1, max2);
     }
