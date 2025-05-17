ac26cd0579f49cbdd7b074a4c5fb9b64c7e8efd5
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-18 00:05:30 +0530

    LC-sort-colors [Runtime: 0 ms; Memory: 42 MB]

--- sorting/sort-colors.java
+++ sorting/sort-colors.java
@@ -4,17 +4,20 @@ class Solution {
         nums[a] = nums[b];
         nums[b] = tmp;
     }
+
     public void sortColors(int[] nums) {
-        int p0 = 0, curr = 0, p2 = nums.length-1;
+        int p0 = 0, curr = 0, p2 = nums.length - 1;
         while(curr <= p2) {
             if(nums[curr] == 0) {
                 swap(p0, curr, nums);
-                p0++; curr++;
-            } else if (nums[curr] == 2) {
+                p0++;
+                curr++;
+            } else if(nums[curr] == 2) {
                 swap(p2, curr, nums);
                 p2--;
-            } else
+            } else {
                 curr++;
+            }
         }
     }
 }
