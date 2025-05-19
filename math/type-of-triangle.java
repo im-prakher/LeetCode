5d043c8d5ec8119968a5a8ac6c88b76ded44c367
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-19 12:29:59 +0530

    LC-type-of-triangle [Runtime: N/A; Memory: N/A]

---
+++ math/type-of-triangle.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        int cnt = 1;
+        Arrays.sort(nums);
+        for(int i= 1; i < 3; i++) {
+            if(nums[i-1] == nums[i])
+                cnt++;
+            else 
+                cnt = 1;
+        }
+        if(cnt == 3)
+            return "equilateral";
+        else if(cnt == 2)
+            return "isosceles";
+        return "scalene";
+    }
+}
