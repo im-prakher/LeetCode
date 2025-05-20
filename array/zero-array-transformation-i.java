aa8c8a4e0f1b94225395bedc42757ae4ed73f759
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-21 00:05:22 +0530

    LC-zero-array-transformation-i [Runtime: 3 ms; Memory: 96.3 MB]

--- array/zero-array-transformation-i.java
+++ array/zero-array-transformation-i.java
@@ -1,24 +1,16 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int acc = 0, n = nums.length, qrySum = 0;
-        for(int i : nums)  {
-            acc += i;
-        }
-        int[] pre = new int[n+1];
-        int sum = 0;
-        for(int i = 0; i < n; i++) {
-            pre[i] = sum;
-            sum += nums[i] > 0 ? 1 : 0;
-        }
-        pre[n] = sum;
-        int rightNeg = -1, leftNeg = n+1;
+        int n = nums.length;
+        int diff[] = new int[n + 1];
         for(int[] qry : queries) {
-            int right = qry[1] >= rightNeg ? pre[qry[1]+1] - 1 : pre[qry[1]+1];
-            int left = qry[0] <= leftNeg ? pre[qry[0]] - 1: pre[qry[0]];
-            qrySum += right - left;
-            rightNeg = Math.max(rightNeg, qry[1]-1);
-            leftNeg = Math.min(leftNeg, qry[0]);
+            diff[qry[0]] += 1;
+            diff[qry[1] + 1] -= 1;
         }
-        return acc <= qrySum; 
+        for(int i = 0; i < n; i++) {
+            diff[i] += i != 0 ? diff[i-1] : 0;
+            if(diff[i] < nums[i])
+                return false;
+        }
+        return true;
     }
 }
