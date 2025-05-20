374e72b7d145dedcfe2ea68f8a12e83ae1fc1e8d
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-20 17:00:49 +0530

    LC-zero-array-transformation-i [Runtime: N/A; Memory: N/A]

--- prefix-sum/zero-array-transformation-i.java
+++ prefix-sum/zero-array-transformation-i.java
@@ -1,13 +1,24 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int sum = 0;
+        int acc = 0, n = nums.length, qrySum = 0;
         for(int i : nums)  {
-            sum += i;
+            acc += i;
         }
-        int transform = 0;
-        for(int[] query : queries) {
-            transform += query[1] - query[0] + 1;
+        int[] pre = new int[n+1];
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            pre[i] = sum;
+            sum += nums[i] > 0 ? 1 : 0;
         }
-        return sum <= transform; 
+        pre[n] = sum;
+        int rightNeg = -1, leftNeg = n+1;
+        for(int[] qry : queries) {
+            int right = qry[1] >= rightNeg ? pre[qry[1]+1] - 1 : pre[qry[1]+1];
+            int left = qry[0] <= leftNeg ? pre[qry[0]] - 1: pre[qry[0]];
+            qrySum += right - left;
+            rightNeg = Math.max(rightNeg, qry[1]);
+            leftNeg = Math.min(leftNeg, qry[0]);
+        }
+        return acc <= qrySum; 
     }
 }
