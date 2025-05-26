10f7528208c2cd9c9c3686a36218e2962e23c77e
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-26 05:58:13 +0530

    LC-number-of-provinces [Runtime: 2 ms; Memory: 44.9 MB]

--- union-find/number-of-provinces.java
+++ union-find/number-of-provinces.java
@@ -1,23 +1,22 @@
 class Solution {
     int par[];
     void union(int x, int y) {
-        par[par[x]] = find(par[y]);
+        par[par[x]] = par[par[y]];
     }
     int find(int x) {
         if(x == par[x])
             return x;
-        return x = find(par[x]);
+        return par[x] = find(par[x]);
     }
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length,  cnt = n;
+        int n = isConnected.length, cnt = n;
         par = new int[n];
         for(int i = 0; i < n; i++) 
             par[i] = i;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                int u = find(i), v = find(j);
-                if(isConnected[i][j] == 1 &&  u != v) {
-                    union(u, v);
+                if(isConnected[i][j] == 1 && find(i) != find(j)) {
+                    union(i, j);
                     cnt--;
                 }
             }
