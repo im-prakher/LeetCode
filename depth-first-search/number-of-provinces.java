2023d1a4e7778cb7dfd7ca31bdac6d3f86d21565
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-26 05:45:13 +0530

    LC-number-of-provinces [Runtime: N/A; Memory: N/A]

--- depth-first-search/number-of-provinces.java
+++ depth-first-search/number-of-provinces.java
@@ -1,21 +1,32 @@
 class Solution {
-    public int findCircleNum(int[][] arr) {
-        int [] vis=new int [arr.length+1];
-        int cnt=0;
-        for(int i=1;i<arr.length+1;i++){
-            if(vis[i]==0){
-                cnt++;
-                dfs(i,arr,vis);
+    int par[];
+    void union(int x, int y) {
+        par[x] = find(par[y]);
+    }
+    int find(int x) {
+        if(x == par[x])
+            return x;
+        return x = find(par[x]);
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length, cnt = n;
+        par = new int[n];
+        for(int i = 0; i < n; i++) 
+            par[i] = i;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j)
+                    continue;
+                int u = find(i), v = find(j);
+                if(u == v)
+                    cnt--;
+                else if(isConnected[i][j]==1) {
+                    union(u, v);
+                    isConnected[i][j] = 0;
+                    isConnected[j][i] = 0;
+                }
             }
         }
         return cnt;
-    }
-    void dfs(int node,int [][] arr,int [] vis){
-        vis[node]=1;
-        for(int i=1;i<arr.length+1;i++){
-            if(vis[i]==0 && arr[node-1][i-1]==1){
-                dfs(i,arr,vis);
-            }
-        }
     }
 }
