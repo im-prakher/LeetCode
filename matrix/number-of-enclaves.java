3ed62ed6d47af8d5988a158746d69920cce00292
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-05-27 06:18:26 +0530

    LC-number-of-enclaves [Runtime: 8 ms; Memory: 56 MB]

--- matrix/number-of-enclaves.java
+++ matrix/number-of-enclaves.java
@@ -4,15 +4,14 @@ class Solution {
         return x >= 0 && x < n && y >= 0 && y < m;
     }
 
-    public void dfs(int[][] board, int i, int j, boolean vis[][]) {
-        vis[i][j] = true;
+    public void dfs(int[][] board, int i, int j) {
         if(board[i][j] == 1) {
             board[i][j] = 2;
             int dir[] = {-1, 0, 1, 0, -1};
             for(int k = 0; k < 4; k++) {        
                 int nx = i + dir[k], ny = j + dir[k+1];
-                if(valid(nx, ny, board) && !vis[nx][ny]) {
-                    dfs(board, nx, ny, vis);
+                if(valid(nx, ny, board)) {
+                    dfs(board, nx, ny);
                 }
             }
         }
@@ -20,18 +19,17 @@ class Solution {
 
     public int numEnclaves(int[][] board) {
           int n = board.length, m = board[0].length;
-        boolean vis[][] = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             if (board[i][0] == 1) 
-                dfs(board, i, 0,vis);
+                dfs(board, i, 0);
             if (board[i][m - 1] == 1) 
-                dfs(board, i, m - 1, vis);
+                dfs(board, i, m - 1);
         }
         for (int j = 1; j < m - 1; j++) {
             if (board[0][j] == 1)
-                dfs(board, 0, j, vis);
+                dfs(board, 0, j);
             if (board[n - 1][j] == 1)
-                dfs(board, n - 1, j, vis);
+                dfs(board, n - 1, j);
         }
         int cnt = 0;
         for (int i = 0; i < n; i++) {
