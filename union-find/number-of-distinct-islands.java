9e9233393f67957eb360720ba4800397edadc775
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-01 08:03:51 +0530

    LC-number-of-distinct-islands [Runtime: N/A; Memory: N/A]

---
+++ union-find/number-of-distinct-islands.java
@@ -0,0 +1,42 @@
+class Solution {
+    boolean valid(int nx, int ny, int n, int m) {
+        return nx >= 0 && nx < n && ny >= 0 && ny < m; 
+    }
+    public boolean bfsDistinct(int[][] grid, int a, int b, Set<String> set) {
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[]{a, b});
+        grid[a][b] = 0;
+        int dir[] = {-1, 0, 1, 0, -1};
+        int n = grid.length, m = grid[0].length;
+        int max_i = 0, max_j = 0, min_i = n, min_j = m, area = 1;
+        String island = "";
+        while(!que.isEmpty()) {
+            int[] land = que.poll();
+            int x = land[0], y = land[1];
+            island = (x-a) + "&" + (y-b) + "&";
+            for(int k = 0; k < 4; k++) {
+                int nx = dir[k] + x, ny = dir[k+1] + y;
+                if(valid(nx, ny, n, m) && grid[nx][ny] == 1) {
+                    que.offer(new int[]{nx, ny});
+                    grid[nx][ny] = 0;
+                }
+            }
+        }
+        if(set.contains(island))
+            return false;
+        set.add(island);
+        return true;
+    }
+    public int numDistinctIslands(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int cnt = 0;
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1 && bfsDistinct(grid, i, j, set))
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+}
