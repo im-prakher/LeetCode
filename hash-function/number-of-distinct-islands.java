bd9c84685f4321e08a6896544ba62570694b66ba
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-01 08:54:58 +0530

    LC-number-of-distinct-islands [Runtime: N/A; Memory: N/A]

--- hash-function/number-of-distinct-islands.java
+++ hash-function/number-of-distinct-islands.java
@@ -1,35 +1,38 @@
 class Solution {
-    int dir[] = { -1, 0, 1, 0, -1 };
-    char[] sign = {'U', 'R', 'D', 'L'};
     boolean valid(int nx, int ny, int n, int m) {
-        return nx >= 0 && nx < n && ny >= 0 && ny < m;
+        return nx >= 0 && nx < n && ny >= 0 && ny < m; 
     }
-
-    public void dfsDistinct(int[][] grid, int x, int y, StringBuilder island) {
-        grid[x][y] = 0;
+    public void bfsDistinct(int[][] grid, int a, int b, Set<String> set) {
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[]{a, b});
+        grid[a][b] = 0;
+        int dir[] = {-1, 0, 1, 0, -1};
+        char[] sign = {'U', 'R', 'D', 'L'};
         int n = grid.length, m = grid[0].length;
-        for (int k = 0; k < 4; k++) {
-            int nx = dir[k] + x, ny = dir[k + 1] + y;
-            if (valid(nx, ny, n, m) && grid[nx][ny] == 1) {
-                island.append(sign[k]);
-                dfsDistinct(grid, nx, ny, island);
-                island.append("B");
+        StringBuilder island = new StringBuilder();
+        island.append('O');
+        while(!que.isEmpty()) {
+            int[] land = que.poll();
+            int x = land[0], y = land[1];
+            for(int k = 0; k < 4; k++) {
+                int nx = dir[k] + x, ny = dir[k+1] + y;
+                if(valid(nx, ny, n, m) && grid[nx][ny] == 1) {
+                    island.append(sign[k]);
+                    que.offer(new int[]{nx, ny});
+                    grid[nx][ny] = 0;
+                }
             }
         }
+        set.add(island.toString());
     }
-
     public int numDistinctIslands(int[][] grid) {
         int n = grid.length, m = grid[0].length;
         int cnt = 0;
         Set<String> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    StringBuilder island = new StringBuilder();
-                    island.append("O");
-                    dfsDistinct(grid, i, j, island);
-                    set.add(island.toString());
-                }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1)
+                 bfsDistinct(grid, i, j, set);
             }
         }
         return set.size();
