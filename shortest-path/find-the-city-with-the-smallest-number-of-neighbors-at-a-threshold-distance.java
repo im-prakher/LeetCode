7dfca291dd424c749bcf7285db65ae74da125852
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-01 12:39:57 +0530

    LC-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance [Runtime: 8 ms; Memory: 43.7 MB]

---
+++ shortest-path/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -0,0 +1,35 @@
+class Solution {
+    int INF = (int)1e9;
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int adj[][] = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(adj[i], INF);
+            adj[i][i] = 0;
+        }
+        for(int[] edge: edges) {
+            int i = edge[0], j = edge[1];
+            adj[i][j] = edge[2];
+            adj[j][i] = edge[2];
+        }
+        for(int k = 0; k < n; k++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+                }
+            }
+        }
+        int ans = -1, min = n+1;
+        for(int i = 0; i < n; i++) {
+            int cnt = 0;
+            for(int j = 0; j < n; j++) {
+                if(adj[i][j] != INF && adj[i][j] <= distanceThreshold) 
+                    cnt++;
+            }
+            if(cnt <= min) {
+                ans = i; 
+                min = cnt;
+            }
+        }
+        return ans;
+    }
+}
