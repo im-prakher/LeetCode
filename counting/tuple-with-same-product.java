4e85f4d4ec164aa78300ef66f854f8728f425718
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-06 07:31:10 +0530

    LC-tuple-with-same-product [Runtime: 313 ms; Memory: 70 MB]

--- counting/tuple-with-same-product.java
+++ counting/tuple-with-same-product.java
@@ -1,17 +1,15 @@
 class Solution {
     public int tupleSameProduct(int[] nums) {
         Map<Integer, Integer> mp = new HashMap<>();
+        int pairs = 0;
         for(int i = 0; i < nums.length; i++) {
             for(int j = i+1; j < nums.length; j++) {
                 int prod = nums[i] * nums[j];
                 mp.putIfAbsent(prod, -1);
                 mp.put(prod, mp.get(prod) + 1);
+                pairs += mp.get(prod);
             }
         }
-        int cnt = 0;
-        for(int k : mp.keySet()) {
-            cnt += mp.get(k);
-        }
-        return cnt * 8;
+        return pairs * 8;
     }
 }
