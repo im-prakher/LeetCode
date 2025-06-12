6dd33194c05385ee0b210f5aff61be02e5b39cc2
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-12 11:01:00 +0530

    LC-partition-array-according-to-given-pivot [Runtime: 4 ms; Memory: 68.3 MB]

---
+++ two-pointers/partition-array-according-to-given-pivot.java
@@ -0,0 +1,22 @@
+class Solution {
+    void swap(int i, int j, int[] nums) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    public int[] pivotArray(int[] nums, int pivot) {
+        int a = 0, n = nums.length, b = n-1;
+        int ans[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            if(nums[i] < pivot)
+                ans[a++] = nums[i];
+        }
+        for(int j = n-1; j >= 0; j--) {
+            if(nums[j] > pivot)
+                ans[b--] = nums[j];
+        }
+        for(int k = a; k <=b; k++) 
+            ans[k] = pivot;
+        return ans;
+    }
+}
