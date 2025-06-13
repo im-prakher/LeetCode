f9a493b51f2b2d2afe3fc07f20a4290e30df9c96
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-13 07:39:46 +0530

    LC-partition-array-according-to-given-pivot [Runtime: 4 ms; Memory: 67.8 MB]

--- array/partition-array-according-to-given-pivot.java
+++ array/partition-array-according-to-given-pivot.java
@@ -1,30 +1,15 @@
 class Solution {
-    void swap(int a, int b, int[] nums) {
-        int tmp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = tmp;
-    }
-    public void reverse(int i, int j, int[] nums) {
-        while(i < j) {
-            int tmp = nums[j];
-            nums[j] = nums[i];
-            nums[i] = tmp;
-            i++; j--;
-        }
-    }
     public int[] pivotArray(int[] nums, int pivot) {
-        int p0 = 0, curr = 0, p2 = nums.length - 1;
-        while(curr <= p2) {
-            if(nums[curr] < pivot) {
-                swap(p0, curr, nums);
-                p0++; curr++;
-            } else if(nums[curr] > pivot) {
-                swap(p2, curr, nums);
-                p2--;
-            } else 
-                curr++;
+        int a = 0, n = nums.length, b = n-1;
+        int ans[] = new int[n];
+        for(int i = 0, j = b; i < n; i++, j--) {
+            if(nums[i] < pivot)
+                ans[a++] = nums[i];
+            if(nums[j] > pivot)
+                ans[b--] = nums[j];
         }
-        reverse(p2+1, nums.length-1,nums);
-        return nums;
+        for(int k = a; k <=b; k++) 
+            ans[k] = pivot;
+        return ans;
     }
 }
