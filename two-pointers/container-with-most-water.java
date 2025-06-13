d4a1842d5ff23b8ef8ddb78ae4cda8aa4fa669f0
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-13 07:51:27 +0530

    LC-container-with-most-water [Runtime: N/A; Memory: N/A]

--- two-pointers/container-with-most-water.java
+++ two-pointers/container-with-most-water.java
@@ -2,12 +2,13 @@ class Solution {
     public int maxArea(int[] nums) {
         int i = 0, j = nums.length - 1, area = 0;
         while(i < j) {
-            area = Math.max(area, Math.min(nums[i], nums[j]) * (j-i));
+            int height = Math.min(nums[i], nums[j]);
+            area = Math.max(area, height * (j-i));
             if(nums[i] < nums[j]) {
-                while(++i < j && nums[i] < );
+                while(++i < j && nums[i] < height);
             }
             else {
-                while(--j > i && nums[i] >= nums[j]);
+                while(--j > i && nums[i] >= height);
             }
         }
         return area;
