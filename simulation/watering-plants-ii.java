7ce66ec1326e9bd70cfb4cde72869565e4366f4b
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-15 17:00:03 +0530

    LC-watering-plants-ii [Runtime: 4 ms; Memory: 57.8 MB]

--- simulation/watering-plants-ii.java
+++ simulation/watering-plants-ii.java
@@ -1,26 +1,24 @@
 class Solution {
-    public int minimumRefill(int[] plants, int capacityA, int capacityB) {
+    public int minimumRefill(int[] plants, int capA, int capB) {
         int i = 0, j = plants.length - 1;
-        int refill = 0, capA = capacityA, capB = capacityB;
-        while(i <= j) {
-            if(i == j) {
-                if(capA >= capB)
-                    capA -= plants[i];
-                else
-                    capB -= plants[i];
-            } else {
-                capA -= plants[i];
-                capB -= plants[j];
-            }
-            if(capA < 0) {
-                capA += capacityA;
+        int refill = 0, alice = capA, bob = capB;
+        while (i < j) {
+            if (alice < plants[i]) {
+                alice = capA;
                 refill++;
             }
-            if(capB < 0) {
-                capB += capacityB;
+            if (bob < plants[j]) {                
+                bob = capB;
                 refill++;
             }
-            i++; j--;
+            alice -= plants[i++];
+            bob -= plants[j--];
+        }
+        if (i == j) {
+            if(alice >= bob) 
+                refill += (plants[i] > alice) ? 1 : 0;
+            else
+                refill += (plants[i] > bob) ? 1 : 0;
         }
         return refill;
     }
