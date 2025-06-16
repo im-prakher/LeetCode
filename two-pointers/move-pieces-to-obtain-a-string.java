26129c36dbdcbfdfd89c44bbf1909bd1e0733ff0
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-16 10:28:11 +0530

    LC-move-pieces-to-obtain-a-string [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/move-pieces-to-obtain-a-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        int i = 0, j = 0, n = start.length();
+        char[] tgr = target.toCharArray();
+        char[] str = start.toCharArray();
+        while(i < n && j < n) {
+            if(tgr[i] != '_') {
+                while(j < n && str[j] == '_') {
+                    j++;
+                }
+                if(tgr[i] == str[j]) {
+                    if(tgr[i] == 'L' && i > j)
+                        return false;
+                    else if(tgr[i] == 'R' &&  j > i)
+                        return false;
+                } else
+                    return false;
+                j++;
+            } 
+            i++;
+        }
+        return true;
+    }
+}
