5065047e46c32597900ca2b2cea130447c054987
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-16 07:00:02 +0530

    LC-string-compression [Runtime: 1 ms; Memory: 43.7 MB]

--- string/string-compression.java
+++ string/string-compression.java
@@ -1,16 +1,27 @@
 class Solution {
     public int compress(char[] str) {
-        char[] comp = new char[];
-        char prv = '';
-        int cnt = 0;
-        for(char ch : str) {
-            if(prv == ch)
+        int cnt = 0, len = 0, prv = 0, idx = 0;
+        for (int i = 0; i <= str.length; i++) {
+            if (i != str.length && str[prv] == str[i])
                 cnt++;
             else {
-                cnt = 0;
-                prv = ch;
-                
+                str[idx++] = str[prv];
+                if (cnt > 1) {
+                    int num = cnt;
+                    while(num > 0) {
+                        num /= 10;
+                        idx++;
+                    }
+                    int k = idx;
+                    while (cnt > 0) {
+                        str[--k] = (char) ('0' + cnt % 10);
+                        cnt /= 10;
+                    }
+                }
+                prv = i;
+                cnt = 1;
             }
         }
+        return idx;
     }
 }
