d25b806a9eff82372bc3b8c840df53a623072bad
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-17 07:06:34 +0530

    LC-sentence-similarity-iii [Runtime: N/A; Memory: N/A]

--- array/sentence-similarity-iii.java
+++ array/sentence-similarity-iii.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        char[] s1, s2;
         if(sentence1.length > sentence2.length) {
             s1 = sentence1.toCharArray();
             s2 = sentence2.toCharArray();
@@ -7,11 +8,10 @@ class Solution {
             s2 = sentence1.toCharArray();
             s1 = sentence2.toCharArray();
         }
-         = sentence1.toCharArray();
-        char[] s2 = sentence2.toCharArray();
         int i = 0, j = 0, n = s1.length, m = s2.length;
         boolean gap = false;
         while(i < n && j < m) {
         }
+        
     }
 }
