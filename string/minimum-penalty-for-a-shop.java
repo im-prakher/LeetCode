791061eee23e94e112af15584a93713aebbd1d5d
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-18 07:05:03 +0530

    LC-minimum-penalty-for-a-shop [Runtime: 10 ms; Memory: 45.8 MB]

--- string/minimum-penalty-for-a-shop.java
+++ string/minimum-penalty-for-a-shop.java
@@ -5,17 +5,17 @@ class Solution {
         int[] onClose = new int[n+2];
         int[] onOpen = new int[n+2];
         for(int i = 1; i <= n; i++) {
-            onClose[i] += onClose[i-1] + (i < n && cust[i] == 'Y' ? 1 : 0);
-            onOpen[i] += onOpen[i-1] + (i < n && cust[i] == 'N' ? 1 : 0);
+            onClose[i] += onClose[i-1] + (cust[i-1] == 'Y' ? 1 : 0);
+            onOpen[i] += onOpen[i-1] + (cust[i-1] == 'N' ? 1 : 0);
         }
         onClose[n+1] = onClose[n];
         onOpen[n+1] = onOpen[n];
         int ans = 0, min = onClose[n+1] - onClose[0] + onOpen[0];
         int penalty;
-        for(int i = 1; i <= n+1; i++) {
+        for(int i = 2; i <= n+1; i++) {
             penalty = onClose[n+1] - onClose[i-1] + onOpen[i-1];
             if(min > penalty) {
-                ans = i;
+                ans = i-1;
                 min = penalty;
             }
         }
