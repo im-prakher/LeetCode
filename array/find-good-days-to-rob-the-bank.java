3938bd2d4d1ffc85c5de6902025523c8c04831ba
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-18 07:31:07 +0530

    LC-find-good-days-to-rob-the-bank [Runtime: N/A; Memory: N/A]

---
+++ array/find-good-days-to-rob-the-bank.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> goodDaysToRobBank(int[] security, int time) {        
+        Set<Integer> set = new HashSet<>();
+        int n = security.length, days = 0;
+        for(int i = 0; i < n-time; i++) {
+            if(i == 0 || security[i-1] >= security[i])
+                days++;
+            else
+                days = 0;
+            if(days > time) 
+                set.add(i);
+        }
+        days = 0;
+        for(int i = n-1; i >= time; i--) {
+            if(i == n-1 || security[i] <= security[i+1])
+                days++;
+            else 
+                days = 0;
+            if(days < time && set.contains(i))
+                set.remove(i);
+        }
+        return new ArrayList<>(set);
+    }
+}
