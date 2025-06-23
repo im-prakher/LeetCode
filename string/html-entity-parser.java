5d3ed531e84674bd3b0b7484233f044328a85975
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-23 07:27:53 +0530

    LC-html-entity-parser [Runtime: 20 ms; Memory: 45.5 MB]

--- string/html-entity-parser.java
+++ string/html-entity-parser.java
@@ -1,34 +1,12 @@
 class Solution {
     public String entityParser(String text) {
-        char[] str = text.toCharArray();
-        StringBuilder res = new StringBuilder();
-        int n = text.length();
-        Map<String, String> mp = new HashMap<>();
-        mp.put("&quot;", "\"");
-        mp.put("&apos;", "\'");
-        mp.put("&amp;", "&");
-        mp.put("&gt;", ">");
-        mp.put("&lt;", "<");
-        mp.put("&frasl;", "/");
-        for(int i = 0; i < n; i++) {
-            StringBuilder word = new StringBuilder();
-            if(str[i] == '&') {
-                while(i < n) {
-                    word.append(str[i++]);
-                    if(mp.containsKey(word.toString())) {
-                        word = new StringBuilder(mp.get(word.toString()));
-                        i--;
-                        break;
-                    }
-                    if(i < n && str[i] == '&') {
-                        i--; break;
-                    }
-                }
-            } else {
-                word.append(str[i]);
-            }
-            res.append(word);
-        }
-        return res.toString(); 
+        Map<String, String> map = new HashMap<>();
+        text = text.replace("&gt;", ">");
+        text = text.replace("&quot;", "\"");
+        text = text.replace("&apos;", "'");
+        text = text.replace("&lt;", "<");
+        text = text.replace("&frasl;", "/");
+        text = text.replace("&amp;", "&");
+        return text;
     }
 }
