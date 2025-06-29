1b047c1c7f2320e03a3b206052682acd82c50224
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-29 16:19:20 +0530

    LC-max-consecutive-ones-ii [Runtime: 4 ms; Memory: 45.3 MB]

--- sliding-window/max-consecutive-ones-ii.java
+++ sliding-window/max-consecutive-ones-ii.java
@@ -1,15 +1,15 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int zero = 0, ones = 0, len = 0;
+        int zdx = -1, len = 0, zero = 0, j = -1;
         for(int i = 0; i < nums.length; i++) {
             zero += nums[i] == 0 ? 1 : 0;
-            ones += nums[i] == 1 ? 1 : 0;
-            if(zero <= 1)
-                len = Math.max(len, zero + ones);
-            else {
-                zero = 1; ones = 0;
-            }           
-       }
+            if(zero > 1) {
+                j = zdx;
+                zero = 1;
+            }
+            zdx = nums[i] == 0 ? i : zdx;
+            len = Math.max(len, i-j);
+        }
        return len;
     }
 }
