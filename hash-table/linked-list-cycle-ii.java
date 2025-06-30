6db95653e191beb279d7c70dbcbf8b0bade08e8c
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-06-30 07:16:57 +0530

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

---
+++ hash-table/linked-list-cycle-ii.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        do {
+            slow = slow.next;
+            if(fast.next != null)
+                fast = fast.next.next;
+        }
+        while(slow!=fast && fast.next != null);
+
+        if(fast == null || fast.next == null)
+            return null;
+        while(head != fast) {
+            head = head.next;
+            fast = fast.next;
+        }
+        return head;
+    }
+}
