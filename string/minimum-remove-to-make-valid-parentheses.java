747fe1d50e57cf676261686c0b8a7fe583ee0b7c
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-01 07:13:26 +0530

    LC-minimum-remove-to-make-valid-parentheses [Runtime: 28 ms; Memory: 52 MB]

--- string/minimum-remove-to-make-valid-parentheses.java
+++ string/minimum-remove-to-make-valid-parentheses.java
@@ -1,34 +1,21 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
         StringBuilder sb = new StringBuilder();
-        int open = 0, close = 0;
-        Set<Integer> set = new HashSet<>();
+        int openIgnore = 0;;
+        Set<Integer> open = new HashSet<>();
+        Set<Integer> close = new HashSet<>();
         char[] str = s.toCharArray();
         for(int i = 0; i < str.length; i++) {
-            open += str[i] == '(' ? 1 : 0;
-            close += str[i] == ')' ? 1 : 0;
-            if(open >= close) {
-                open--; close--;
-            }
-            if(close > open) {
-                set.add(i);
-                close--;
-            }
-        }
-        open = 0; close = 0;
-        for(int i = str.length-1; i >= 0; i--) {
-            open += str[i] == '(' ? 1 : 0;
-            close += str[i] == ')' ? 1 : 0;
-            if(open <= close) {
-                open--; close--;
-            }
-            if(close < open) {
-                set.add(i);
-                open--;
+            if(str[i] == '(') {
+                open.add(i);
+            } else if(str[i] == ')' && open.size() > openIgnore) {
+                openIgnore++;
+            } else if(str[i] == ')'){
+                close.add(i);
             }
         }
         for(int i =0; i< str.length; i++) {
-            if(!set.contains(i))
+            if((open.contains(i) && openIgnore-- > 0) || (!open.contains(i) && !close.contains(i)))
                 sb.append(str[i]);
         }
         return sb.toString();
