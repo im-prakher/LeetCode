0f5373c737880ca91e55d87b367cd276b863fb2a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-01 06:43:00 +0530

    LC-minimum-add-to-make-parentheses-valid [Runtime: 0 ms; Memory: 41.9 MB]

--- greedy/minimum-add-to-make-parentheses-valid.java
+++ greedy/minimum-add-to-make-parentheses-valid.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int open = 0, close = 0, bal = 0;
+        int open = 0, close = 0, moves = 0;
         for(char ch : s.toCharArray()) {
-            open += ch == '(' ? 1 : 0;
-            close += ch == ')' ? 1 : 0;
+            open += ch =='(' ? 1 : 0;
+            close += ch ==')' ? 1 : 0;
             if(close > open) {
-                bal++;
                 close--;
+                moves++;
             }
         }
-        return bal + open - close;
+        return moves + open - close;
     }
 }
