ee7d22549f831c512e4a013b868ddf8824d8f528
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-02 17:50:09 +0530

    LC-count-number-of-nice-subarrays [Runtime: 8 ms; Memory: 56 MB]

--- sliding-window/count-number-of-nice-subarrays.java
+++ sliding-window/count-number-of-nice-subarrays.java
@@ -1,17 +1,13 @@
 class Solution {
-    public int atMost(int[] nums, int k) {
-        int cnt = 0;
-        for(int i = 0, j = -1; i < nums.length; i++) {
-            k -= nums[i] % 2 == 1 ? 1 : 0;
-            while(k < 0) {
-                k += nums[++j] % 2 == 1 ? 1 : 0;
-            }
-            cnt += i - j;
-        }
-        return cnt;
-    }
-
     public int numberOfSubarrays(int[] nums, int k) {
-        return atMost(nums, k) - atMost(nums, k-1);
+        int cnt[] = new int[100_000];
+        cnt[0] = 1;
+        int odd = 0, res = 0;
+        for(int i = 0; i < nums.length; i++) {
+            odd += (nums[i] % 2) == 1 ? 1 : 0;
+            cnt[odd]++;
+            res += (odd - k >= 0) ? cnt[odd-k] : 0;
+        }
+        return res;
     }
 }
