d64770917cc898c019931d9fa1fecc03e1c14c3f
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-02 17:18:05 +0530

    LC-longest-repeating-character-replacement [Runtime: 4 ms; Memory: 44.4 MB]

--- string/longest-repeating-character-replacement.java
+++ string/longest-repeating-character-replacement.java
@@ -1,21 +1,12 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int freq[] = new int[26];
+        int freq[] = new int[260];
         char str[] = s.toCharArray();
         int maxFreq = 0, res = 0;
-        TreeMap<Integer, Integer> tmap = new TreeMap<>();
         for(int i = 0, j = -1; i < str.length; i++) {
-            int idx = str[i] - 'A';
-            tmap.put(++freq[idx], tmap.getOrDefault(freq[idx], 0) + 1);
-            maxFreq = tmap.lastKey();
+            maxFreq = Math.max(maxFreq, ++freq[str[i]]);
             while(i - j - maxFreq > k) {
-                int key = freq[str[++j]-'A'];
-                if(tmap.get(key) == 1)
-                    tmap.remove(key);
-                else
-                    tmap.put(key, tmap.get(key)-1);
-                tmap.put(--freq[str[j]-'A'], tmap.getOrDefault(freq[str[j]-'A'],0) +1);
-                maxFreq = tmap.lastKey();
+                freq[str[++j]]--;
             }
             res = Math.max(res, i - j);
         }
