a3c3fa8c4a71d9e245170b6508319ddfd34e9695
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-02 07:29:38 +0530

    LC-check-if-a-parentheses-string-can-be-valid [Runtime: 20 ms; Memory: 46.9 MB]

--- string/check-if-a-parentheses-string-can-be-valid.java
+++ string/check-if-a-parentheses-string-can-be-valid.java
@@ -3,12 +3,12 @@ class Solution {
         StringBuilder sb = new StringBuilder(s);
         StringBuilder lock = new StringBuilder(locked);
         return isValid(sb.toString(), lock.toString()) 
-                || isValidRL(sb.toString(), lock.toString());
+                ;
     }
     public boolean isValid(String s, String locked) {
         if(s.length() % 2 != 0)
             return false;
-        int closeUnlock = 0, open = 0, close = 0;
+        int closeUnlock = 0, open = 0, close = 0, change = 0;
         char[] str = s.toCharArray();
         char[] lock = locked.toCharArray();
         for(int i = 0; i < s.length(); i++) {
@@ -20,21 +20,20 @@ class Solution {
                     closeUnlock--;
                     open++;
                     close--;
+                    change++;
                 } else {
                     return false;
                 }
             }
         }
-        // if(close > open)
-        //     close unlocked cnt > 0 & open++
-        return open == close;
-    }
-    public boolean isValidRL(String s, String locked) {
-        if(s.length() % 2 != 0)
-            return false;
-        int closeUnlock = 0, open = 0, close = 0;
-        char[] str = s.toCharArray();
-        char[] lock = locked.toCharArray();
+        for(int i = 0; change > 0 && i < str.length; i++) {
+            if(str[i] == ')' && lock[i] == '0') {
+                str[i] = '(';
+                change--;
+            }
+        }
+
+        closeUnlock = 0; open = 0; close = 0;
         for(int i = s.length()-1; i >= 0 ; i--) {
             close += str[i] == '(' ? 1 : 0;
             open += str[i] == ')' ? 1 : 0;
