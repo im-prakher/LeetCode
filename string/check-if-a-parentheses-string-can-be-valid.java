409eec7b76b46ec553e9eb97a5d7a580458feeef
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-02 07:19:29 +0530

    LC-check-if-a-parentheses-string-can-be-valid [Runtime: N/A; Memory: N/A]

---
+++ string/check-if-a-parentheses-string-can-be-valid.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        StringBuilder sb = new StringBuilder(s);
+        StringBuilder lock = new StringBuilder(locked);
+        return isValid(sb.toString(), lock.toString()) 
+                || isValidRL(sb.toString(), lock.toString());
+    }
+    public boolean isValid(String s, String locked) {
+        if(s.length() % 2 != 0)
+            return false;
+        int closeUnlock = 0, open = 0, close = 0;
+        char[] str = s.toCharArray();
+        char[] lock = locked.toCharArray();
+        for(int i = 0; i < s.length(); i++) {
+            close += str[i] == ')' ? 1 : 0;
+            open += str[i] == '(' ? 1 : 0;
+            closeUnlock += str[i] == ')' && lock[i] == '0' ? 1 : 0;
+            if(close > open) { 
+                if(closeUnlock > 0) {
+                    closeUnlock--;
+                    open++;
+                    close--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        // if(close > open)
+        //     close unlocked cnt > 0 & open++
+        return open == close;
+    }
+    public boolean isValidRL(String s, String locked) {
+        if(s.length() % 2 != 0)
+            return false;
+        int closeUnlock = 0, open = 0, close = 0;
+        char[] str = s.toCharArray();
+        char[] lock = locked.toCharArray();
+        for(int i = s.length()-1; i >= 0 ; i--) {
+            close += str[i] == '(' ? 1 : 0;
+            open += str[i] == ')' ? 1 : 0;
+            closeUnlock += str[i] == '(' && lock[i] == '0' ? 1 : 0;
+            if(close > open) { 
+                if(closeUnlock > 0) {
+                    closeUnlock--;
+                    open++;
+                    close--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        // if(close > open)
+        //     close unlocked cnt > 0 & open++
+        return open == close;
+    }
+}
