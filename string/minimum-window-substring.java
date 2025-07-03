0aa0fb5d7a1bdbd12b8d1218d24a5acadc641006
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-03 23:03:30 +0530

    LC-minimum-window-substring [Runtime: 2 ms; Memory: 44.6 MB]

--- string/minimum-window-substring.java
+++ string/minimum-window-substring.java
@@ -1,24 +1,23 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> mp = new HashMap<>();
-        for(char ch : t.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
-        }
-        int cnt = 0, m = mp.size(), n = s.length(), res = n+1, a = -1, b = -1;
         int[] freq = new int[128];
+        int[] tmap = new int[128];
+        int cnt = 0, dist = 0, n = s.length(), res = n+1, a = -1, b = -1;
+        for(char ch : t.toCharArray()) {
+            if(tmap[ch] == 0)
+                dist++;
+            tmap[ch]++;
+        }
         char str[] = s.toCharArray();
         for(int i = 0, j = 0; i < n; i++) {
-            cnt += mp.getOrDefault(str[i], n*2) == ++freq[str[i]] ? 1 : 0;
-            while(cnt == m) {
+            cnt += tmap[str[i]] == ++freq[str[i]] ? 1 : 0;
+            while(cnt == dist) {
                 if(res > i-j+1) {
                     a = j; b = i;
                     res = i-j+1;
                 }
-                if(mp.containsKey(str[j])) {
-                    if(freq[str[j]] == mp.get(str[j]))
-                        cnt--;
-                }
-                freq[str[j++]]--;
+                if(freq[str[j]]-- == tmap[str[j++]])
+                    cnt--;
             }
         }
         if(a == -1)
