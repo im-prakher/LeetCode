f125b08d26bb56dcd499c7f515016546bc364504
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-07 20:16:01 +0530

    LC-next-greater-element-ii [Runtime: N/A; Memory: N/A]

--- monotonic-stack/next-greater-element-ii.java
+++ monotonic-stack/next-greater-element-ii.java
@@ -3,11 +3,11 @@ class Solution {
         int stack[] = new int[nums.length];
         int n = nums.length, top = -1;
         int[] nge = new int[n];
-        for(int i = 2 * n-1 ; i >= 0; i--) {
-            while(top != -1 && nums[stack[top]] <= nums[i % n])
+        for(int i = 2*n-1; i >= 0; i--) {
+            while(top!=-1 && nums[i % n] > nums[stack[top]]) 
                 top--;
-            nge[i % n] = top == -1 ? top : nums[stack[top]];
-            stack[++top] = i % n;
+            nge[i] = top!= -1 ? nums2[stack[top]] : -1;
+            stack[++top] = i;
         }
         return nge;
     }
