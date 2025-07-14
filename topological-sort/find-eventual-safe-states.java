79770c74008bd840699b5ecd52ec2a6ea93757c3
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-14 15:51:13 +0530

    LC-find-eventual-safe-states [Runtime: 4 ms; Memory: 55.2 MB]

--- topological-sort/find-eventual-safe-states.java
+++ topological-sort/find-eventual-safe-states.java
@@ -1,19 +1,19 @@
 class Solution {
-    public boolean cycle(int node, int[] cyc, boolean[] pathVis, int[][] graph) {
+    public boolean isUnsafe(int node, int[] cyc, boolean[] pathVis, int[][] graph) {
         cyc[node] = 1;
         pathVis[node] = true;
-        boolean isCycle = false;
-        for(int vtx : graph[node]) {
-            if(cyc[vtx]==0) 
-                isCycle |= cycle(vtx, cyc, pathVis, graph);
+        boolean isUnsafe = false;
+        for(int vtx: graph[node]) {
+            if(cyc[vtx] == 0)
+                isUnsafe |= isUnsafe(vtx, cyc, pathVis, graph);
             else if(pathVis[vtx] || cyc[vtx] == 2) {
                 cyc[node] = 2;
                 return true;
             }
         }
-        cyc[node] = isCycle ? 2 : 1;
+        cyc[node] = isUnsafe ? 2 : 1;
         pathVis[node] = false;
-        return isCycle;
+        return isUnsafe;
     }
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
@@ -22,7 +22,7 @@ class Solution {
         boolean[] pathVis = new boolean[n];
         for(int i = 0; i < n; i++) {
             if(cyc[i]==0)
-                cycle(i, cyc, pathVis, graph);
+                isUnsafe(i, cyc, pathVis, graph);
         }
 
         List<Integer> res = new ArrayList<>();
