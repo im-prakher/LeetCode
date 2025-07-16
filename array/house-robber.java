7801cd3912aceacb0de0c7cf1566ff92664da1dd
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-16 21:49:53 +0530

    LC-house-robber [Runtime: 0 ms; Memory: 41.3 MB]

--- array/house-robber.java
+++ array/house-robber.java
@@ -5,7 +5,7 @@ class Solution {
         int a = nums[0], b = nums[1], c = Math.max(a, b);
         for (int i = 2; i < nums.length; i++) {
             c = Math.max(nums[i] + a, b);
-            a = b;
+            a = Math.max(a, b);
             b = c;
         }
         return c;
