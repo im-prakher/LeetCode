c7b71d1b7e0fce3b28fdc3274f76eb6eb8bffc31
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-17 17:32:52 +0530

    LC-edit-distance [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/edit-distance.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int editDistance(String a, String b) {
+        int m = a.length(), n = b.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(a.charAt(i-1) == b.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], 
+                        Math.min(dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public int minDistance(String a, String b) {
+        return 1 + editDistance(a, b);
+    }
+}
