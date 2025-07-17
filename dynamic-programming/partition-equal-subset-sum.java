fd435132537c2ebec6640f4a8e77b53a64777f6c
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-17 07:30:14 +0530

    LC-partition-equal-subset-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/partition-equal-subset-sum.java
+++ dynamic-programming/partition-equal-subset-sum.java
@@ -6,13 +6,17 @@ class Solution {
         if(sum % 2 != 0)
             return false;
         int target = sum >> 1, n = nums.length;
-        boolean dp[] = new boolean[target+1];
-        dp[0] = true;
+        boolean[][] dp = new boolean[n+1][target+1];
         for(int i = 0; i < n; i++) {
-            for(int j = target; j >= nums[i]; j--) {
-                dp[j] = dp[j] || dp[j-nums[i]];
+            dp[i][0] = true;
+        }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= target; j++) {
+                dp[i][j] = dp[i-1][j-1];
+                if(j >= nums[i-1])
+                    dp[i][j] |= dp[i-1][j-nums[i-1]];
             }
         }
-        return dp[target];
+        return dp[n][target];
     }
 }
