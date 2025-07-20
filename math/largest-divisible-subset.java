ee08404e595a87b580108eabe9dc62e8db7818d1
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-20 08:34:57 +0530

    LC-largest-divisible-subset [Runtime: 14 ms; Memory: 43.8 MB]

--- math/largest-divisible-subset.java
+++ math/largest-divisible-subset.java
@@ -5,27 +5,28 @@ class Solution {
         Arrays.sort(nums);
         int dp[] = new int[n];
         int[] hash = new int[n];
-        for(int i = 0 ; i < n; i++)
-            hash[i] = i;
         Arrays.fill(dp, 1);
-        int mx = 0, k = 0;
-        for(int i = 1; i < n; i++) {
+        int k = 0, res = 0;
+        for(int i = 0; i < n; i++) {
+            hash[i] = i;
             for(int j = 0; j < i; j++) {
-                if(nums[i] % nums[j] == 0 && dp[i] < dp[j]+1) {
-                    dp[i] = dp[j] + 1;
-                    hash[i] = j;
-                    if(mx < dp[i]) {
-                        mx = dp[i];
-                        k = i;
+                if(nums[i] % nums[j] == 0) {
+                    if(dp[i] <= dp[j]) {
+                        dp[i] = 1 + dp[j];
+                        hash[i] = j;
                     }
                 }
+            } 
+            if(res < dp[i]) {
+                res = dp[i];
+                k = i;
             }
         }
-        while(hash[k] != k) {
-            ans.add(nums[k]);
-            k = hash[k];
-        }
         ans.add(nums[k]);
+        while(hash[k] != k) {
+            k = hash[k];
+            ans.add(nums[k]);
+        }
         return ans;
     }
 }
