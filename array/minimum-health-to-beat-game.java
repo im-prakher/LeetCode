6a1adc8d0c025b007fe44cf396ccb095e86119b8
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-20 21:11:13 +0530

    LC-minimum-health-to-beat-game [Runtime: 2 ms; Memory: 57.1 MB]

---
+++ array/minimum-health-to-beat-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long minimumHealth(int[] damage, int armor) {
+        int max = 0; long sum = 0;
+        for(int i : damage) {
+            max = Math.max(max, i);
+            sum += i;
+        }
+        long ans = sum - Math.min(max, armor) + 1;
+        return ans;
+    }
+}
