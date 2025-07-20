c211011d1367761548dce5ec0336c5d2f6d53c2f
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-20 08:09:16 +0530

    LC-shortest-common-supersequence [Runtime: N/A; Memory: N/A]

--- string/shortest-common-supersequence.java
+++ string/shortest-common-supersequence.java
@@ -15,11 +15,18 @@ class Solution {
     }
     public String shortestCommonSupersequence(String str1, String str2) {
         int[][] dp = lcsMat(str1, str2);
-        int i = dp.length, j = dp[0].length;
-        while(i > 0 && j > 0) {
-            if(dp[i][j] == dp[i-1][j]) {
-
-            } else if {}
+        int i = dp.length-1, j = dp[0].length-1;
+        StringBuilder res = new StringBuilder();
+        while(i > 0 || j > 0) {
+            if(i > 0 && dp[i][j] == dp[i-1][j]) {
+                res.append(str1.charAt(--i));
+            } else if (j > 0 && dp[i][j] == dp[i][j-1]) {
+                res.append(str2.charAt(--j));
+            } else {
+                res.append(str1.charAt(--i));
+                --j;
+            }
         }
+        return res.toString();
     }
 }
