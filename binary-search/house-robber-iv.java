7d08b0037d76e51e8e66380348d2fb51397159b6
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-21 16:27:33 +0530

    LC-house-robber-iv [Runtime: N/A; Memory: N/A]

---
+++ binary-search/house-robber-iv.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canRob(int[] nums, int cap, int k) {
+        int cnt = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] <= cap) {
+                i++;
+                cnt++;
+            }
+        }
+        return cnt >= k;
+    }
+
+    public int minCapability(int[] nums, int k) {
+        int i = nums[0], j = nums[0];
+        for(int n : nums) {
+            i = Math.min(i, n);
+            j = Math.max(j, n);
+        }
+        int ans = j;
+        while(i < j) {
+            int mid = (i + j) >> 1;
+            if(canRob(nums, mid, k)) {
+                ans = mid;
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
