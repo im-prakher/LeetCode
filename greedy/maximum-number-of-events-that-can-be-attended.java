981ab612dc148a7fbccd683c3adc38e706c57079
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-23 23:27:57 +0530

    LC-maximum-number-of-events-that-can-be-attended [Runtime: N/A; Memory: N/A]

--- greedy/maximum-number-of-events-that-can-be-attended.java
+++ greedy/maximum-number-of-events-that-can-be-attended.java
@@ -1,13 +1,24 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        Arrays.sort(events, (a, b)-> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
-        int time = events[0][0], attend = 1;
-        for(int i = 1; i < events.length; i++) {
-            if(time < events[i][1]) {
+        Arrays.sort(events, (a, b)-> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        int maxTime = events[0][1], attend = 0, i = 0;
+        for(int[] event : events) {
+            maxTime = Math.max(maxTime, event[1]);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+        for(int time = 1; time <= maxTime; time++) {
+            while(i != events.length && events[i][0] <= time) {
+                pq.offer(events[i++]);
+            }
+            if(!pq.isEmpty() && pq.peek()[0] <= time) {
+                pq.poll();
                 attend++;
-                time = Math.max(events[i][0], time + 1);
             }
         }
         return attend;
     }
 }
+
+// [[1,5],[4,4],[2,2],[3,4],[1,1]]
+// [[1,5],[1,5],[1,5],[2,3],[2,3]]
+// 1 2 3 4 5
