8a764b8187fcb10950b7ab241bda4bb40259128e
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-23 11:42:05 +0530

    LC-maximum-number-of-events-that-can-be-attended [Runtime: N/A; Memory: N/A]

---
+++ sorting/maximum-number-of-events-that-can-be-attended.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxEvents(int[][] events) {
+        Arrays.sort(events, (a, b)-> a[0] == b[0] ? a[0] - b[0] : a[1] - b[1]);
+        int time = events[0][0], attend = 1;
+        for(int i = 1; i < events.length; i++) {
+            if(time < events[i][1]) {
+                attend++;
+                time = Math.max(events[i][0], time + 1);
+            }
+        }
+        return attend;
+    }
+}
