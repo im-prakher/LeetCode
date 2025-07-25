f30dd62de4cfacbd68269e0d0af3ee06bc5420be
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-25 08:08:22 +0530

    LC-all-nodes-distance-k-in-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Map<TreeNode, Integer> lvl = new HashMap<>();
+    int fillLevel(TreeNode root, TreeNode target) {
+        if(root == null)
+            return -1;
+        if(root == target) 
+            return 0;
+        int left = fillLevel(root.left, target);
+        int right = fillLevel(root.right, target);
+        int res = 1 + Math.max(left, right);
+        if(left != -1 || right != -1) {
+            lvl.put(root, res);
+            return res;
+        }
+        return -1;
+    }
+
+    public void kthLevel(TreeNode root, int k, Set<Integer> set) {
+        if(root == null || k < 0)
+            return;
+        if(k == 0) {
+            set.add(root.val);
+            return;
+        }
+        kthLevel(root.left, k-1, set);
+        kthLevel(root.right, k-1, set);
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        lvl.put(target, 0);
+        fillLevel(root, target);
+        List<Integer> ans = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for(var entry : lvl.entrySet()) {
+            if(entry.getValue() > k)
+                continue;
+            kthLevel(entry.getKey(), k - entry.getValue(), set);
+        }
+        set.remove(target.val);
+        return new ArrayList<>(set);
+    }
+}
