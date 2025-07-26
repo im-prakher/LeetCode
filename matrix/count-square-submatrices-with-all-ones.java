fac22b5467a215008e14a9f4cf5f0773770beae4
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-26 17:21:41 +0530

    LC-count-square-submatrices-with-all-ones [Runtime: 8 ms; Memory: 54.9 MB]

--- matrix/count-square-submatrices-with-all-ones.java
+++ matrix/count-square-submatrices-with-all-ones.java
@@ -3,15 +3,19 @@ class Solution {
         int n = mat.length, m = mat[0].length;
         int[][] dp = new int[n][m];
         int sum = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(i == 0 || j == 0)
-                    dp[i][j] = mat[i][j];
-                else if(mat[i][j] == 1)                 
-                    dp[i][j] = Math.min(dp[i-1][j-1], 
-                                Math.min(dp[i-1][j], dp[i][j-1])) + 1;
-                else
-                    dp[i][j] = 0;
+        for(int i = 0; i < m; i++){
+            dp[0][i] = mat[0][i];
+            sum += dp[0][i];
+        }
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = mat[i][0];
+            sum += dp[i][0];
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                int min = Math.min(dp[i-1][j-1], 
+                    Math.min(dp[i-1][j], dp[i][j-1]));
+                dp[i][j] = mat[i][j] == 1 ? min + 1 : 0;
                 sum += dp[i][j];
             }
         }
