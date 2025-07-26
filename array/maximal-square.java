5c5f435f164d1669b358c3d6c9a6d9880fcd2089
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-26 17:57:44 +0530

    LC-maximal-square [Runtime: 10 ms; Memory: 60.4 MB]

--- array/maximal-square.java
+++ array/maximal-square.java
@@ -3,19 +3,14 @@ class Solution {
         int n = mat.length, m = mat[0].length;
         int dp[][] = new int[n][m];
         int maxLen = 0;
-        for(int i = 0; i < m; i++) {
-            dp[0][i] = mat[0][i] - '0';
-            maxLen = Math.max(maxLen, dp[0][i]);
-        }
-        for(int j = 0; j < n; j++) {
-            dp[j][0] = mat[j][0] - '0';
-            maxLen = Math.max(maxLen, dp[j][0]);
-        }
-        for(int i = 1; i < n; i++) {
-            for(int j = 1; j < m; j++) {
-                if(mat[i][j] == '1')
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int digit = mat[i][j] == '1' ? 1 : 0;
+                if(i == 0 || j == 0)
+                    dp[i][j] = digit;
+                else if(digit == 1)                 
                     dp[i][j] = Math.min(dp[i-1][j-1], 
-                                    Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                                Math.min(dp[i-1][j], dp[i][j-1])) + 1;
                 else
                     dp[i][j] = 0;
                 maxLen = Math.max(maxLen, dp[i][j]);
