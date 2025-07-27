67d367c78116eab5bc3aa6b45a07a37e0a847ffe
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-27 14:17:13 +0530

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- array/jump-game-ii.java
+++ array/jump-game-ii.java
@@ -1,6 +1,8 @@
 class Solution {
     public int jump(int[] nums) {
-          int jmp = nums[0], idx = 0, n = nums.length, hops = 1;
+        if(nums.length == 1)
+            return 0;
+        int jmp = 0, idx = 0, n = nums.length, hops = 0;
         for(int i : nums) {
             if(jmp < i) {
                 hops++;
