aa25b916a4f77b40029a4b6c526dba8634b8de52
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-27 14:11:42 +0530

    LC-jump-game [Runtime: 2 ms; Memory: 45.9 MB]

--- array/jump-game.java
+++ array/jump-game.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int jmp = 0, idx = 0;
+        int jmp = 0, idx = 0, n = nums.length;
         for(int i : nums) {
             jmp = Math.max(jmp, i);
-            if(jmp + idx >= nums.length-1)
+            if(jmp + idx >= n-1)
                 return true;
-            else if(jmp == 0)
+            if(jmp == 0)
                 return false;
             jmp--;
             idx++;
