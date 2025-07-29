9d18ba5daa1dceff758a5a3273724d19657acc55
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-29 11:48:26 +0530

    LC-shortest-path-in-binary-matrix [Runtime: N/A; Memory: N/A]

--- matrix/shortest-path-in-binary-matrix.java
+++ matrix/shortest-path-in-binary-matrix.java
@@ -1,25 +1,21 @@
 class Solution {
-    boolean valid(int nx, int ny, int n, int m) {
-        return nx >= 0 && nx < n && ny >= 0 && ny < m; 
-    }
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid[0][0] == 1)
-            return -1;
-        Queue<int[]> que = new LinkedList<>();
-        que.offer(new int[]{0, 0, 1});
-        int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}, dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
-        grid[0][0] = 1;
+        int dx[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+        int dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+        PriorityQueue<int[]> que = new PriorityQueue<>((a, b)-> a[0]- b[0]);
         int n = grid.length, m = grid[0].length;
+        if(grid[0][0] == 0)
+            que.offer(new int[]{0,0,1});
         while(!que.isEmpty()) {
-            int arr[] = que.poll();
-            int x = arr[0], y = arr[1], dist = arr[2];
-            if(x == n-1 && y == m-1)
-                return dist;
+            int[] node = que.poll();
+            int i = node[0], j = node[1], dis = node[2];
+            if(i == n-1 && j == m-1)
+                return node[2];
             for(int k = 0; k < 8; k++) {
-                int nx = dx[k] + x, ny = dy[k] + y;
-                if(valid(nx, ny, n, m) && grid[nx][ny] == 0) {
-                    que.offer(new int[]{nx, ny, dist + 1});
+                int nx = i + dx[k], ny = j + dy[k];
+                if(nx >= 0 && nx< n && ny >= 0 && ny < m && grid[nx][ny]== 0) {
                     grid[nx][ny] = 1;
+                    que.offer(new int[]{nx, ny, 1 + dis});
                 }
             }
         }
