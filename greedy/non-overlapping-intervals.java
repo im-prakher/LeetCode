02ebd7917bc5e6165956639ac1999d6114f30bee
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-07-31 04:53:55 +0530

    LC-non-overlapping-intervals [Runtime: N/A; Memory: N/A]

--- greedy/non-overlapping-intervals.java
+++ greedy/non-overlapping-intervals.java
@@ -1,6 +1,6 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        Arrays.sort(intervals, (a, b) -> a[0]!=b[0] ? a[0] - b[0] : a[1] - b[1]);
         int ans = 0, k = intervals[0][1];
         for(int i = 1; i < intervals.length; i++) {
             int x = intervals[i][0];
