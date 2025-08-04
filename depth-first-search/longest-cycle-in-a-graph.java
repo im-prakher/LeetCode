9e6e8a2ef4808208984cc459941b921915692b08
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-08-04 23:54:51 +0530

    LC-longest-cycle-in-a-graph [Runtime: 23 ms; Memory: 65.5 MB]

--- depth-first-search/longest-cycle-in-a-graph.java
+++ depth-first-search/longest-cycle-in-a-graph.java
@@ -1,11 +1,13 @@
 class Solution {
     public int dfs(int node, int len, int[] cyc, boolean[] vis, int[] edges) {
+        if(cyc[node] != -1)
+            return len - cyc[node] + 1; 
+        if(vis[node])
+            return -1;
         int vtx = edges[node];
         vis[node] = true;
         if(vtx == -1)
             return -1;
-        if(cyc[node] != -1)
-            return len - cyc[node] + 1; 
         cyc[node] = ++len;
         int ans = dfs(vtx, len, cyc, vis, edges);
         cyc[node] = -1;
