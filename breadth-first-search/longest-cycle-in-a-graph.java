c8223894f8ed92a206ef8a545c722f2427bb8da2
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-08-04 23:52:45 +0530

    LC-longest-cycle-in-a-graph [Runtime: N/A; Memory: N/A]

--- breadth-first-search/longest-cycle-in-a-graph.java
+++ breadth-first-search/longest-cycle-in-a-graph.java
@@ -1,27 +1,26 @@
 class Solution {
-    public int dfs(int node, int[] cyc, int[] edges) {
+    public int dfs(int node, int len, int[] cyc, boolean[] vis, int[] edges) {
         int vtx = edges[node];
+        vis[node] = true;
         if(vtx == -1)
             return -1;
-        int ans = -1;
-        if(cyc[vtx] != -1)
-            return cyc[node] - cyc[vtx] + 1;
-        cyc[vtx] = cyc[node] + 1;
-        ans = Math.max(ans, dfs(vtx, cyc, edges));
-        cyc[vtx] = -1;
+        if(cyc[node] != -1)
+            return len - cyc[node] + 1; 
+        cyc[node] = ++len;
+        int ans = dfs(vtx, len, cyc, vis, edges);
+        cyc[node] = -1;
         return ans;
     }
 
     public int longestCycle(int[] edges) {
         int n = edges.length;
         int[] cyc = new int[n];
+        boolean[] vis = new boolean[n];
         Arrays.fill(cyc, -1);
         int ans = -1;
         for(int i = 0; i < n; i++) {
-            if(cyc[i] == -1) {
-                cyc[i] = 0;
-                ans = Math.max(ans, dfs(i, cyc, edges));
-                cyc[i] = -1;
+            if(!vis[i]) {
+                ans = Math.max(ans, dfs(i, 0, cyc, vis, edges));
             }
         }
         return ans;
