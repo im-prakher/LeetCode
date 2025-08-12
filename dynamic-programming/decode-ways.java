db4665e1931b9cde3740b1a36d75fefb5b9032ab
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-08-12 09:32:38 +0530

    LC-decode-ways [Runtime: 2 ms; Memory: 42.4 MB]

--- dynamic-programming/decode-ways.java
+++ dynamic-programming/decode-ways.java
@@ -1,14 +1,21 @@
 class Solution {
-    public int numDecodings(String s) {
+    int solve(String s, Map<String, Integer> dp) {
+        if(dp.containsKey(s))
+            return dp.get(s);
         if(s.length() == 0)
             return 1;
         if(s.charAt(0) == '0')
             return 0;
         int ways = 0;
         if(s.length() >=2 && Integer.valueOf(s.substring(0, 2)) < 27) {
-            ways += numDecodings(s.substring(2));
+            ways += solve(s.substring(2), dp);
         } 
-        ways += numDecodings(s.substring(1));
+        ways += solve(s.substring(1), dp);
+        dp.put(s, ways);
         return ways;
+    }
+    public int numDecodings(String s) {
+        Map<String, Integer> dp = new HashMap<>();
+        return solve(s, dp);
     }
 }
