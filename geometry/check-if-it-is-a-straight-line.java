8a736bb751c8a627571b44cb5a4afe3ac5bff5be
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-08-17 12:03:34 +0530

    LC-check-if-it-is-a-straight-line [Runtime: N/A; Memory: N/A]

--- geometry/check-if-it-is-a-straight-line.java
+++ geometry/check-if-it-is-a-straight-line.java
@@ -4,6 +4,7 @@ class Solution {
     }
     public boolean checkStraightLine(int[][] cord) {
         int n = cord.length;
+        Arrays.sort(cord, (a, b)-> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
         for(int i  = 1; i < n-1; i++) {
             if(abs(cord[i][0] - cord[i-1][0]) != abs(cord[i+1][0] - cord[i][0])
                 || abs(cord[i][1] - cord[i-1][1]) 
