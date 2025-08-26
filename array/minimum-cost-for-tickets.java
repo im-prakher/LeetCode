8eaade69106f042c6ed936a8cb7d53c4d90f4762
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-08-26 09:11:30 +0530

    LC-minimum-cost-for-tickets [Runtime: 3 ms; Memory: 42 MB]

--- array/minimum-cost-for-tickets.java
+++ array/minimum-cost-for-tickets.java
@@ -1,14 +1,13 @@
 class Solution {
-     int[] dp;
-    public int solve(int[] costs, TreeSet<Integer> tset, Integer day) {
+    public int solve(int[] dp, int[] costs, TreeSet<Integer> tset, Integer day) {
         if(day == null || day > tset.last())
             return 0;
         if(dp[day]!=-1)
             return dp[day];
-        return dp[day] =  Math.min(costs[0] + solve(costs, tset, tset.ceiling(day + 1)),
+        return dp[day] =  Math.min(costs[0] + solve(dp, costs, tset, tset.ceiling(day + 1)),
             Math.min(
-                costs[1] + solve(costs, tset, tset.ceiling(day + 7)),
-                costs[2] + solve(costs, tset, tset.ceiling(day + 30))
+                costs[1] + solve(dp, costs, tset, tset.ceiling(day + 7)),
+                costs[2] + solve(dp, costs, tset, tset.ceiling(day + 30))
             )
         );
     }
@@ -18,8 +17,8 @@ class Solution {
         for(int d : days) {
             tset.add(d);
         }
-        dp = new int[366];
+        int[] dp = new int[366];
         Arrays.fill(dp, -1);
-        return solve(costs, tset, days[0]);
+        return solve(dp, costs, tset, days[0]);
     }
 }
