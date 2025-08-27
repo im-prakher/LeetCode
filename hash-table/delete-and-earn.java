2ec23c5c76bd30cadf6df1b2cf17d838c5831d01
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-08-27 06:57:16 +0530

    LC-delete-and-earn [Runtime: 7 ms; Memory: 45.6 MB]

--- hash-table/delete-and-earn.java
+++ hash-table/delete-and-earn.java
@@ -1,7 +1,9 @@
 class Solution {
-    public int solve(int nums[], int idx) {
+    public int solve(int nums[], int idx, Map<Integer, Integer> dp) {
         if(idx == nums.length)
             return 0;
+        if(dp.containsKey(idx))
+            return dp.get(idx);
         int i = idx, take = 0;
         for(i = idx; i < nums.length; i++) {
             if(nums[i] != nums[idx])
@@ -12,11 +14,14 @@ class Solution {
             if(nums[idx] + 1 != nums[i]) 
                 break;
         }
-        return Math.max(take + solve(nums, i), solve(nums, idx+1));
+        int ans = Math.max(take + solve(nums, i, dp), solve(nums, idx+1, dp));
+        dp.put(idx, ans);
+        return ans;
     }
 
     public int deleteAndEarn(int[] nums) {
         Arrays.sort(nums);
-        return solve(nums, 0);
+        Map<Integer, Integer> dp = new HashMap<>();
+        return solve(nums, 0, dp);
     }
 }
