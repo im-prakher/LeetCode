7f83621de821a2eaa707adda25208174798dee6f
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-08-27 07:18:56 +0530

    LC-maximum-total-damage-with-spell-casting [Runtime: N/A; Memory: N/A]

--- sorting/maximum-total-damage-with-spell-casting.java
+++ sorting/maximum-total-damage-with-spell-casting.java
@@ -8,7 +8,7 @@ class Solution {
         for(int i : power) {
             dp[i] += i;
         }
-        if(power.length > 2)
+        if(max >= 2)
             dp[2] = Math.max(dp[2], dp[1]);
         for(int i = 3; i <= max; i++) {
             dp[i] = Math.max(dp[i-2], Math.max(dp[i-1], dp[i-3] + dp[i]));
