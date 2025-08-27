b4ba091f9254b88b1b64608bd530303b1a4ab6c3
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-08-27 07:16:48 +0530

    LC-maximum-total-damage-with-spell-casting [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-total-damage-with-spell-casting.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long maximumTotalDamage(int[] power) {
+        int max = 0;
+        for(int i : power) {
+            max = Math.max(max, i);
+        }
+        long dp[] = new long[max+1];
+        for(int i : power) {
+            dp[i] += i;
+        }
+        for(int i = 3; i <= max; i++) {
+            dp[i] = Math.max(dp[i-2], Math.max(dp[i-1], dp[i-3] + dp[i]));
+        }
+        return dp[max];
+    }
+}
