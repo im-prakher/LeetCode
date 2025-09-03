47d213361cbce527222c7c67dde16fe35dca171d
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-03 10:07:05 +0530

    LC-maximum-population-year [Runtime: N/A; Memory: N/A]

---
+++ counting/maximum-population-year.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+        int year[] = new int[2052];
+        for(int[] log : logs) {
+            year[log[0]]++;
+            year[log[1]+1]--;
+        }
+        int pop = 0, maxPop = 0, earlyYear = 1950;
+        for(int i = 1950; i < 2052; i++) {
+            pop += year[i];
+            if(pop > maxPop) {
+                earlyYear = i;
+                maxPop = pop;
+            }
+        }
+        return earlyYear;
+    }
+}
