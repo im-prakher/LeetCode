5755b0e986d14fb60f169dc9f27643b0c9bb103a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-08 08:10:50 +0530

    LC-dota2-senate [Runtime: N/A; Memory: N/A]

--- greedy/dota2-senate.java
+++ greedy/dota2-senate.java
@@ -1,17 +1,27 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        int cntR = 0, exR = 0, cntD = 0, exD = 0;
+        int cntR = 0, revR= 0, exR = 0, cntD = 0, exD = 0, revD= 0;
         for(char s : senate.toCharArray()) {
             if(s == 'D') {
-                if(cntR <= exR) 
-                    cntD++;
-                exR++;
+                if(cntR > exR) {
+                    exR++;
+                    revD++;
+                } else if(revR < cntR) {
+                    exD++;
+                    revR++;
+                }
+                cntD++;
             } else {
-                if(cntD <= exD)            
-                    cntR++; 
-                exD++;
+                if(cntD > exD) {
+                    exD++;
+                    revR++;
+                } else if(revD < cntD) {
+                    exR++;
+                    revD++;
+                }
+                cntR++;
             }
         }
-        return (cntD - exD) > (cntR - exR) ? "Dire" : "Radiant";
+        return (cntD - revD - exR) > (cntR - revR - exD) ? "Dire" : "Radiant";
     }
 }
