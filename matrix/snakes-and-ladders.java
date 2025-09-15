3afc9d678ca1efc3bcad41a7cc5a5b63b1241077
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-15 06:34:44 +0530

    LC-snakes-and-ladders [Runtime: 5 ms; Memory: 44.3 MB]

--- matrix/snakes-and-ladders.java
+++ matrix/snakes-and-ladders.java
@@ -1,12 +1,11 @@
 class Solution {
     public int row(int cell, int n) {
-        // if((cell / n) % 2 == 0)
-            return (cell-1) / n;
+        return (cell-1) / n;
     }
 
     public int col(int cell, int n) {
-        int c = (cell-1) % n;
-        if(((cell - 1) / n) % 2 == 0)
+        int c = (cell-1) % n, r = (cell - 1) / n;
+        if(r % 2 == 0)
             return c;
         return n - 1 - c;
     }
@@ -22,31 +21,25 @@ class Solution {
     }
 
     public int snakesAndLadders(int[][] board) {
-        // do bfs for next 6 cells
-        //if any cell has snake/ladder push that in queue
-        //if unable to reach last cell & queue becomes empty return -1
-        Queue<int[]> que = new LinkedList<>();
-        que.offer(new int[] {1, 0});
-        Set<Integer> vis = new HashSet<>();
+        Queue<Integer> que = new LinkedList<>();
         int n = board.length;
+        boolean [] vis = new boolean[n * n + 1];
+        int[] moves = new int[n * n + 1];
         reverse(board, n);
-        vis.add(1);
-        int ans = Integer.MAX_VALUE;
+        que.offer(1);
+        vis[1] = true;
         while(!que.isEmpty()) {
-            int[] cell = que.poll();
-            for(int k = cell[0]+1; k < Math.min(cell[0] + 7, n * n + 1); k++) {
+            int cell = que.poll();
+            for(int k = cell+1; k < Math.min(cell + 7, n * n + 1); k++) {
                 int i = row(k, n), j = col(k, n);
-                if(board[i][j] != -1) {
-                    if(!vis.contains(board[i][j])) {
-                        que.offer(new int[] {board[i][j], cell[1]+1});
-                        vis.add(board[i][j]);
-                    }
-                } else if(!vis.contains(k)) {
-                    que.offer(new int[] {k, cell[1]+1});
-                    vis.add(k);
+                int next = (board[i][j] != -1) ? board[i][j] : k;
+                if(next == n * n)
+                    return moves[cell] + 1;
+                if(!vis[next]) {
+                    que.offer(next);
+                    moves[next] = moves[cell] + 1;
                 }
-                if(board[i][j] == n * n || k == n*n)
-                    return cell[1] + 1;
+                vis[next] = true;
             }
         }
         return -1;
