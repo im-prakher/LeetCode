b9064b32e6bb0c70b88a6b20e663a9a3af7c55db
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-15 07:14:41 +0530

    LC-open-the-lock [Runtime: 117 ms; Memory: 49.4 MB]

--- string/open-the-lock.java
+++ string/open-the-lock.java
@@ -13,11 +13,12 @@ class Solution {
         mp.put("0000", 0);
         while(!que.isEmpty()) {
             String s = que.poll();
+            char[] atr = s.toCharArray();
+            char[] ntr = atr.clone();
             for(int i = 0; i < 4; i++) {
-                char[] atr = s.toCharArray();
-                char[] ntr = atr.clone();
-                atr[i] = (atr[i] == '9') ? '0' : (char) (atr[i] + 1);
-                ntr[i] = (ntr[i] == '0') ? '9' : (char) (ntr[i] - 1);
+                char ch = atr[i];
+                atr[i] = (ch == '9') ? '0' : (char) (ch + 1);
+                ntr[i] = (ch == '0') ? '9' : (char) (ch - 1);
                 String a = new String(atr);
                 String n = new String(ntr);
                 if(a.equals(target) || n.equals(target)) 
@@ -32,6 +33,7 @@ class Solution {
                 }
                 vis.add(a);
                 vis.add(n);
+                atr[i] = ntr[i] = ch;
             }
         }
         return -1;
