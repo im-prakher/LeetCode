8226c51980fd4c6faa37f80e06a32ade5df64707
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-16 17:21:39 +0530

    LC-maximum-non-negative-product-in-a-matrix [Runtime: N/A; Memory: N/A]

--- dynamic-programming/maximum-non-negative-product-in-a-matrix.java
+++ dynamic-programming/maximum-non-negative-product-in-a-matrix.java
@@ -1,25 +1,27 @@
 class Solution {
     public int maxProductPath(int[][] grid) {
         int n = grid.length, m = grid[0].length;
+        int mod = (int) 1e9 + 7;
         int[][] dp = new int[n][m];
         int[][] neg = new int[n][m];
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
+                int val = grid[i][j];
                 if(i == 0 && j == 0)
-                    dp[i][j] = neg[i][j] = grid[i][j];
+                    dp[i][j] = neg[i][j] = val;
                 else if(i == 0)
-                    dp[i][j] = neg[i][j] = grid[i][j] * dp[i][j-1];
+                    dp[i][j] = neg[i][j] = (val * (dp[i][j-1])) % mod;
                 else if(j == 0)
-                    dp[i][j] = neg[i][j] = grid[i][j] * dp[i-1][j];
+                    dp[i][j] = neg[i][j] = (val * (dp[i-1][j])) % mod;
                 else {
                     dp[i][j] = Math.max(
-                        grid[i][j] * Math.max(dp[i-1][j], dp[i][j-1]),
-                        grid[i][j] * Math.min(neg[i-1][j], neg[i][j-1])
-                        );
+                        val * (Math.max(dp[i-1][j], dp[i][j-1])),
+                        val * (Math.min(neg[i-1][j], neg[i][j-1]))
+                        ) % mod;
                     neg[i][j] = Math.min(
-                        grid[i][j] * Math.max(dp[i-1][j], dp[i][j-1]),
-                        grid[i][j] * Math.min(neg[i-1][j], neg[i][j-1])
-                        );
+                        val * (Math.max(dp[i-1][j], dp[i][j-1])),
+                        val * (Math.min(neg[i-1][j], neg[i][j-1]))
+                        ) % mod;
                 }
             }
         }
