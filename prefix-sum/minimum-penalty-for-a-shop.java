4e8bdebf3be00d3e4be7dee223f659d74a970942
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-22 05:53:01 +0530

    LC-minimum-penalty-for-a-shop [Runtime: 8 ms; Memory: 45.5 MB]

--- prefix-sum/minimum-penalty-for-a-shop.java
+++ prefix-sum/minimum-penalty-for-a-shop.java
@@ -2,9 +2,9 @@ class Solution {
     public int bestClosingTime(String customers) {
         int visit = 0, no_visit = 0;
         char[] cust = customers.toCharArray();
-        for(char ch : cust) {
-            visit += (ch == 'Y') ? 1 : 0;
-        }
+        // for(char ch : cust) {
+        //     visit += (ch == 'Y') ? 1 : 0;
+        // }
         int ans = -1, min = cust.length;
         for(int i = 0; i <= cust.length; i++) {
             if(min > visit + no_visit) {
