44852e9ee6fc43f65493679f64fc660315727625
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-22 06:34:41 +0530

    LC-sum-of-absolute-differences-in-a-sorted-array [Runtime: 5 ms; Memory: 59 MB]

--- array/sum-of-absolute-differences-in-a-sorted-array.java
+++ array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -3,10 +3,9 @@ class Solution {
         int pre = 0, suf = 0;
         int n = nums.length;
         int[] ans = new int[n];
-        for(int i = n-1; i >= 0; i--) {
+        for(int i = n-1, j = 0; i >= 0; i--, j++) {
             suf += nums[i];
             ans[i] += suf - (n-i) * nums[i];
-            int j = n - 1 - i;
             pre += nums[j];
             ans[j] += (j+1) * nums[j] - pre;
         }
