ad87badd13e2b824612977984bdbb2681973f9c9
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-22 05:44:42 +0530

    LC-minimum-penalty-for-a-shop [Runtime: 10 ms; Memory: 45.3 MB]

--- string/minimum-penalty-for-a-shop.java
+++ string/minimum-penalty-for-a-shop.java
@@ -1,23 +1,20 @@
 class Solution {
     public int bestClosingTime(String customers) {
+        int visit = 0, no_visit = 0;
         char[] cust = customers.toCharArray();
-        int n = cust.length;
-        int[] onClose = new int[n+2];
-        int[] onOpen = new int[n+2];
-        for(int i = 1; i <= n; i++) {
-            onClose[i] += onClose[i-1] + (cust[i-1] == 'Y' ? 1 : 0);
-            onOpen[i] += onOpen[i-1] + (cust[i-1] == 'N' ? 1 : 0);
+        for(char ch : cust) {
+            visit += (ch == 'Y') ? 1 : 0;
         }
-        onClose[n+1] = onClose[n];
-        onOpen[n+1] = onOpen[n];
-        int ans = 0, min = onClose[n+1] - onClose[0] + onOpen[0];
-        int penalty;
-        for(int i = 2; i <= n+1; i++) {
-            penalty = onClose[n+1] - onClose[i-1] + onOpen[i-1];
-            if(min > penalty) {
-                ans = i-1;
-                min = penalty;
+        int ans = -1, min = cust.length;
+        for(int i = 0; i <= cust.length; i++) {
+            if(min > visit + no_visit) {
+                min = visit + no_visit;
+                ans = i;
             }
+            if(i == cust.length)
+                break;
+            visit -= cust[i] == 'Y' ? 1 : 0;
+            no_visit += cust[i] == 'N' ? 1 : 0;
         }
         return ans;
     }
