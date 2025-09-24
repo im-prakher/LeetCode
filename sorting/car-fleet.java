39ab0e5426f1eb45e8571031164f2b188584951a
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-24 21:11:49 +0530

    LC-car-fleet [Runtime: N/A; Memory: N/A]

---
+++ sorting/car-fleet.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canMeet(int a, int x, int b, int y, int tar) {
+        while(a <= tar && b <= tar) {
+            if(a >= b)
+                return true;
+            a += x;
+            b += y;
+        }
+        return false;
+    }
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = speed.length, fleet = n, top = -1;
+        int[][] det = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            det[i][0] = position[i];
+            det[i][1] = speed[i];
+        }
+        Arrays.sort(det, (a, b)-> a[0] - b[0]);
+        int[][] stack = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            while(top!= -1 && canMeet(stack[top][0], stack[top][1],
+                det[i][0], det[i][1], target)) {
+                    top--;
+                    fleet--;
+            }
+            top++;
+            stack[top][0] = det[i][0];
+            stack[top][1] = det[i][1];
+        }
+        return fleet;
+    }
+}
