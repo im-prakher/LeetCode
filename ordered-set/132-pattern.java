71de18a637201b91f282ec1233eacf02fcf41455
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-26 13:06:27 +0530

    LC-132-pattern [Runtime: 5 ms; Memory: 67 MB]

--- ordered-set/132-pattern.java
+++ ordered-set/132-pattern.java
@@ -2,13 +2,19 @@ class Solution {
     public boolean find132pattern(int[] nums) {
         int n = nums.length, top = -1;
         int[] stack = new int[n];
-        for(int i = 0; i < n; i++) {
-            while(top!= -1 && nums[stack[top]] > nums[i]) {
-                if(top > 0 && stack[top-1] < nums[i])
-                    return true;
+        int[] min = new int[n];
+        min[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            min[i] = Math.min(min[i-1], nums[i]);
+        }
+        for(int i = n-1; i >= 0; i--) {
+            while(top!= - 1 && min[i] >= stack[top]) {
                 top--;
             }
-            stack[++top] = i;
+            if(top != -1 && stack[top] < nums[i])
+                return true;
+            if(min[i] < nums[i])
+                stack[++top] = nums[i];
         }
         return false;
     }
