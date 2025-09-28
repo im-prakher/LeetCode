c9ebe0be92bd1aa494244a1500a76f62905dec45
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-28 05:35:46 +0530

    LC-longest-arithmetic-subsequence-of-given-difference [Runtime: 55 ms; Memory: 57.6 MB]

--- dynamic-programming/longest-arithmetic-subsequence-of-given-difference.java
+++ dynamic-programming/longest-arithmetic-subsequence-of-given-difference.java
@@ -1,12 +1,12 @@
 class Solution {
     public int longestSubsequence(int[] arr, int difference) {
         int n = arr.length;
-        int nums[] = new int[100_00];
+        int ans = 0;
         Map<Integer, Integer> mp = new HashMap<>();
         for(int i = 0; i < n; i++) {
             mp.put(arr[i], 1 + mp.getOrDefault(arr[i]-difference, 0));
+            ans = Math.max(ans, mp.get(arr[i]));
         }
-        int res = mp.values().stream().max(Integer::compare).get();
-        return res;
+        return ans;
     }
 }
