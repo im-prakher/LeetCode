b3ea0645ebdb4f23a7f9d2b34707ec02fad2a993
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-30 21:58:00 +0530

    LC-wiggle-subsequence [Runtime: N/A; Memory: N/A]

--- greedy/wiggle-subsequence.java
+++ greedy/wiggle-subsequence.java
@@ -1,24 +1,50 @@
 class Solution {
-    boolean signMatch(List<Integer> seq, int num) {
-        if(seq.size() == 0)
+    Map<String, Map<Integer, Integer>> dp;
+    boolean signMatch(StringBuilder seq, int num) {
+        int n = seq.length();
+        if(n == 0)
             return true;
-        else if(seq.size() == 1)    
-            return seq.getLast() != num;
-        int val1 = seq.getLast()  - seq.get(seq.size()- 2);
-        int val2 = num - seq.getLast();
+        else if(n <= 3) {
+            String digit = "";
+            for(int i = n - 1; seq.charAt(i) != '_'; i--) {
+                digit = seq.charAt(i) + digit;
+            }
+            return Integer.valueOf(digit) != num;
+        }
+        int cnt = 2, i;
+        String digit = "";
+        for(i = n - 1; seq.charAt(i) != '_'; i--) {
+            digit = seq.charAt(i) + digit;
+        }
+        int last = Integer.valueOf(digit);
+        digit = "";
+        for( i = i-1; seq.charAt(i) != '_'; i--) {
+            digit = seq.charAt(i) + digit;
+        }
+        int secLast = Integer.valueOf(digit);
+        int val1 = last  - secLast;
+        int val2 = num - last;
         return (val1 > 0 && val2 < 0) || (val1 < 0 && val2 > 0);
     }
-    int func(int[] nums, int idx, List<Integer> seq) {
+    int func(int[] nums, int idx, StringBuilder seq, Map<String, Map<Integer,Integer>> dp) {
         if(idx == nums.length)
             return 0;
+        String str = seq.toString();
+        if(dp.containsKey(str) && dp.get(str).containsKey(idx))
+            return dp.get(str).get(idx);
         int pick = 0;
         if(signMatch(seq, nums[idx])) {
-            seq.add(nums[idx]);
-            pick = 1 + func(nums, idx + 1, seq);
-            seq.remove(seq.size()-1);
+            seq.append("_");
+            String digit = String.valueOf(nums[idx]);
+            seq.append(digit);
+            pick = 1 + func(nums, idx + 1, seq, dp);
+            seq.delete(seq.length() - digit.length() - 1, seq.length());
         }
-        int not_pick = func(nums, idx+1, seq);
-        return Math.max(pick, not_pick);
+        int not_pick = func(nums, idx+1, seq, dp);
+        int ans = Math.max(pick, not_pick);
+        dp.putIfAbsent(seq.toString(), new HashMap<>());
+        dp.get(seq.toString()).put(idx, ans);
+        return ans;
     }
 
     public int wiggleMaxLength(int[] nums) {
@@ -26,6 +52,7 @@ class Solution {
             return 1;
         if(nums.length == 2)
             return nums[0] != nums[1] ? 2 : 1;
-        return func(nums, 0, new ArrayList<Integer>());
+        dp = new HashMap<>();
+        return func(nums, 0, new StringBuilder(), dp);
     }
 }
