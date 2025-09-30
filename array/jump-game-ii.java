dc7519839a60e28d5d574dbf433c0978db2d4199
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-09-30 05:56:15 +0530

    LC-jump-game-ii [Runtime: 1 ms; Memory: 45.1 MB]

--- array/jump-game-ii.java
+++ array/jump-game-ii.java
@@ -1,18 +1,13 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length == 1)
-            return 0;
-        int jmp = 0, idx = 0, n = nums.length, hops = 0;
-        for(int i : nums) {
-            if(jmp < i) {
-                hops++;
-                jmp = i;
+        int jmp = 0, maxReach = 0, currReach = 0;
+        for(int i = 0; i < nums.length-1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if(i == currReach) {
+                jmp++;
+                currReach = maxReach;
             }
-            if(jmp + idx >= n-1)
-                break;
-            jmp--;
-            idx++;
         }
-        return hops;
+        return jmp;
     }
 }
