8c7f91d1336b33c7c77692830e632943ea3316ab
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-02 04:29:33 +0530

    LC-wiggle-subsequence [Runtime: 278 ms; Memory: 102 MB]

--- greedy/wiggle-subsequence.java
+++ greedy/wiggle-subsequence.java
@@ -1,58 +1,24 @@
 class Solution {
-    Map<String, Map<Integer, Integer>> dp;
-    boolean signMatch(StringBuilder seq, int num) {
-        int n = seq.length();
-        if(n == 0)
-            return true;
-        else if(n <= 3) {
-            String digit = "";
-            for(int i = n - 1; seq.charAt(i) != '_'; i--) {
-                digit = seq.charAt(i) + digit;
-            }
-            return Integer.valueOf(digit) != num;
-        }
-        int cnt = 2, i;
-        String digit = "";
-        for(i = n - 1; seq.charAt(i) != '_'; i--) {
-            digit = seq.charAt(i) + digit;
-        }
-        int last = Integer.valueOf(digit);
-        digit = "";
-        for( i = i-1; seq.charAt(i) != '_'; i--) {
-            digit = seq.charAt(i) + digit;
-        }
-        int secLast = Integer.valueOf(digit);
-        int val1 = last  - secLast;
-        int val2 = num - last;
-        return (val1 > 0 && val2 < 0) || (val1 < 0 && val2 > 0);
-    }
-    int func(int[] nums, int idx, StringBuilder seq, Map<String, Map<Integer,Integer>> dp) {
+    Map<String, Integer> dp;
+    public int func(int[] nums, int idx, boolean state, int prev) {
         if(idx == nums.length)
             return 0;
-        String str = seq.toString();
-        if(dp.containsKey(str) && dp.get(str).containsKey(idx))
-            return dp.get(str).get(idx);
-        int pick = 0;
-        if(signMatch(seq, nums[idx])) {
-            seq.append("_");
-            String digit = String.valueOf(nums[idx]);
-            seq.append(digit);
-            pick = 1 + func(nums, idx + 1, seq, dp);
-            seq.delete(seq.length() - digit.length() - 1, seq.length());
-        }
-        int not_pick = func(nums, idx+1, seq, dp);
+        String key = String.valueOf(state) + "_" + idx + "_" + prev;
+        if(dp.containsKey(key))
+            return dp.get(key);
+        int pick = 0, not_pick = 0;
+        if(state && prev < nums[idx]) 
+            pick = 1 + func(nums, idx + 1, !state, nums[idx]);
+        else if(!state && prev > nums[idx])
+            pick = 1 + func(nums, idx + 1, !state, nums[idx]);
+        not_pick = func(nums, idx + 1, state, prev);
         int ans = Math.max(pick, not_pick);
-        dp.putIfAbsent(seq.toString(), new HashMap<>());
-        dp.get(seq.toString()).put(idx, ans);
+        dp.put(key, ans);
         return ans;
     }
 
     public int wiggleMaxLength(int[] nums) {
-        if(nums.length == 1)
-            return 1;
-        if(nums.length == 2)
-            return nums[0] != nums[1] ? 2 : 1;
         dp = new HashMap<>();
-        return func(nums, 0, new StringBuilder(), dp);
+        return Math.max(func(nums, 0, true, -1), func(nums, 0, false, 1005));
     }
 }
