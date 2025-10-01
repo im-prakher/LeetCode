531ab889f2bb5896a0e1f68ef07bfe7ea4abbccf
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-02 05:26:30 +0530

    LC-wiggle-subsequence [Runtime: 0 ms; Memory: 41.5 MB]

--- dynamic-programming/wiggle-subsequence.java
+++ dynamic-programming/wiggle-subsequence.java
@@ -2,21 +2,15 @@ class Solution {
     //LIS DP
     public int wiggleMaxLength(int[] nums) {
         int n = nums.length;
-        int low[] = new int[n];
-        int high[] = new int[n];
-        low[0] = high[0] = 1;
+        int low, high;
+        low = high = 1;
         for(int i = 1; i < n; i++) {
             if(nums[i] > nums[i-1]) {
-                high[i] = low[i-1] + 1;
-                low[i] = low[i-1];
+                high = low + 1;
             } else if(nums[i] < nums[i-1]) { 
-                low[i] = high[i-1] + 1;
-                high[i] = high[i-1];
-            } else {
-                low[i] = low[i-1];
-                high[i] = high[i-1];
+                low = high + 1;
             }
         }
-        return Math.max(high[n-1], low[n-1]);
+        return Math.max(high, low);
     }
 }
