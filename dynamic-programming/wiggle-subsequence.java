0e21a3baea22e7f8a94393eaab959eaea6909d34
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-02 04:56:49 +0530

    LC-wiggle-subsequence [Runtime: 5 ms; Memory: 41.5 MB]

--- dynamic-programming/wiggle-subsequence.java
+++ dynamic-programming/wiggle-subsequence.java
@@ -1,24 +1,19 @@
 class Solution {
-    Map<String, Integer> dp;
-    public int func(int[] nums, int idx, boolean state, int prev) {
-        if(idx == nums.length)
-            return 0;
-        String key = String.valueOf(state) + "_" + idx + "_" + prev;
-        if(dp.containsKey(key))
-            return dp.get(key);
-        int pick = 0, not_pick = 0;
-        if(state && prev < nums[idx]) 
-            pick = 1 + func(nums, idx + 1, !state, nums[idx]);
-        else if(!state && prev > nums[idx])
-            pick = 1 + func(nums, idx + 1, !state, nums[idx]);
-        not_pick = func(nums, idx + 1, state, prev);
-        int ans = Math.max(pick, not_pick);
-        dp.put(key, ans);
-        return ans;
-    }
-
+    //LIS DP
     public int wiggleMaxLength(int[] nums) {
-        dp = new HashMap<>();
-        return Math.max(func(nums, 0, true, -1), func(nums, 0, false, 1005));
+        int n = nums.length;
+        int low[] = new int[n];
+        int high[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            low[i] = high[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    high[i] = Math.max(high[i], low[j] + 1);
+                } else if(nums[j] > nums[i]) {
+                    low[i] = Math.max(low[i], high[j] + 1);
+                }
+            }
+        }
+        return Math.max(high[n-1], low[n-1]);
     }
 }
