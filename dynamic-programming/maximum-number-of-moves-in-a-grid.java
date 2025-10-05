cb3072c7c5d3bdc4cb8d55406e3bad39a070a659
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-05 17:07:37 +0530

    LC-maximum-number-of-moves-in-a-grid [Runtime: N/A; Memory: N/A]

--- dynamic-programming/maximum-number-of-moves-in-a-grid.java
+++ dynamic-programming/maximum-number-of-moves-in-a-grid.java
@@ -2,20 +2,28 @@ class Solution {
 
     public int maxMoves(int[][] grid) {
         int n = grid.length, m = grid[0].length;
-        int[] dp = new int[m];
+        int[] prev = new int[m];
+        int[] next = new int[m];
         for(int j = m-2; j >= 0; j--) {
             for(int i = 0; i < n; i++) {
                 int up = i-1 < 0 ? Integer.MIN_VALUE : grid[i-1][j+1];
                 int down = i+1 == n ? Integer.MIN_VALUE : grid[i+1][j+1];
                 int cell = grid[i][j];
-                if(grid[i][j+1] > cell || up > cell || down > cell)
-                    dp[i]++;
+                if(grid[i][j+1] > cell)
+                    next[i] = prev[i] + 1;
+                else if(up > cell)
+                    next[i] = prev[i-1] + 1;
+                else if(down > cell)
+                    next[i] = prev[i+1] + 1;
                 else 
-                    dp[i] = 0;
+                    next[i] = 0;
             }
+            int[] tmp = prev;
+            prev = next;
+            next = tmp;
         }
         int ans = 0;
-        for(int i : dp) {
+        for(int i : prev) {
             ans = Math.max(ans, i);
         }
         return ans;
