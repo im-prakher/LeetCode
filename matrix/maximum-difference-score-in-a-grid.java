8f4c25be388fce5b575da9bb53fc4ae654a25090
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-05 15:15:40 +0530

    LC-maximum-difference-score-in-a-grid [Runtime: 26 ms; Memory: 61.9 MB]

--- matrix/maximum-difference-score-in-a-grid.java
+++ matrix/maximum-difference-score-in-a-grid.java
@@ -1,24 +1,17 @@
 class Solution {
-    int maxDiff;
-
-    public int func(List<List<Integer>> grid, int i, int j, boolean[][] vis) {
-        if (i == grid.size() || j == grid.get(0).size())
-            return 0;
-        int cell = grid.get(i).get(j);
-        if(vis[i][j])
-            return cell;
-        int max = Math.max(func(grid, i + 1, j, vis), func(grid, i, j + 1, vis));
-        maxDiff = Math.max(maxDiff, max - cell);
-        max = Math.max(cell, max);
-        grid.get(i).set(j, max);
-        vis[i][j] = true;
-        return max;
-    }
-
     public int maxScore(List<List<Integer>> grid) {
-        boolean[][] vis = new boolean[grid.size()][grid.get(0).size()];
-        maxDiff = Integer.MIN_VALUE;
-        func(grid, 0, 0, vis);
+        int maxDiff = Integer.MIN_VALUE;
+        int n = grid.size(), m = grid.get(0).size();
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j >= 0; j--) {
+                int left = j + 1 == m ? Integer.MIN_VALUE / 10: grid.get(i).get(j+1);
+                int right = i + 1 == n ? Integer.MIN_VALUE / 10 : grid.get(i+1).get(j);
+                int max = Math.max(left, right);
+                int cell = grid.get(i).get(j);
+                maxDiff = Math.max(maxDiff, max - cell);
+                grid.get(i).set(j, Math.max(max, cell));
+            }
+        }
         return maxDiff;
     }
 }
