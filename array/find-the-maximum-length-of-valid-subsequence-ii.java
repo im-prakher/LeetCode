cd64a81766db50dcf014ade104b8e77cfa66e007
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-07 18:20:44 +0530

    LC-find-the-maximum-length-of-valid-subsequence-ii [Runtime: 38 ms; Memory: 55.9 MB]

--- array/find-the-maximum-length-of-valid-subsequence-ii.java
+++ array/find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maximumLength(int[] nums, int k) {
+        int n = nums.length, ans = 0;
         int[][] dp = new int[k][k];
-        int ans = 0;
-        for(int i = 0; i < nums.length; i++) {
-            int curr_rem = nums[i] % k; // curr remainder
-            for(int j = 0; j < k; j++) { // j-> prev remainder
-                dp[curr_rem][j] = 1 + dp[j][curr_rem];
-                ans = Math.max(ans, dp[curr_rem][j]);
+        for(int i = 0; i < n; i++) {
+            int curr_rem = nums[i] % k;
+            for(int prv_rem = 0; prv_rem < k; prv_rem++) {
+                dp[curr_rem][prv_rem] = 1 + dp[prv_rem][curr_rem];
+                ans = Math.max(ans, dp[curr_rem][prv_rem]);
             }
         }
         return ans;
