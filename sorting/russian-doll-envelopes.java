e128527c0960d4a5d18da69293a226c205af31da
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-09 16:42:53 +0530

    LC-russian-doll-envelopes [Runtime: 43 ms; Memory: 104.6 MB]

--- sorting/russian-doll-envelopes.java
+++ sorting/russian-doll-envelopes.java
@@ -13,7 +13,7 @@ class Solution {
 
     public int maxEnvelopes(int[][] nums) {
         Arrays.sort(nums, (a, b)-> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
-        List<int[]> bin = new ArrayList<>();
+        List<int[]> bin = new ArrayList<>(); // will always be in increasing order
         bin.add(nums[0]);
         for(int i = 1; i < nums.length; i++) {
             int[] last = bin.getLast();
@@ -21,7 +21,6 @@ class Solution {
                 bin.add(nums[i]);
             else {
                 int lb = lower_bound2d(bin, nums[i]);
-                if(bin.getLast()[1] != nums[i][1])
                 bin.set(lb, nums[i]);
             }
         }
