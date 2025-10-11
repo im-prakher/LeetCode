15d204a9d1bc26140c8f87b2de1f11ebdd4ace92
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-11 06:30:39 +0530

    LC-remove-zero-sum-consecutive-nodes-from-linked-list [Runtime: N/A; Memory: N/A]

--- hash-table/remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ hash-table/remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -9,17 +9,6 @@
  * }
  */
 class Solution {
-    void clearMap(int k, Map<Integer, ListNode> mp) {
-        Map<Integer, ListNode> temp = new LinkedHashMap<>();
-        for(int key : mp.keySet()) {
-            temp.put(key, mp.get(key));
-            if(key == k)
-                break;
-        }
-        mp.clear();
-        mp.putAll(temp);
-    }
-
     public ListNode removeZeroSumSublists(ListNode head) {
         int sum = 0;
         ListNode dum = new ListNode(1001);
@@ -27,16 +16,18 @@ class Solution {
         ListNode curr = head;
         ListNode prev = dum;
         Map<Integer, ListNode> mp = new LinkedHashMap<>();
-        mp.put(0, dum);
+        mp.put(0, head);
         while(curr != null) {
             sum += curr.val;
-            if(mp.containsKey(sum)) {
-                prev = mp.get(sum);
-                prev.next = curr.next;
-                clearMap(sum, mp);
-            } else { 
-                mp.put(sum, curr);
-            }
+            mp.put(sum, curr);
+            curr = curr.next;
+        }
+
+        curr = head;
+        sum = 0;
+        while(curr!=null) {
+            sum += curr.val;
+            curr.next = mp.get(sum).next;
             curr = curr.next;
         }
         return dum.next;
