2817e8c184b04e258a9ad7265c9381377639a99c
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-13 06:45:55 +0530

    LC-container-with-most-water [Runtime: 5 ms; Memory: 57.4 MB]

--- greedy/container-with-most-water.java
+++ greedy/container-with-most-water.java
@@ -5,10 +5,12 @@ class Solution {
             int height = Math.min(nums[i], nums[j]);
             area = Math.max(area, height * (j-i));
             if(nums[i] < nums[j]) {
-                while(++i < j && nums[i] <= height);
+                i++;
+                // while(++i < j && nums[i] <= height);
             }
             else {
-                while(--j > i && nums[j] <= height);
+                j--;
+                // while(--j > i && nums[j] <= height);
             }
         }
         return area;
