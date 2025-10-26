a17ebc5675491420be0731a2ab0e2a2bde880c5d
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-10-27 04:56:20 +0530

    LC-minimum-cost-to-make-array-equal [Runtime: 22 ms; Memory: 56.8 MB]

--- binary-search/minimum-cost-to-make-array-equal.java
+++ binary-search/minimum-cost-to-make-array-equal.java
@@ -3,25 +3,24 @@ class Solution {
         return Math.abs(num);
     }
     public long minCost(int[] nums, int[] cost) {
-        long sum = 0, nat_sum = 0, mul_sum = 0, tml_sum = 0, k = 1;
+        long sum = 0l, nat_sum = 0l, mul_sum = 0l, tml_sum = 0l;
         int n = nums.length;
-        int[][] conv = new int[n][2];
+        long[][] conv = new long[n][2];
         for(int i = 0; i < n; i++) {
             conv[i][0] = nums[i];
             conv[i][1] = cost[i];
-            tml_sum += nums[i] * cost[i];
-            nat_sum += cost[i];
+            tml_sum += 1l * nums[i] * cost[i];
+            nat_sum += 1l * cost[i];
         }
-        Arrays.sort(conv, (a, b)-> a[0] - b[0]);
-        long ans = tml_sum;
-        for(int[] cov : conv) {
+        Arrays.sort(conv, (a, b)-> Long.compare(a[0],  b[0]));
+        long ans = tml_sum * 1l;
+        for(long[] cov : conv) {
             mul_sum += cov[0] * cov[1];
             sum += cov[1];
             long res = abs(mul_sum - cov[0] * sum) + abs(tml_sum - cov[0] * nat_sum);
             ans = Math.min(ans, res);
             tml_sum -= cov[0] * cov[1];
             nat_sum -= cov[1];
-            k++;
         }
         return ans;
     }
