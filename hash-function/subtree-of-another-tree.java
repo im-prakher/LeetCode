4b4f49a203a15026858f9cbabe0be81edcf7bacf
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-02 20:58:45 +0530

    LC-subtree-of-another-tree [Runtime: 3 ms; Memory: 46.6 MB]

--- hash-function/subtree-of-another-tree.java
+++ hash-function/subtree-of-another-tree.java
@@ -15,18 +15,19 @@
  */
 class Solution {
     TreeNode sub;
-    public boolean travel(TreeNode root, TreeNode subRoot) {
-        if(root == null)
-            return subRoot == null;
-        if(subRoot != null && root.val == subRoot.val)
-            return travel(root.left, subRoot.left)
-                    && travel(root.right, subRoot.right);
-        subRoot = sub;
-        return travel(root.left, subRoot) || travel(root.right, subRoot);
+    public boolean match(TreeNode a, TreeNode b) {
+        if(a == null || b == null)
+            return a == b;
+        return a.val == b.val && match(a.left, b.left)
+                && match(a.right, b.right);
+        
     }
 
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        sub = subRoot;
-        return travel(root, subRoot);
+        if(root == null)
+            return false;
+        if(match(root, subRoot))
+            return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 }
