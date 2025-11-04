f2c485bae28aadb99d96ab61bf31024b18863257
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-04 07:31:01 +0530

    LC-find-minimum-time-to-reach-last-room-i [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/find-minimum-time-to-reach-last-room-i.java
+++ heap-priority-queue/find-minimum-time-to-reach-last-room-i.java
@@ -1,16 +1,34 @@
 class Solution {
-    public int dfsTime(int[][] moveTime, int i, int j) {
-        if(i == 0 && j == 0)
-            return 0;
-        if(i < 0 || j < 0)
-            return Integer.MAX_VALUE/10;
-        int minTime = Math.min(dfsTime(moveTime, i-1, j),
-                    dfsTime(moveTime, i, j-1));
-        return 1 + Math.max(minTime, moveTime[i][j]);
-    }
+    //bfs similar to rotten oranges
+    // public int bfsTime(int[][] moveTime, int i, int j) {
+    // }
 
     public int minTimeToReach(int[][] moveTime) {
-        int time = 0;
-        return dfsTime(moveTime, moveTime.length - 1, moveTime[0].length - 1);
+        int time = 0, n = moveTime.length, m = moveTime[0].length;
+        // return bfsTime(moveTime, 0, 0);
+        PriorityQueue<int[]> que = new PriorityQueue<>(
+            (a, b)-> moveTime[a[0]][a[1]] != moveTime[b[0]][b[1]] ? moveTime[a[0]][a[1]] - moveTime[b[0]][b[1]] : b[1] == a[1] ? b[0] - a[0] : b[1] - a[1]);
+        que.offer(new int[]{0, 0});
+        int dx[] = {0, 1, 0, -1}; 
+        int dy[] = {1, 0, -1, 0};
+        boolean[][] vis = new boolean[n][m];
+        vis[0][0] = true;
+        while(!que.isEmpty()) {
+            int[] node = que.poll();
+            int a = node[0], b = node[1];
+            if(a != 0 || b != 0)
+                time = Math.max(time, moveTime[a][b]) + 1;
+            for(int i = 0; i < 4; i++) {
+                int x = a + dx[i], y = b + dy[i];
+                if(x >= 0 && x < n && y >= 0 && y < m && !vis[x][y]) {
+                    if(x == n-1 && y == m-1)
+                        return Math.max(time, moveTime[x][y]) + 1;;
+                    que.offer(new int[]{x, y});
+                    vis[x][y] = true;
+                }
+            }
+
+        }
+        return time;
     }
 }
