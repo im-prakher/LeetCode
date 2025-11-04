31efd227643eef0b9b927b6912972a0a37298300
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-04 07:50:03 +0530

    LC-find-minimum-time-to-reach-last-room-i [Runtime: 7 ms; Memory: 46.3 MB]

--- array/find-minimum-time-to-reach-last-room-i.java
+++ array/find-minimum-time-to-reach-last-room-i.java
@@ -1,34 +1,28 @@
 class Solution {
     //bfs similar to rotten oranges
-    // public int bfsTime(int[][] moveTime, int i, int j) {
-    // }
-
     public int minTimeToReach(int[][] moveTime) {
         int time = 0, n = moveTime.length, m = moveTime[0].length;
-        // return bfsTime(moveTime, 0, 0);
-        PriorityQueue<int[]> que = new PriorityQueue<>(
-            (a, b)-> moveTime[a[0]][a[1]] != moveTime[b[0]][b[1]] ? moveTime[a[0]][a[1]] - moveTime[b[0]][b[1]] : b[1] == a[1] ? b[0] - a[0] : b[1] - a[1]);
-        que.offer(new int[]{0, 0});
+        PriorityQueue<int[]> que = new PriorityQueue<>((a, b)-> a[2] - b[2]);
+        que.offer(new int[]{0, 0, 0});
         int dx[] = {0, 1, 0, -1}; 
         int dy[] = {1, 0, -1, 0};
         boolean[][] vis = new boolean[n][m];
         vis[0][0] = true;
         while(!que.isEmpty()) {
             int[] node = que.poll();
-            int a = node[0], b = node[1];
-            if(a != 0 || b != 0)
-                time = Math.max(time, moveTime[a][b]) + 1;
+            int a = node[0], b = node[1], curTime = node[2];
+            if(a == n-1 && b == m-1)
+                return curTime;
             for(int i = 0; i < 4; i++) {
                 int x = a + dx[i], y = b + dy[i];
                 if(x >= 0 && x < n && y >= 0 && y < m && !vis[x][y]) {
-                    if(x == n-1 && y == m-1)
-                        return Math.max(time, moveTime[x][y]) + 1;;
-                    que.offer(new int[]{x, y});
+                    int reach = Math.max(curTime, moveTime[x][y]) + 1;;
+                    que.offer(new int[]{x, y, reach});
                     vis[x][y] = true;
                 }
             }
 
         }
-        return time;
+        return -1;
     }
 }
