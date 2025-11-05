8c2167042b0412693b0cae08f690f12e635bda65
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-05 15:48:40 +0530

    LC-minimum-cost-to-make-at-least-one-valid-path-in-a-grid [Runtime: 21 ms; Memory: 47.2 MB]

--- graph/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ graph/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -7,19 +7,20 @@ class Solution {
         int[][] vis = new int[n][m];
         for(int[] row: vis) 
             Arrays.fill(row, (int) 1e9);
+        vis[0][0] = 0;
         while(!que.isEmpty()) {
             int[] node = que.poll();
             int cost = node[0], a = node[1], b = node[2];
-            if(vis[a][b] <= cost) 
+            if(vis[a][b] > cost) 
                 continue;
-            vis[a][b] = cost;
             if(a == n-1 && b == m-1)
                 return cost;
             for(int k = 0; k < 4; k++) {
                 int x = a + dx[k], y = b + dy[k];
                 if(x >= 0 && y >= 0 && x < n && y < m) {
                     int reach = cost + (grid[a][b] == k + 1 ? 0 : 1);
-                    if(vis[x][y] >= reach) {
+                    if(vis[x][y] > reach) {
+                        vis[x][y] = reach;
                         que.offer(new int[] {reach, x, y});
                     }
                 }
