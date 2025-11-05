38c240e3d93b11cc77932a72f90c5fcc2df093d8
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-05 15:30:08 +0530

    LC-minimum-cost-to-make-at-least-one-valid-path-in-a-grid [Runtime: 31 ms; Memory: 47.3 MB]

--- heap-priority-queue/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ heap-priority-queue/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -4,18 +4,25 @@ class Solution {
         que.offer(new int[]{0, 0, 0});
         int n = grid.length, m = grid[0].length;
         int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
-        boolean[][] vis = new boolean[n][m];
+        int[][] vis = new int[n][m];
+        for(int[] row: vis) {
+            Arrays.fill(row, (int) 1e9);
+        }
         while(!que.isEmpty()) {
             int[] node = que.poll();
             int cost = node[0], a = node[1], b = node[2];
-            vis[a][b] = true;
+            if(vis[a][b] <= cost) 
+                continue;
+            vis[a][b] = cost;
             if(a == n-1 && b == m-1)
                 return cost;
             for(int k = 0; k < 4; k++) {
                 int x = a + dx[k], y = b + dy[k];
-                if(x >= 0 && y >= 0 && x < n && y < m && !vis[x][y]) {
+                if(x >= 0 && y >= 0 && x < n && y < m) {
                     int reach = cost + (grid[a][b] == k + 1 ? 0 : 1);
-                    que.offer(new int[] {reach, x, y});
+                    if(vis[x][y] > reach) {
+                        que.offer(new int[] {reach, x, y});
+                    }
                 }
             }
         }
