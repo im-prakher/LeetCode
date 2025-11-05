40d499fa2b0deda179f60bdb41a22dd2b3782fa9
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-05 15:54:55 +0530

    LC-find-minimum-time-to-reach-last-room-i [Runtime: N/A; Memory: N/A]

--- graph/find-minimum-time-to-reach-last-room-i.java
+++ graph/find-minimum-time-to-reach-last-room-i.java
@@ -1,24 +1,30 @@
 class Solution {
     //bfs similar to rotten oranges
     public int minTimeToReach(int[][] moveTime) {
-        int time = 0, n = moveTime.length, m = moveTime[0].length;
+        int n = moveTime.length, m = moveTime[0].length;
         PriorityQueue<int[]> que = new PriorityQueue<>((a, b)-> a[2] - b[2]);
         que.offer(new int[]{0, 0, 0});
         int dx[] = {0, 1, 0, -1}; 
         int dy[] = {1, 0, -1, 0};
-        boolean[][] vis = new boolean[n][m];
-        vis[0][0] = true;
+        int[][] time = new int[n][m];
+        for(int[] row: time)
+            Arrays.fill(row, (int) 1e9);
+        time[0][0] = 0;
         while(!que.isEmpty()) {
             int[] node = que.poll();
             int a = node[0], b = node[1], curTime = node[2];
+            if(curTime > time[a][b])
+                continue;
             if(a == n-1 && b == m-1)
                 return curTime;
             for(int i = 0; i < 4; i++) {
                 int x = a + dx[i], y = b + dy[i];
-                if(x >= 0 && x < n && y >= 0 && y < m && !vis[x][y]) {
-                    int reach = Math.max(curTime, moveTime[x][y]) + 1;;
-                    que.offer(new int[]{x, y, reach});
-                    vis[x][y] = true;
+                if(x >= 0 && x < n && y >= 0 && y < m) {
+                    int reach = Math.max(curTime, moveTime[x][y]) + 1;
+                    if(reach < time[x][y]) {
+                        time[x][y] = reach;
+                        que.offer(new int[]{x, y, reach});
+                    }
                 }
             }
 
