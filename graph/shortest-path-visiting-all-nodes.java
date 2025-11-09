5529ebffd3d245dfbc1cb1e2e2b4d11a8d52729c
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-09 17:29:19 +0530

    LC-shortest-path-visiting-all-nodes [Runtime: N/A; Memory: N/A]

--- graph/shortest-path-visiting-all-nodes.java
+++ graph/shortest-path-visiting-all-nodes.java
@@ -1,25 +1,28 @@
 class Solution {
-    public int minPath(int i, int n, int[][] graph) {
-        PriorityQueue<int[]> que = new PriorityQueue<>((a, b)-> a[1] - b[1]);
-        que.offer(new int[]{i, 0, 0});
-        int all = (1 << n) - 1;
-        while(!que.isEmpty()) {
-            int[] next = que.poll();
-            int node = next[0], len = next[1], mask = next[2];
-            mask |= (1 << node);
-            if((mask & all) == all)
-                return len;
-            for(int vtx : graph[node]) {
-                que.offer(new int[]{vtx, len + 1, mask});
-            }
-        }
-        return -1;
-    }
     public int shortestPathLength(int[][] graph) {
         int n = graph.length, ans = (int) 1e9;
-        for(int i = 0; i < n; i++) {
-            ans = Math.min(ans, minPath(i, n, graph));
+        Queue<int[]> que = new LinkedList<>();
+        for(int i = 0; i < n; i++) 
+            que.offer(new int[]{i, 0}); 
+        int all = (1 << n) - 1, path = 0;
+        boolean[][] vis = new boolean[n][all+1];
+        while(!que.isEmpty()) {
+            int size = que.size();
+            while(size-- > 0) {
+                int[] next = que.poll();
+                int node = next[0], mask = next[1];
+                for(int vtx : graph[node]) {
+                    int nxtMask = mask | (1 << vtx);
+                    if((nxtMask & all) == all)
+                        return path;
+                    if(!vis[vtx][nxtMask]) {
+                        vis[vtx][nxtMask] = true;
+                        que.offer(new int[]{vtx, nxtMask});
+                    }
+                }
+            }
+            path++;
         }
-        return ans;
+        return -1;
     }
 }
