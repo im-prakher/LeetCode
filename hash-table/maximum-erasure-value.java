6a4c4768bf2282e1da401ed8f715ddee059c8afd
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-16 22:50:16 +0530

    LC-maximum-erasure-value [Runtime: N/A; Memory: N/A]

---
+++ hash-table/maximum-erasure-value.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+      Set<Integer> set = new HashSet<>();
+      int sum = 0;
+      for(int i = 0, j = 0; i < nums.length; i++) {
+        while(set.contains(nums[i])) {
+            sum -= nums[j];
+            set.remove(nums[j++]);
+        }
+        set.add(nums[i]);
+        sum += nums[i];
+      }
+      return sum;
+    }
+}
