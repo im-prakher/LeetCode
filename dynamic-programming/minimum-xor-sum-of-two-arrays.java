b096f7d6906b2c79eff3fd1d793647bec38bccf7
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-21 09:17:16 +0530

    LC-minimum-xor-sum-of-two-arrays [Runtime: N/A; Memory: N/A]

--- dynamic-programming/minimum-xor-sum-of-two-arrays.java
+++ dynamic-programming/minimum-xor-sum-of-two-arrays.java
@@ -1,25 +1,29 @@
 class Solution {
+    int ans;
     public void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
     }
-
-    public int permute(int idx, int[] nums1, int[] nums2) {
+    
+    public void permute(int[] nums1, int[] nums2, String str) {
         int n = nums2.length;
-        if(idx == n)
-            return 0;
-        int ans = Integer.MAX_VALUE;
+        String[] par = str.split("_");
+        int idx = Integer.valueOf(par[0]), sum = Integer.valueOf(par[1]);
+        if(idx == n) 
+            ans = Math.min(ans, sum);
         for(int i = idx; i < n; i++) {
             swap(nums2, idx, i);
-            int xor = nums1[idx] ^ nums2[idx] + permute(idx + 1, nums1, nums2);
+            int xor = nums1[idx] ^ nums2[idx];
+            String s = (idx+1) + "_" + (sum + xor);
+            permute(nums1, nums2, s);
             swap(nums2, idx, i);
-            ans = Math.min(ans, xor);
         }
-        return ans;
     }
 
     public int minimumXORSum(int[] nums1, int[] nums2) {
-        return permute(0, nums1, nums2);
+        ans = Integer.MAX_VALUE;
+        permute(nums1, nums2, "0_0");
+        return ans;
     }
 }
