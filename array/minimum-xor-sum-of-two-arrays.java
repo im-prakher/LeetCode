4d9a50cee34dc2d77d73a0a3c56b298252749f35
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-23 05:24:53 +0530

    LC-minimum-xor-sum-of-two-arrays [Runtime: 11 ms; Memory: 43.4 MB]

--- array/minimum-xor-sum-of-two-arrays.java
+++ array/minimum-xor-sum-of-two-arrays.java
@@ -1,10 +1,5 @@
 class Solution {
     int[] dp;
-    
-    int setBit(int mask, int i) {
-        return mask | (1 << i);
-    }
-
     public int permute(int[] nums1, int[] nums2, int mask) {
         int n = nums2.length, idx = Integer.bitCount(mask);
         if(idx == n) 
@@ -13,8 +8,8 @@ class Solution {
             return dp[mask];
         int ans = Integer.MAX_VALUE;
         for(int i = 0; i < n; i++) {
-            int newMask = setBit(mask, i);
-            if(newMask != mask) {
+            if((mask & (1 << i)) == 0) {
+                int newMask =  mask | (1 << i);
                 int xor = nums1[idx] ^ nums2[i];
                 ans = Math.min(ans, xor + permute(nums1, nums2, newMask));
             }
