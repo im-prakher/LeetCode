148ea6a7c0b40be62d518a274f919b98b2c3983e
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-11-23 05:11:08 +0530

    LC-minimum-xor-sum-of-two-arrays [Runtime: 40 ms; Memory: 48.5 MB]

--- bitmask/minimum-xor-sum-of-two-arrays.java
+++ bitmask/minimum-xor-sum-of-two-arrays.java
@@ -1,29 +1,31 @@
 class Solution {
-    int ans;
-    public void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
-    }
+    Map<Integer, Integer> dp;
     
-    public void permute(int[] nums1, int[] nums2, String str) {
-        int n = nums2.length;
-        String[] par = str.split("_");
-        int idx = Integer.valueOf(par[0]), sum = Integer.valueOf(par[1]);
+    int setBit(int mask, int i) {
+        return mask | (1 << i);
+    }
+
+    public int permute(int[] nums1, int[] nums2, int mask) {
+        int n = nums2.length, idx = Integer.bitCount(mask);
         if(idx == n) 
-            ans = Math.min(ans, sum);
-        for(int i = idx; i < n; i++) {
-            swap(nums2, idx, i);
-            int xor = nums1[idx] ^ nums2[idx];
-            String s = (idx+1) + "_" + (sum + xor);
-            permute(nums1, nums2, s);
-            swap(nums2, idx, i);
+            return 0;
+        if(dp.containsKey(mask))
+            return dp.get(mask);
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++) {
+            int newMask = setBit(mask, i);
+            if(newMask != mask) {
+                int xor = nums1[idx] ^ nums2[i];
+                ans = Math.min(ans, xor + permute(nums1, nums2, newMask));
+            }
         }
+        dp.put(mask, ans);
+        return ans;
     }
 
     public int minimumXORSum(int[] nums1, int[] nums2) {
-        ans = Integer.MAX_VALUE;
-        permute(nums1, nums2, "0_0");
-        return ans;
+        dp = new HashMap<>();
+        int mask = 0, n = nums1.length;
+        return permute(nums1, nums2, mask);
     }
 }
