b5b1ff414498d80d81c46d84313575cfe2cf1bba
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-12-01 23:00:14 +0530

    LC-longest-subarray-of-1s-after-deleting-one-element [Runtime: 5 ms; Memory: 64.5 MB]

--- sliding-window/longest-subarray-of-1s-after-deleting-one-element.java
+++ sliding-window/longest-subarray-of-1s-after-deleting-one-element.java
@@ -13,10 +13,12 @@ class Solution {
             }
         }
         list.add(sum);
-        int ans = list.get(0);
-        for(int i = 1; i < list.size() - 1; i++) {
+        int ans = list.get(0), m = list.size();
+        for(int i = 0; i < m; i++) {
+            int lft = i > 0 ? list.get(i-1) : 0;
+            int rht = i < m-1 ? list.get(i+1) : 0;
             if(list.get(i) == 0) {
-                ans = Math.max(ans, list.get(i-1) + list.get(i+1));
+                ans = Math.max(ans, lft + rht);
             }
         }
         return  list.size() == 1 ? ans - 1 : ans;
