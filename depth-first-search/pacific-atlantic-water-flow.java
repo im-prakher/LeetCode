6262085805b707a746f91b01f34165fe9c25bdd8
Prakher Prashank <43149558+im-prakher@users.noreply.github.com>
2025-12-04 20:54:16 +0530

    LC-pacific-atlantic-water-flow [Runtime: 6 ms; Memory: 47.3 MB]

--- depth-first-search/pacific-atlantic-water-flow.java
+++ depth-first-search/pacific-atlantic-water-flow.java
@@ -4,16 +4,24 @@ class Solution {
                         && j < vis[0].length && !vis[i][j];
     }
 
-    public void dfs(int[][] mat, int i, int j, boolean[][] vis) {
-        vis[i][j] = true;
+    public void bfs(int[][] mat, int a, int b, boolean[][] vis) {
         int[] dr = {-1, 0, 1, 0};
         int[] dc = {0, 1, 0, -1};
-        for(int k = 0; k < 4; k++) {
-            int ni = i + dr[k], nj = j + dc[k];
-            if(isValid(ni, nj, vis) && mat[i][j] <= mat[ni][nj]) {
-                dfs(mat, ni, nj, vis);
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[] {a, b});
+        vis[a][b] = true;
+        while(!que.isEmpty()) {
+            int[] cell = que.poll();
+            int i = cell[0], j = cell[1];
+            for(int k = 0; k < 4; k++) {
+                int ni = i + dr[k], nj = j + dc[k];
+                if(isValid(ni, nj, vis) && mat[i][j] <= mat[ni][nj]) {
+                    vis[ni][nj] = true;
+                    que.offer(new int[] {ni, nj});
+                }
             }
         }
+        
     }
 
     public List<List<Integer>> pacificAtlantic(int[][] mat) {
@@ -22,15 +30,15 @@ class Solution {
         boolean[][] atlVis = new boolean[n][m];
         for(int j = 0; j < m; j++) {
             if(!pacVis[0][j])
-                dfs(mat, 0, j, pacVis);
+                bfs(mat, 0, j, pacVis);
             if(!atlVis[n-1][j])
-                dfs(mat, n-1, j, atlVis);
+                bfs(mat, n-1, j, atlVis);
         }
         for(int i = 0; i < n; i++) {
             if(!pacVis[i][0])
-                dfs(mat, i, 0, pacVis);
+                bfs(mat, i, 0, pacVis);
             if(!atlVis[i][m-1])
-                dfs(mat, i, m-1, atlVis);
+                bfs(mat, i, m-1, atlVis);
         }
 
         List<List<Integer>> ans = new ArrayList<>();
